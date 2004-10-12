d3e068bf76c77be595a5c63876c351849bc13613
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-10-12 14:53:49 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@144 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ linux-2.6.x/include/asm-blackfin/bf533_rtc.h
@@ -0,0 +1,19 @@
+/*
+ * BF533 RTC support functions.
+ */
+
+#ifndef _BF533_RTC_H_
+#define _BF533_RTC_H_
+
+#include <linux/time.h>
+
+/* Intialize the BF533 RTC */
+int rtc_init(void);
+
+/* Get the time stored in RTC. time_in_seconds is no. of seconds from Jan 1970 */
+int rtc_get(time_t *time_in_seconds);
+
+/* Set a new time in the RTC. time_in_seconds is no. of seconds from Jan 1970  */
+int rtc_set(time_t time_in_seconds);
+
+#endif
