1e0df976566eb96310634007835817e99204a959
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-10-19 08:53:34 +0000

    change init.h as per other arch
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@180 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/init.h
+++ linux-2.6.x/include/asm-blackfin/init.h
@@ -1,10 +1,1 @@
-#ifndef _BFIN_INIT_H
-#define _BFIN_INIT_H
-
-/* For assembly routines */
-/*
-#define __INIT		.section	".text.init",#alloc,#execinstr
-#define __FINIT		.previous
-#define __INITDATA	.section	".data.init",#alloc,#write
-*/
-#endif
+#error "<asm/init.h> should never be used - use <linux/init.h> instead" in asm/init.h
