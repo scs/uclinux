b0486a7f3e5d729b5fbead71e4b48aa643fc3fea
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-10-19 09:02:30 +0000

    remove __page_address macro
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@188 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/pgtable.h
+++ linux-2.6.x/include/asm-blackfin/pgtable.h
@@ -15,7 +15,6 @@ typedef pte_t *pte_addr_t;
 #define kern_addr_valid(addr)	(1)
 #define	pmd_offset(a, b)	((void *)0)
 
-#define __page_address(page) (PAGE_OFFSET + (((page) - mem_map) << PAGE_SHIFT))
 #define kern_addr_valid(addr) (1)
 
 #define PAGE_NONE		__pgprot(0)    /* these mean nothing to NO_MM */
