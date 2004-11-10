31f502f20059296f54de2403702af3c5a70e9bbb
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-11-10 03:36:12 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@272 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-test/rtc-test/rtc_test.c
+++ user/blkfin-test/rtc-test/rtc_test.c
@@ -131,7 +131,7 @@ int main()
 	/* This blocks until the alarm ring causes an interrupt */
 	ret = read(rtc_fd, &data, sizeof(unsigned long));
 	if (ret == -1) {
-		printf("rtc read errot\r\n");
+		printf("rtc read error\r\n");
 	}
 	printf(" Okay. Alarm rang.\r\n");
 	
@@ -150,6 +150,19 @@ int main()
 	if (ret == -1) {
 		printf("ioctl RTC_ALE_OFF error\r\n");
 	}
+
+	/* Robin Getz */
+	printf("8.5 test sleep 10 \n");
+	sleep (10);
+	ret = ioctl(rtc_fd, RTC_RD_TIME, &rtc_tm);
+	if (ret == -1) {
+		printf("rtc ioctl RTC_RD_TIME error\r\n");
+	}
+	printf("Current RTC date/time is %d-%d-%d, %02d:%02d:%02d.\r\n",
+		rtc_tm.tm_mday, rtc_tm.tm_mon + 1, rtc_tm.tm_year,
+		rtc_tm.tm_hour, rtc_tm.tm_min, rtc_tm.tm_sec);
+	
+	/* R Getz till here */
 
 	printf("9. ioctl RTC_SWCNT_ON\r\n");
 	ret = ioctl(rtc_fd, RTC_SWCNT_ON, 0);
@@ -171,7 +184,7 @@ int main()
 	}
 	ret = read(rtc_fd, &data, sizeof(unsigned long));
 	if (ret == -1) {
-		printf("rtc read errot\r\n");
+		printf("rtc read error\r\n");
 	}
 	ret = ioctl(rtc_fd, RTC_RD_TIME, &rtc_tm);
 	if (ret == -1) {
@@ -186,7 +199,7 @@ int main()
 	if (ret == -1) {
 		printf("ioctl RTC_SWCNT_OFF error\r\n");
 	}
-#if 0
+#if 1
 	printf("12. ioctl RTC_EPOCH_READ\r\n");
 	ret = ioctl(rtc_fd, RTC_EPOCH_READ, &data);
 	if (ret == -1) {
@@ -195,7 +208,7 @@ int main()
 	printf("Current epoch is %ld\r\n",data);
 
 	printf("13. ioctl RTC_EPOCH_SET\r\n");
-	ret = ioctl(rtc_fd, RTC_EPOCH_SET, 2310);
+	ret = ioctl(rtc_fd, RTC_EPOCH_SET, 2000);
 	if (ret == -1) {
 		printf("ioctl RTC_EPOCH_SET error\r\n");
 	}
@@ -207,6 +220,9 @@ int main()
 		rtc_tm.tm_mday, rtc_tm.tm_mon + 1, rtc_tm.tm_year,
 		rtc_tm.tm_hour, rtc_tm.tm_min, rtc_tm.tm_sec);
 #endif
+
+	printf("RTC Tests done !!\n");	
+
 	close(rtc_fd);
 
 	return 0;
