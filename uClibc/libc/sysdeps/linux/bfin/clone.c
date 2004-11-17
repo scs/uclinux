b079170538b29e2ff3ea84cc1855253880ef7ed6
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-11-17 06:09:59 +0000

    hardcoded values for __NR_clone and __NR_exit removed
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@280 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- uClibc/libc/sysdeps/linux/bfin/clone.c
+++ uClibc/libc/sysdeps/linux/bfin/clone.c
@@ -17,22 +17,22 @@ clone (int (*fn)(void *arg), void *child_stack, int flags, void *arg)
 	
 	if (fn && child_stack) {
 
-	__asm__ __volatile__ ("r1 = %1;"
-			"r0 = %2;"
-			"P0 = 0x78;"
+	__asm__ __volatile__ ("r1 = %2;"
+			"r0 = %3;"
+			"P0 = %1;"
 			"excpt 0;"	 /*Call sys_clone*/
 			"%0  = r0;"
 			: "=d" (rval)
-			: "a" (child_stack), "a" (flags)
+			: "i" (__NR_clone), "a" (child_stack), "a" (flags)
 			: "CC", "R0", "R1", "P0");
 			
 		if (rval == 0) {
 		/* In child thread, call FN and exit.  */
 		arg0 = (*fn) (arg);
 		__asm__ __volatile__ (
-			"P0 = 0x1;" 	 /* should be need to pass arg0 to exit sys call*/
+			"P0 = %0;" 	 /* should be need to pass arg0 to exit sys call*/
 			"excpt 0;"	 /*Call sys_exit*/
-			: : : "P0");
+			: : "i" (__NR_exit) : "P0");
 		}
 	}
 	return rval;
