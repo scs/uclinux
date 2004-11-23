6f1311272389e907ca6e4a658e39d761e6601cf2
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-11-23 16:10:19 +0000

    Parameter passing problem to thread function fixed
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@341 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- uClibc/libc/sysdeps/linux/bfin/clone.c
+++ uClibc/libc/sysdeps/linux/bfin/clone.c
@@ -13,29 +13,27 @@
 int
 clone (int (*fn)(void *arg), void *child_stack, int flags, void *arg)
 {
-	long rval = -1, arg0;
+	register long rval = -1;
 	
-	printf("lib: Arg %d\n",arg);
 	if (fn && child_stack) {
 
-	__asm__ __volatile__ ("r1 = %2;"
+	__asm__ __volatile__ (
+			"r1 = %2;"
 			"r0 = %3;"
 			"P0 = %1;"
 			"excpt 0;"	 /*Call sys_clone*/
 			"%0  = r0;"
+			"cc = r0 == 0;"
+			"if !cc jump xxx;"	/* if (rval != 0) skip to parent */
+			"r0 = %4;"
+			"p0 = %5;"
+			"call (p0);"	/* Call cloned function */
+			"p0 = %6;"
+			"excpt 0;"	/* Call sys_exit */
+			"xxx: nop;"
 			: "=d" (rval)
-			: "i" (__NR_clone), "a" (child_stack), "a" (flags)
+			: "i" (__NR_clone), "a" (child_stack), "a" (flags), "a" (arg), "a" (fn), "i" (__NR_exit)
 			: "CC", "R0", "R1", "P0");
-			
-		if (rval == 0) {
-		/* In child thread, call FN and exit.  */
-		arg0 = (*fn) (arg);
-		__asm__ __volatile__ (
-			"P0 = %0;" 	 /* should be need to pass arg0 to exit sys call*/
-			"excpt 0;"	 /*Call sys_exit*/
-			: : "i" (__NR_exit) : "P0");
-		}
 	}
 	return rval;
 }
-
