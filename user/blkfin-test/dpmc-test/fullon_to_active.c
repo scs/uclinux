59db6fd492b223a0ab566e346991970518d5959e
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-11-23 05:46:09 +0000

    application to change the processor mode from fullon to active
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@311 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/blkfin-test/dpmc-test/fullon_to_active.c
@@ -0,0 +1,47 @@
+/*
+ * RTC driver test code
+ */
+
+#include <time.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <linux/ioctl.h>
+#include <errno.h>
+#include "dpmc.h"
+#include <linux/rtc.h>
+#include "blackfin_rtc.h"
+
+int main()
+{
+	int fd;
+	int ret;
+   	unsigned long pllstat;
+
+	printf("##########################DPMC Test Programs##################################\n");
+
+/*******************************Open the dpmc device ***********************************/
+	fd = open("/dev/dpmc", O_RDONLY,0);
+	if (fd == -1) {
+		printf("/dev/dpmc open error %d\n",errno);
+		exit(1);
+	}
+	else printf("open success fd = %d \n",fd);
+
+/********************************Fullon to Active Mode ********************************/	
+	printf("IOCTL to CHANGE OPERATING MODE FROM FULLON TO ACTIVE \n");
+	printf("Entering Active Mode \n");
+	ret = ioctl(fd, IOCTL_ACTIVE_MODE, NULL);
+	printf("Active mode set %d \n",ret);
+
+/********************************Get the PLL Status***********************************/
+	printf("IOCTL to get the PLL status \n");
+	ret = ioctl(fd, IOCTL_GET_PLLSTATUS, &pllstat);
+	printf("pll status got is 0x%x\n",pllstat);
+
+	close(fd);
+
+	return 0;
+}
