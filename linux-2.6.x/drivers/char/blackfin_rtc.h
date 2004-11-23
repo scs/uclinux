e778ac5fc798a69f2bc832719c274106b02cced2
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-11-23 06:04:37 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@322 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/char/blackfin_rtc.h
+++ linux-2.6.x/drivers/char/blackfin_rtc.h
@@ -1,22 +1,16 @@
+
 /*
  * Copyright (C) 2004  LG Soft India 
  */
 
 /* bit define */
-#define DAY_BITS_OFF     17
-#define HOUR_BITS_OFF    12
-#define MIN_BITS_OFF     6
+#define DAY_BITS_OFF     24
+#define HOUR_BITS_OFF    16
+#define MIN_BITS_OFF     8
 #define SEC_BITS_OFF     0
 
-#define MIN_TO_SECS(_x_) (60 * _x_)
-#define HRS_TO_SECS(_x_) (60 * 60 * _x_)
-#define DAYS_TO_SECS(_x_) (24 * 60 * 60 * _x_)
-
-#define NUM_SECS_IN_DAY (24 * 3600)
-#define NUM_SECS_IN_HOUR (3600)
-#define NUM_SECS_IN_MIN (60)
-
 /*RTC Interrupt Control Register Bit Define*/
+
 #define STPW_INT_EN     0x0001
 #define ALM_INT_EN      0x0002
 #define SEC_INT_EN      0x0004
