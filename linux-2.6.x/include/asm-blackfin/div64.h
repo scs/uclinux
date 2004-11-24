ac1a8995f8c09c4a7d5fb0d58614713c7ca71f42
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-11-24 08:15:36 +0000

    use asm-generic do_div
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@347 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/div64.h
+++ linux-2.6.x/include/asm-blackfin/div64.h
@@ -1,13 +1,1 @@
-#ifndef _BFINNOMMU_DIV64_H
-#define _BFINNOMMU_DIV64_H
-
-/* n = n / base; return rem; */
-
-#define do_div(n,base) ({					\
-	int __res;						\
-	__res = ((unsigned long) n) % (unsigned) base;		\
-	n = ((unsigned long) n) / (unsigned) base;		\
-	__res;							\
-})
-
-#endif /* _BFIN_DIV64_H */
+#include <asm-generic/div64.h>
