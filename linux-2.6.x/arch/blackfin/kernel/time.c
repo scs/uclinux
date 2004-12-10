08add8e2b5ad4a031ecc339349e5acdb9635270d
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-12-10 13:17:04 +0000

    move timer_init func here
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@437 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/arch/blackfin/kernel/time.c
+++ linux-2.6.x/arch/blackfin/kernel/time.c
@@ -22,6 +22,8 @@
 #include <linux/time.h> 
 #include <linux/timex.h>
 
+#include <asm/blackfin.h>
+#include <asm/irq.h>
 #include <asm/io.h>
 #include <asm/bf533_rtc.h>
 
@@ -34,6 +36,53 @@ EXPORT_SYMBOL(jiffies_64);
 extern void time_sched_init(irqreturn_t (*timer_routine)(int, void *, struct pt_regs *));
 unsigned long gettimeoffset (void);
 extern unsigned long wall_jiffies;
+
+/*
+ * By setting TSCALE such that TCOUNT counts a binary fraction
+ * of microseconds, we can read TCOUNT directly and then with
+ * a logical shift trivially calculate how many microseconds 
+ * since the last tick, allowing do_gettimeofday() to yield
+ * far better time resolution for real time benchmarking.
+ */
+
+extern u_long get_cclk(void);
+
+#define TSCALE_SHIFT 2	/* 0.25 microseconds */
+#define TSCALE_COUNT ((get_cclk()/1000000) >> TSCALE_SHIFT)
+#define CLOCKS_PER_JIFFY ((1000*1000/HZ) << TSCALE_SHIFT)
+
+
+void time_sched_init(irqreturn_t (*timer_routine)(int, void *, struct pt_regs *))
+{
+	/* power up the timer, but don't enable it just yet */
+
+	*pTCNTL = 1;
+	asm("csync;");
+
+	/* make TCOUNT a binary fraction of microseconds using
+	* the TSCALE prescaler counter.
+	*/
+
+	*pTSCALE = TSCALE_COUNT - 1;
+	asm("csync;");
+	*pTCOUNT = *pTPERIOD = CLOCKS_PER_JIFFY - 1;
+	asm("csync;");
+
+	/* now enable the timer */
+	
+	*pTCNTL = 7;
+	asm("csync;");
+
+	/* set up the timer irq */
+
+	request_irq(IRQ_CORETMR, timer_routine, IRQ_FLG_LOCK, "timer", NULL);
+	enable_irq(IRQ_CORETMR);
+}
+
+unsigned long gettimeoffset (void)
+{
+	return (CLOCKS_PER_JIFFY - *pTCOUNT) >> TSCALE_SHIFT;
+}
 
 static inline int set_rtc_mmss(unsigned long nowtime)
 {
