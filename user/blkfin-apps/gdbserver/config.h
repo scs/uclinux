5f935c5134adc24bd3bbdce0b5f7c3d732e06743
pcs <pcs@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-12-22 11:00:48 +0000

    blackfin gdb server. Master in binutils cvs
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@473 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/blkfin-apps/gdbserver/config.h
@@ -0,0 +1,70 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if the target supports PTRACE_PEEKUSR for register access.  */
+#define HAVE_LINUX_USRREGS 1
+
+/* Define if the target supports PTRACE_GETREGS for register access.  */
+/* #undef HAVE_LINUX_REGSETS */
+
+/* Define if the target supports PTRACE_GETFPXREGS for extended
+   register access.  */
+/* #undef HAVE_PTRACE_GETFPXREGS */
+
+/* Define if the prfpregset_t type is broken. */
+/* #undef PRFPREGSET_T_BROKEN */
+
+/* Define if you have the <linux/elf.h> header file.  */
+#define HAVE_LINUX_ELF_H 1
+
+/* Define if you have the <proc_service.h> header file.  */
+/* #undef HAVE_PROC_SERVICE_H */
+
+/* Define if you have the <sgtty.h> header file.  */
+#define HAVE_SGTTY_H 1
+
+/* Define if you have the <stdlib.h> header file.  */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <sys/procfs.h> header file.  */
+#define HAVE_SYS_PROCFS_H 1
+
+/* Define if you have the <sys/reg.h> header file.  */
+#define HAVE_SYS_REG_H 1
+
+/* Define if you have the <termio.h> header file.  */
+#define HAVE_TERMIO_H 1
+
+/* Define if you have the <termios.h> header file.  */
+#define HAVE_TERMIOS_H 1
+
+/* Define if you have the <thread_db.h> header file.  */
+#define HAVE_THREAD_DB_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if strerror is not declared in system header files. */
+/* #undef NEED_DECLARATION_STRERROR */
+
+/* Define if <sys/procfs.h> has lwpid_t. */
+/* #undef HAVE_LWPID_T */
+
+/* Define if <sys/procfs.h> has psaddr_t. */
+/* #undef HAVE_PSADDR_T */
+
+/* Define if <sys/procfs.h> has prgregset_t. */
+/* #undef HAVE_PRGREGSET_T */
+
+/* Define if <sys/procfs.h> has prfpregset_t. */
+/* #undef HAVE_PRFPREGSET_T */
+
+/* Define if <sys/procfs.h> has elf_fpregset_t. */
+/* #undef HAVE_ELF_FPREGSET_T */
+
