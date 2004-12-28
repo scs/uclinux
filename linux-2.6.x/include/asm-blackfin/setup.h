a1ee442e02a9f44fa6e852e677e341bfa040c80d
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-12-28 13:32:10 +0000

    cleanup
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@489 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/setup.h
+++ linux-2.6.x/include/asm-blackfin/setup.h
@@ -12,64 +12,6 @@
 #ifndef _BFIN_SETUP_H
 #define _BFIN_SETUP_H
 
-#include <linux/config.h>
-
-#undef COMMAND_LINE_SIZE
 #define COMMAND_LINE_SIZE	512
 
-/*
- *  Linux/bfin Architectures
- */
-
-#define MACH_BFIN    1
-
-#ifdef __KERNEL__
-
-#ifndef __ASSEMBLY__
-extern unsigned long bfin_machtype;
-#endif /* !__ASSEMBLY__ */
-
-#if defined(CONFIG_BFIN)
-#define MACH_IS_BFIN (bfin_machtype == MACH_BFIN)
-#endif
-
-#ifndef MACH_TYPE
-#define MACH_TYPE (bfin_machtype)
-#endif
-
-#endif /* __KERNEL__ */
-
-
-    /*
-     *  CPU, FPU and MMU types
-     *
-     *  Note: we don't need now:
-     *
-     */
-#ifdef __KERNEL__
-
-#ifndef __ASSEMBLY__
-extern unsigned long bfin_cputype;
-#ifdef CONFIG_VME
-extern unsigned long vme_brdtype;
-#endif 
-
-    /*
-     *  Miscellaneous
-     */
-
-#define NUM_MEMINFO	4
-#define CL_SIZE		256
-
-extern int bfin_num_memory;		/* # of memory blocks found (and used) */
-extern int bfin_realnum_memory;		/* real # of memory blocks found */
-extern struct mem_info bfin_memory[NUM_MEMINFO];/* memory description */
-
-struct mem_info {
-	unsigned long addr;		/* physical address of memory chunk */
-	unsigned long size;		/* length of memory chunk (in bytes) */
-};
-#endif /* __ASSEMBLY__ */
-
-#endif /*__KERNEL__*/
 #endif /* _BFIN_SETUP_H */
