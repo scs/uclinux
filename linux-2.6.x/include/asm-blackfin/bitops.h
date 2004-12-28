a21aa44413e725787f879e8d557480dbb7ec77c2
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-12-28 15:39:21 +0000

    remove warnings
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@497 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/bitops.h
+++ linux-2.6.x/include/asm-blackfin/bitops.h
@@ -152,7 +152,7 @@ static __inline__ void __change_bit(int nr, volatile unsigned long * addr)
         *ADDR ^= mask;
 }
 
-static __inline__ int test_and_set_bit(int nr, volatile unsigned long * addr)
+static __inline__ int test_and_set_bit(int nr, void *addr)
 {
 	int	mask, retval;
 	volatile unsigned int *a = (volatile unsigned int *) addr;
@@ -238,12 +238,12 @@ static __inline__ int __test_and_change_bit(int nr, volatile unsigned long * add
 /*
  * This routine doesn't need to be atomic.
  */
-static __inline__ int __constant_test_bit(int nr, const volatile unsigned long * addr)
+static __inline__ int __constant_test_bit(int nr, const void *addr)
 {
 	return ((1UL << (nr & 31)) & (((const volatile unsigned int *) addr)[nr >> 5])) != 0;
 }
 
-static __inline__ int __test_bit(int nr, const volatile unsigned long * addr)
+static __inline__ int __test_bit(int nr, const void *addr)
 {
 	int 	* a = (int *) addr;
 	int	mask;
