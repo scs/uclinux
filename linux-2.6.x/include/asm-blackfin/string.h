b1b2d0f8e02fad1f37509ba4f76ac708f1233d4d
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-12-28 13:13:15 +0000

    cleanup
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@488 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/string.h
+++ linux-2.6.x/include/asm-blackfin/string.h
@@ -3,11 +3,6 @@
 
 #ifdef __KERNEL__ /* only set these up for kernel code */
 
-/*************
- * Still not sure whether the optimised inline asm 
- * are small.
-**************/
-
 #include <asm/setup.h>
 #include <asm/page.h>
 
@@ -106,33 +101,5 @@ extern void * memcpy(void *d, const void *s, size_t count);
 #define __HAVE_ARCH_MEMCMP
 extern int memcmp(const void *,const void *,__kernel_size_t);
 
-#else /* KERNEL */
-
-/*
- *	let user libraries deal with these,
- *	IMHO the kernel has no place defining these functions for user apps
- */
-
-#define __HAVE_ARCH_STRCPY 1
-#define __HAVE_ARCH_STRNCPY 1
-#define __HAVE_ARCH_STRCAT 1
-#define __HAVE_ARCH_STRNCAT 1
-#define __HAVE_ARCH_STRCMP 1
-#define __HAVE_ARCH_STRNCMP 1
-#define __HAVE_ARCH_STRNICMP 1
-#define __HAVE_ARCH_STRCHR 1
-#define __HAVE_ARCH_STRRCHR 1
-#define __HAVE_ARCH_STRSTR 1
-#define __HAVE_ARCH_STRLEN 1
-#define __HAVE_ARCH_STRNLEN 1
-#define __HAVE_ARCH_MEMSET 1
-#define __HAVE_ARCH_MEMCPY 1
-#define __HAVE_ARCH_MEMMOVE 1
-#define __HAVE_ARCH_MEMSCAN 1
-#define __HAVE_ARCH_MEMCMP 1
-#define __HAVE_ARCH_MEMCHR 1
-#define __HAVE_ARCH_STRTOK 1
-
-#endif /* KERNEL */
-
+#endif /*__KERNEL__*/
 #endif /* _BFIN_STRING_H_ */
