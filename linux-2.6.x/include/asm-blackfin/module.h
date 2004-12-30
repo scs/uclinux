601cf6114f4f9f91d8daf3606c233b83a5becdf6
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-12-30 13:40:43 +0000

    cleanup
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@501 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/module.h
+++ linux-2.6.x/include/asm-blackfin/module.h
@@ -1,24 +1,10 @@
-/* changes origined from m68k.....   Lineo, Inc		May 2001  */
-
 #ifndef _ASM_BFIN_MODULE_H
 #define _ASM_BFIN_MODULE_H
 
+struct mod_arch_specific {};
 
-/*
- * This file contains the bfin architecture specific module code.
- */
-struct mod_arch_specific
-{
-	int foo;
-};
 #define Elf_Shdr	Elf32_Shdr
 #define Elf_Sym		Elf32_Sym
 #define Elf_Ehdr	Elf32_Ehdr
-
-
-
-#define module_map(x)		vmalloc(x)
-#define module_unmap(x)		vfree(x)
-#define module_arch_init(x)	(0)
 
 #endif /* _ASM_BFIN_MODULE_H */
