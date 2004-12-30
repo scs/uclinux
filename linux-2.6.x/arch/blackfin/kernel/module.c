aba6ba64dff24af2a8437aaa2fc7bae897741d98
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2004-12-30 12:42:52 +0000

    sample application can be loaded into the kernel now
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@499 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/arch/blackfin/kernel/module.c
+++ linux-2.6.x/arch/blackfin/kernel/module.c
@@ -7,6 +7,129 @@
 
 #define DEBUGP(fmt...)
 
+/* handle arithmetic relocations.
+   See binutils/bfd/elf32-bfin.c for more details
+*/
+#define RELOC_STACK_SIZE 100
+static uint32_t reloc_stack[RELOC_STACK_SIZE];
+static unsigned int reloc_stack_tos = 0;
+
+#define is_reloc_stack_empty() ((reloc_stack_tos > 0)?0:1)
+
+static void
+reloc_stack_push(uint32_t value)
+{
+  reloc_stack[reloc_stack_tos++] = value;
+}
+
+static uint32_t
+reloc_stack_pop(void)
+{
+  return reloc_stack[--reloc_stack_tos];
+}
+
+static uint32_t
+reloc_stack_operate(unsigned int oper)
+{
+  uint32_t value;
+  switch(oper){
+    case R_add :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] + reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_sub :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] - reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_mult :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] * reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_div :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] / reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_mod :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] % reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_lshift :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] << reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_rshift :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] >> reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_and :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] & reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_or :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] | reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_xor :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] ^ reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_land :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] && reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_lor :
+    {
+      value = reloc_stack[reloc_stack_tos - 2] || reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 2;
+      break;
+    }
+    case R_neg :
+    {
+      value = -reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos --;
+      break;
+    }
+    case R_comp :
+    {
+      value = ~reloc_stack[reloc_stack_tos - 1];
+      reloc_stack_tos -= 1;
+      break;
+    }
+    default :
+    {
+       printk("unhandled reloction\n");
+       return 0;
+    }
+  }
+
+  /* now push the new value back on stack */
+  reloc_stack_push(value);
+
+  return value;
+}
+
 void *module_alloc(unsigned long size)
 {
 	if (size == 0)
@@ -40,6 +163,16 @@ int apply_relocate(Elf32_Shdr *sechdrs,
 	return -ENOEXEC;
 }
 
+/*************************************************************************/
+/* FUNCTION : apply_relocate_add                                         */
+/* ABSTRACT : Blackfin specific relocation handling for the loadable     */
+/*            modules. Modules are expected to be .o files.              */
+/*            Arithmetic relocations are handled.                        */
+/*            We do not expect LSETUP to be split and hence is not       */
+/*            handled.                                                   */
+/*            R_byte and R_byte2 are also not handled as the gas         */
+/*            does not generate it.                                      */
+/*************************************************************************/
 int apply_relocate_add(Elf32_Shdr *sechdrs,
 		       const char *strtab,
 		       unsigned int symindex,
@@ -47,42 +180,93 @@ int apply_relocate_add(Elf32_Shdr *sechdrs,
 		       struct module *me)
 {
 	unsigned int i;
-	Elf32_Rel *rel = (void *)sechdrs[relsec].sh_addr;
+	Elf32_Rela *rel = (void *)sechdrs[relsec].sh_addr;
 	Elf32_Sym *sym;
-	uint32_t *location;
+	uint32_t *location32;
+	uint16_t *location16;
+	uint32_t value;
 
 	DEBUGP("Applying relocate section %u to %u\n", relsec,
 	       sechdrs[relsec].sh_info);
 	for (i = 0; i < sechdrs[relsec].sh_size / sizeof(*rel); i++) {
 		/* This is where to make the change */
-		location = (void *)sechdrs[sechdrs[relsec].sh_info].sh_addr
-			+ rel[i].r_offset;
+		location16 = (uint16_t *)(sechdrs[sechdrs[relsec].sh_info].sh_addr
+			+ rel[i].r_offset);
+		location32 = (uint32_t *)location16;
 		/* This is the symbol it is referring to.  Note that all
-		   undefined symbols have been resolved.  */
+	   	   undefined symbols have been resolved.  */
 		sym = (Elf32_Sym *)sechdrs[symindex].sh_addr
 			+ ELF32_R_SYM(rel[i].r_info);
+		if(is_reloc_stack_empty()){
+			value = sym->st_value;
+		}
+		else{
+			value = reloc_stack_pop();
+		}
+		value += rel[i].r_addend;
+		DEBUGP("location is %x, value is %x type is %d \n", location32, value, ELF32_R_TYPE(rel[i].r_info));
+
 		switch (ELF32_R_TYPE(rel[i].r_info)) {
 	
-		case R_pcrel12_jump:
-		case R_pcrel12_jump_s:
 		case R_pcrel24:
 		case R_pcrel24_jump_l:
-		case R_pcrel24_jump_x:
-		case R_pcrel24_call_x:
-		case R_pcrel10:
 			/* Add the value, subtract its postition */
-			*location += sym->st_value - (uint32_t)location;
+			location16 = (uint16_t *)(sechdrs[sechdrs[relsec].sh_info].sh_addr
+				+ rel[i].r_offset - 2);
+			location32 = (uint32_t *)location16;
+			value -= (uint32_t)location32;
+			value >>= 1;
+			DEBUGP("value is %x, before %x-%x after %x-%x\n", value, *location16, *(location16 + 1), (*location16 & 0xff00) | (value>>16 & 0x00ff), value&0xffff);
+			*location16 = (*location16 & 0xff00) | (value>>16 & 0x00ff);
+			*(location16 + 1) = value&0xffff;
 			break;
-	
+		case R_pcrel12_jump:
+		case R_pcrel12_jump_s:
+			value -= (uint32_t)location32;
+			value >>= 1;
+			*location16 = (value & 0xfff);
+			break;
+		case R_pcrel10 :
+			value -= (uint32_t)location32;
+			value >>= 1;
+			*location16 = (value & 0x3ff);
+			break;
 		case R_luimm16:
+			DEBUGP("before %x after %x\n", *location16, (value & 0xffff));
+			*location16 = (value & 0xffff);
+			break;
 		case R_huimm16:
+			DEBUGP("before %x after %x\n", *location16, ((value>>16) & 0xffff));
+			*location16 = ((value>>16) & 0xffff);
 			break;
 		case R_rimm16:
+			*location16 = (value & 0xffff);
 			break;
 		case R_byte4_data:
+			DEBUGP("before %x after %x\n", *location32, value);
+			*location32 = value;
 			break;
-		case R_unusedc:
-		case R_unused0:
+		case R_push :
+			reloc_stack_push(value);
+			break;
+		case R_const :
+			reloc_stack_push(rel[i].r_addend);
+			break;
+		case R_add :
+		case R_sub :
+		case R_mult :
+		case R_div :
+		case R_mod :
+		case R_lshift :
+		case R_rshift :
+		case R_and :
+		case R_or :
+		case R_xor :
+		case R_land :
+		case R_lor :
+		case R_neg :
+		case R_comp :
+			reloc_stack_operate(ELF32_R_TYPE(rel[i].r_info));
 			break;
 		default:
 			printk(KERN_ERR "module %s: Unknown relocation: %u\n",
