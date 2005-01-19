4381bcd7123a973a20bb18dbb00da2feacb7699b
pcs <pcs@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-01-19 08:29:01 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@544 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/devfbapp/devtest.c
+++ user/blkfin-apps/devfbapp/devtest.c
@@ -7,28 +7,50 @@
 #include <asm/param.h>
 #include "devtest.h"
 
+#define YELLOW  0x00ff00ff 
+#define RED     0x000000ff 
+#define BLUE    0xff00ff00 
+#define GREEN   0x00ff0000 
+#define WHITE   0xffffffff
+#define BLACK	0x00
+#define OFFSET  37800
 
 int main(int argc, char *argb[])
 {
-	int black =0, yellow = 0x00ff00ff, red = 0x000000ff, blue = 0xff00ff00, green = 0x00ff0000, white = 0xffffffff, offset = 37800;
 	dev_init_func();
 	int i;
-	for(i=0;i<756000;i++)
-        *(screen_ptr+i) = 0xff;
-	for(i = 0;i<100;i++)
-		draw_line(130+i, 212, 130+i, 312, blue);
-	sleep(2);
-	for(i = 0;i<100;i++)
-		draw_line(left_mar +i, up_mar, left_mar +i, up_mar +100, red);
-	sleep(2);
-	for(i = 0;i<100;i++)
-		draw_line(left_mar +i, screen_height - low_mar -100, left_mar +i, screen_height - low_mar, green);
-	sleep(2);
-	for(i = 0;i<100;i++)
-		draw_line(screen_width - right_mar -100 +i, up_mar, screen_width -right_mar -100 +i, up_mar +100, yellow);
-	sleep(2);
-	for(i = 0;i<100;i++)
-		draw_line(screen_width -right_mar  -100 +i, screen_height -low_mar -100, screen_width -right_mar  -100 +i, screen_height -low_mar, black);
-		
+	for(;;){
+		for(i=0;i<756000;i++)
+			*(screen_ptr+i) = 0xff;
+		for(i = 0;i<100;i++)
+			draw_line(130+i, 212, 130+i, 312, BLUE);
+		sleep(2);
+		for(i = 0;i<100;i++)
+			draw_line(130+i, 212, 130+i, 312, WHITE);
+
+		for(i = 0;i<100;i++)
+			draw_line(left_mar +i, up_mar, left_mar +i, up_mar +100, RED);
+		sleep(2);
+		for(i = 0;i<100;i++)
+			draw_line(left_mar +i, up_mar, left_mar +i, up_mar +100, WHITE);
+
+		for(i = 0;i<100;i++)
+			draw_line(left_mar +i, screen_height - low_mar -100, left_mar +i, screen_height - low_mar, GREEN);
+		sleep(2);        
+		for(i = 0;i<100;i++)
+			draw_line(left_mar +i, screen_height - low_mar -100, left_mar +i, screen_height - low_mar, WHITE);
+	
+		for(i = 0;i<100;i++)
+			draw_line(screen_width - right_mar -100 +i, up_mar, screen_width -right_mar -100 +i, up_mar +100, YELLOW);
+		sleep(2);
+		for(i = 0;i<100;i++)
+			draw_line(screen_width - right_mar -100 +i, up_mar, screen_width -right_mar -100 +i, up_mar +100, WHITE);
+
+		for(i = 0;i<100;i++)
+			draw_line(screen_width -right_mar  -100 +i, screen_height -low_mar -100, screen_width -right_mar  -100 +i, screen_height -low_mar, BLACK);
+		sleep(2);
+		for(i = 0;i<100;i++)
+			draw_line(screen_width -right_mar  -100 +i, screen_height -low_mar -100, screen_width -right_mar  -100 +i, screen_height -low_mar, WHITE);
+	}	
 	for(;;);
 }
