bb9aa326982aa7d1bc0dc9ea2ba80359d1906747
pcs <pcs@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-02-18 11:29:11 +0000

    clean up. i will revisit it
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@632 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- lib/libc/main.c
+++ lib/libc/main.c
@@ -6,8 +6,6 @@ int main() {
 }
 
 
-#ifdef CONFIG_BFIN
-
 /* We've got to provide an entry point that doesn't stuff around with p5 like
  * C routines tend to do.
  */
@@ -18,21 +16,4 @@ asm(    ".global lib_main\n\t"
         "jump.l _main;\n"
         ".size   libmain, .-libmain"
 );
-
-#else
-
-/* We've got to provide an entry point that doesn't stuff around with a5 like
- * C routines tend to do.  We must also setup a5 from d5 which won't point to
- * this libraries data segment but from which it can be obtained.
- */
-asm(	".globl lib_main\n\t"
-	".type lib_main,@function\n"
-	"lib_main:\n\t"
-	"move.l %d5, %a5\n\t"
-	"bra.w main\n"
-	".L__end_lib_main__:\n\t"
-	".size lib_main,.L__end_lib_main__-libmain"
-);
-
-#endif
 
