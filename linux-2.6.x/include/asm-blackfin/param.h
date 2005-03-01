7c2593ee9860472eb3ca2b70ee90b33f97c352dc
lgsoft <lgsoft@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-03-01 05:55:50 +0000

    cleanup
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@667 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/param.h
+++ linux-2.6.x/include/asm-blackfin/param.h
@@ -1,23 +1,17 @@
 #ifndef _BFINNOMMU_PARAM_H
 #define _BFINNOMMU_PARAM_H
 
-#include <linux/config.h>
-
-#ifndef HZ
-#ifdef CONFIG_BFIN
-#define HZ 100		/* need changes accordingly	*/
-#endif
-#endif
 #ifdef __KERNEL__
+#define HZ 		100
 #define	USER_HZ		HZ
 #define	CLOCKS_PER_SEC	(USER_HZ)
 #endif
 
-#define EXEC_PAGESIZE	4096
-
-#ifndef NGROUPS
-#define NGROUPS		32
+#ifndef HZ
+#define HZ 100
 #endif
+
+#define EXEC_PAGESIZE	4096
 
 #ifndef NOGROUP
 #define NOGROUP		(-1)
