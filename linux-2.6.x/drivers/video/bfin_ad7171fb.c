7e71c96fd3798fab3aca3789c35bae5c572471cd
pcs <pcs@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-04-05 13:23:01 +0000

    /dev/fb driver is fixed. /dev/fb driver is fixed, though still there are some issued that need to addressed. To use this driver keep RAM size as 64 only.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@754 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/video/bfin_ad7171fb.c
+++ linux-2.6.x/drivers/video/bfin_ad7171fb.c
@@ -34,10 +34,8 @@
 #define CONFIG_VIDEO_BLACKFIN_PPI_IRQ_ERR IRQ_DMA_ERROR
 char *rgb_buffer = 0 ;
 char *ycrcb_buffer = 0 ;
+int id ;
 
-static void vga16fb_pan_var(struct fb_info *info, 
-			    struct fb_var_screeninfo *var);
-static void vga16fb_update_fix(struct fb_info *info);
 static int bfin_ad7171_fb_open(struct fb_info *info, int user);
 static int bfin_ad7171_fb_release(struct fb_info *info, int user);
 static int bfin_ad7171_fb_check_var(struct fb_var_screeninfo *var,
@@ -49,6 +47,12 @@ static void bfin_ad7171_fb_fillrect(struct fb_info *info, const struct fb_fillre
 static void bfin_ad7171_fb_imageblit(struct fb_info *info, const struct fb_image *image);
 static int bfin_ad7171_fb_blank(int blank, struct fb_info *info);
 static int bfin_mmap(struct fb_info *info, struct file *file, struct vm_area_struct * vma);
+
+extern void _NtscVideoOutFrameBuffInit(void *, void *);
+extern void _NtscVideoOutBuffUpdate(void *, void *);
+extern void _Flash_Setup_ADV_Reset(void);
+extern void _config_dma(void *buffer);
+extern void _config_ppi(void);
 
 /* --------------------------------------------------------------------- */
 
@@ -121,10 +125,8 @@ ppi_handler(int irq,
   return IRQ_HANDLED;
 }
 
-
-
 void 
-  rgbsetup(int *rgb)			//This function sets up colour patter in ycrcb buffer
+rgbsetup(int *rgb)			//This function sets up colour patter in ycrcb buffer
 { 
 	int i=0 , j, black =0, yellow = 0x00ff00ff, red = 0x000000ff, blue = 0xff00ff00, green = 0x00ff0000, offset = 37800;
 
@@ -133,7 +135,7 @@ void
 	for(j=1; j<=105;){
 		for(i=0; i<360; i++)
 		{
-			rgb[offset * 0 + i + j * 360] = yellow;			// j = line no., i = pixel no. in that particular line
+			rgb[offset * 0 + i + j * 360] = yellow;		// j = line no., i = pixel no. in that particular line
 			rgb[offset * 1 + i + j * 360] = black;			
 			rgb[offset * 2 + i + j * 360] = red;
 			rgb[offset * 3 + i + j * 360] = blue;
@@ -142,13 +144,14 @@ void
 		j += 1;
  	}                 
 }                                                                               
+
 static int 
 bfin_mmap(struct fb_info *info, struct file *file, struct vm_area_struct * vma)
 {
   /* we really dont need any map ... not sure how the smem_start will
      end up in the kernel
   */
-	return(rgb_buffer) ;
+	return((int)rgb_buffer) ;
 }
 
 /* copied from vga16, do nothing setup! */
@@ -169,128 +172,98 @@ static void timerfunction(unsigned long ptr);
 static void timer_setup(void);
 int __init bfin_ad7171_fb_init(void)
 {
-	int i;
-	int ret;
+	int ret = 0;
 
-printk("bfin_ad7171_fb: initializing:\n");
+	printk("bfin_ad7171_fb: initializing:\n");
 	ycrcb_buffer = (char *)kmalloc(BFIN_FB_YCRCB_LEN, GFP_KERNEL);
 	rgb_buffer = (char *)kmalloc(BFIN_FB_PHYS_LEN , GFP_KERNEL);
 
 	bfin_ad7171_fb.screen_base = (void *)rgb_buffer;
-	bfin_ad7171_fb_fix.smem_start = (void *)rgb_buffer;
+	bfin_ad7171_fb_fix.smem_start = (int)rgb_buffer;
 	if (!bfin_ad7171_fb.screen_base) {
 		printk("bfin_ad7171_fb: unable to map device\n");
 		ret = -ENOMEM;
-		goto err_ioremap;
 	}
-	printk("bfin_ad7171_fb: mapped to 0x%p\n");
-
 	bfin_ad7171_fb_defined.red.length   = 8;
 	bfin_ad7171_fb_defined.green.length = 8;
 	bfin_ad7171_fb_defined.blue.length  = 8;	
 
 	bfin_ad7171_fb.fbops = &bfin_ad7171_fb_ops;
 	bfin_ad7171_fb.var = bfin_ad7171_fb_defined;
-
 	/* our physical memory is dynamically allocated */
-	bfin_ad7171_fb_fix.smem_start	= BFIN_FB_PHYS;
+	bfin_ad7171_fb_fix.smem_start	= (int)BFIN_FB_PHYS;
 	bfin_ad7171_fb.fix = bfin_ad7171_fb_fix;
 	bfin_ad7171_fb.par = &bfin_par;
 	bfin_ad7171_fb.flags = FBINFO_DEFAULT;
 
-#if 0 /* TODO : do we need a colormap? */
-	i = (bfin_ad7171_fb_defined.bits_per_pixel == 8) ? 256 : 16;
-	ret = fb_alloc_cmap(&bfin_ad7171_fb.cmap, i, 0);
-	if (ret) {
-		printk(KERN_ERR "bfin_ad7171_fb: unable to allocate colormap\n");
-		ret = -ENOMEM;
-		goto err_alloc_cmap;
-	}
-#endif
-
 	if (register_framebuffer(&bfin_ad7171_fb) < 0) {
 		printk(KERN_ERR "bfin_ad7171_fb: unable to register framebuffer\n");
 		ret = -EINVAL;
-		goto err_check_var;
 	}
-
 	printk(KERN_INFO "fb%d: %s frame buffer device\n",
 	       bfin_ad7171_fb.node, bfin_ad7171_fb.fix.id);
-
-	return 0;
- err_check_var:
-	
- err_alloc_cmap:
-	iounmap(bfin_ad7171_fb.screen_base);
- err_ioremap:
 	return ret;
 }
 static void __attribute((section(".text.l1")))
 timerfunction(unsigned long ptr)
 {
-//	_NtscVideoOutFrameBuffInit(ycrcb_buffer, rgb_buffer);
 	_NtscVideoOutBuffUpdate(ycrcb_buffer, rgb_buffer);
 	timer_setup();
         add_timer(&buffer_swapping_timer) ;
 }
 
-
 void
 timer_setup(void)
 {
-
         /*** Initialize the timer structure***/
-
         init_timer(&buffer_swapping_timer) ;
         buffer_swapping_timer.function = timerfunction ;
         buffer_swapping_timer.expires = jiffies + HZ*1 ;
-
         /***Initialisation ends***/
-
-}
-
-
-static void vga16fb_pan_var(struct fb_info *info, 
-			    struct fb_var_screeninfo *var)
-{
-printk("vga16fb_pan_var called ... not implemented\n");
-
-}
-
-static void vga16fb_update_fix(struct fb_info *info)
-{
-printk("vga16fb_update_fix called ... not implemented\n");
 }
 
 static int bfin_ad7171_fb_open(struct fb_info *info, int user)
 {
-/*******It is needed to check the status of devices and do it accordingly...but for the current purpose lets skip it.*********/
-        if( request_irq(CONFIG_VIDEO_BLACKFIN_PPI_IRQ, &ppi_handler, SA_SHIRQ, "PPI Data", NULL ) ){
+        if( request_irq(CONFIG_VIDEO_BLACKFIN_PPI_IRQ, &ppi_handler, SA_SHIRQ, "PPI Data", &id ) ){
                 printk( KERN_ERR "Unable to allocate ppi IRQ %d\n", CONFIG_VIDEO_BLACKFIN_PPI_IRQ);
                 return -ENODEV;
         }
-	rgbsetup(rgb_buffer);
-printk("ycrcb_buffer = %x\n",ycrcb_buffer);
-printk("rgb_buffer = %x\n",rgb_buffer) ;
-	_NtscVideoOutFrameBuffInit(ycrcb_buffer, rgb_buffer);
-	
+	bfin_ad7171_fb.screen_base = (void *)rgb_buffer;
+	bfin_ad7171_fb_fix.smem_start = (int)rgb_buffer;
+	if (!bfin_ad7171_fb.screen_base) {
+		printk("bfin_ad7171_fb: unable to map device\n");
+		return -ENOMEM;
+	}
 	_Flash_Setup_ADV_Reset() ;
 	_config_ppi() ;
 	_config_dma(ycrcb_buffer) ;
+	rgbsetup((int *)rgb_buffer);
+	printk("ycrcb_buffer = %p\n",ycrcb_buffer);
+	printk("rgb_buffer = %p\n",rgb_buffer) ;
+
+	_NtscVideoOutFrameBuffInit(ycrcb_buffer, rgb_buffer);
 	timer_setup() ;
 	enable_irq(CONFIG_VIDEO_BLACKFIN_PPI_IRQ);
         // enable the dma
         *pDMA0_CONFIG |= 1;
         *pPPI_CONTROL |= 1;
+printk("PPI transfer initialized\n");
 	add_timer(&buffer_swapping_timer) ;
 	return 0;
 }
 
 static int bfin_ad7171_fb_release(struct fb_info *info, int user)
 {
-	//disable DMA
-	*pPPI_CONTROL &= 0;
-	*pDMA0_CONFIG &= 0;
+	//disable PPI 
+	*pPPI_CONTROL &= 0xfffe;
+	//Reset PPI
+	*pPPI_CONTROL &= 0x0000;
+	//Disable DMA
+	*pDMA0_CONFIG &= 0xfffe;
+	//Reset DMA0
+	*pDMA0_CONFIG &= 0x0000;
+
+	//Reset DMA0
 	//Release the interrupt.
 	disable_irq(CONFIG_VIDEO_BLACKFIN_PPI_IRQ);
 	del_timer_sync(&buffer_swapping_timer) ;
@@ -302,13 +275,13 @@ static int bfin_ad7171_fb_check_var(struct fb_var_screeninfo *var,
 			     struct fb_info *info)
 {
 	printk("bfin_ad7171_fb Variables checked\n") ;
-	return 0;
+	return -EINVAL;
 }
 
 static int bfin_ad7171_fb_set_par(struct fb_info *info)
 {
-	printk("Parameters are set\n") ; 
-	return 0;
+	printk("bfin_ad7171_fb_set_par called not implemented\n") ; 
+	return -EINVAL;
 }
 
 
@@ -316,65 +289,29 @@ static int bfin_ad7171_fb_pan_display(struct fb_var_screeninfo *var,
 			       struct fb_info *info) 
 {
 	printk("bfin_ad7171_fb_pan_display called ... not implemented\n");
-	return 0;
+	return -EINVAL;
 }
 
 /* 0 unblank, 1 blank, 2 no vsync, 3 no hsync, 4 off */
 static int bfin_ad7171_fb_blank(int blank, struct fb_info *info)
 {
-	struct bfin_ad7171_fb_par *par = (struct bfin_ad7171_fb_par *) info->par;
 printk("bfin_ad7171_fb_blank called ... not implemented\n");
-return 0;
-
-	switch (blank) {
-	case 0:				/* Unblank */
-		break;
-	case 1:				/* blank */
-		break;
-	default:			/* VESA blanking */
-		break;
-	}
-	return 0;
+	return -EINVAL;
 }
 
 static void bfin_ad7171_fb_fillrect(struct fb_info *info, const struct fb_fillrect *rect)
 {
-	int vxres, vyres;
-	int x2, y2, width;
-
 printk("bfin_ad7171_fb_fillrect called ... not implemented\n");
-return;
-	vxres = info->var.xres_virtual;
-	vyres = info->var.yres_virtual;
-
-	if (!rect->width || !rect->height || rect->dx > vxres || rect->dy > vyres)
-		return;
-
-	/* We could use hardware clipping but on many cards you get around
-	 * hardware clipping by writing to framebuffer directly. */
-
-	x2 = rect->dx + rect->width;
-	y2 = rect->dy + rect->height;
-	x2 = x2 < vxres ? x2 : vxres;
-	y2 = y2 < vyres ? y2 : vyres;
-	width = x2 - rect->dx;
-
 }
 
 static void bfin_ad7171_fb_imageblit(struct fb_info *info, const struct fb_image *image)
 {
 printk("bfin_ad7171_fb_imageblit called ... not implemented\n");
-/*
-	if (image->depth == 1)
-	else if (image->depth <= info->var.bits_per_pixel)
-*/
 }
 
 static void __exit bfin_ad7171_fb_exit(void)
 {
     unregister_framebuffer(&bfin_ad7171_fb);
-    //iounmap(bfin_ad7171_fb.screen_base);
-    //fb_dealloc_cmap(&bfin_ad7171_fb.cmap);
 }
 
 #ifdef MODULE
