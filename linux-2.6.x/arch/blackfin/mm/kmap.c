c600f6ff06c99e7a16e40b7a1808682f077beefd
aubrey <aubrey@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-06-02 01:19:56 +0000

    Fix bug #598, _acces_ok broken in uClinux-dist/linux-2.6.x/include/asm-bfinnommu/uaccess.h
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@873 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/arch/blackfin/mm/kmap.c
+++ linux-2.6.x/arch/blackfin/mm/kmap.c
@@ -41,3 +41,22 @@ void __iounmap(void *addr, unsigned long size)
 void kernel_set_cachemode(void *addr, unsigned long size, int cmode)
 {
 }
+
+int is_in_rom(unsigned long addr)
+{
+        extern unsigned long _ramstart, _ramend;
+                                                                                
+        /*
+         *      What we are really trying to do is determine if addr is
+         *      in an allocated kernel memory region. If not then assume
+         *      we cannot free it or otherwise de-allocate it. Ideally
+         *      we could restrict this to really being in a ROM or flash,
+         *      but that would need to be done on a board by board basis,
+         *      not globally.
+         */
+        if ((addr < _ramstart) || (addr >= _ramend))
+                return(1);
+                                                                                
+        /* Default case, not in ROM */
+        return(0);
+}
