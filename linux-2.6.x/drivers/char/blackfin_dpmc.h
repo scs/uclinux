23bba177b4d0abd649bac28cf755beae8a7a99d0
hennerich <hennerich@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-06-03 12:32:18 +0000

    Remove board dependencies from DPMC device driver
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@878 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/char/blackfin_dpmc.h
+++ linux-2.6.x/drivers/char/blackfin_dpmc.h
@@ -22,22 +22,11 @@ void disable_wdog_timer(void);
 #define FLAG_CSEL	0x0
 #define FLAG_SSEL	0x1
 
-#ifdef CONFIG_EZKIT
-	#define MAX_VCO		600000000
-#endif
-#ifdef CONFIG_BLKFIN_STAMP
-	#define MAX_VCO		650000000
-#endif
-
+#define MAX_VCO		650000000
 #define MIN_VCO		56000000	/* VCO minimum should get set to 50 only */
-#define MAX_SCLK	133000000
 
-#ifdef CONFIG_EZKIT
+#define MAX_SCLK	133000000
 #define MIN_SCLK	27000000
-#endif
-#ifdef CONFIG_BLKFIN_STAMP
-#define MIN_SCLK	27000000 /* For now lets keep it at 22, actually it shld be 11 */
-#endif
 
 #define INTER_FREQ	MIN_SCLK*5
 
