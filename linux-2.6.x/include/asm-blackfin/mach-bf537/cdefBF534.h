f2d6c01fce87b4c994b788f28ed79ebb102ad740
hennerich <hennerich@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-06-13 10:49:10 +0000

    More Cleanup: Restructure Blackfin specific include and source folders, as well as device driver includes.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@896 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/mach-bf537/cdefBF534.h
+++ linux-2.6.x/include/asm-blackfin/mach-bf537/cdefBF534.h
@@ -1,6 +1,6 @@
 /************************************************************************
  *
- * $id$
+ * $Id$
  *
  * This file is subject to the terms and conditions of the GNU Public 
  * License. See the file "COPYING" in the main directory of this archive
