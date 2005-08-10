f88a085b113d82f3af68804bdc930b76b8004a7e
hennerich <hennerich@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-08-10 12:28:12 +0000

    Fix BogoMIPS calculation
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1098 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/delay.h
+++ linux-2.6.x/include/asm-blackfin/delay.h
@@ -3,11 +3,24 @@
 
 static __inline__ void __delay(unsigned long loops)
 {
-	__asm__ __volatile__ (	"1:\t cc = %0 == 0;\n\t"
-				"%0 += -1;\n\t"
-				"if ! cc jump 1b;\n"
-				: "=d" (loops) 
-				: "0" (loops));
+
+/* FIXME: Currently the assembler doesn't recognize Loop Register Clobbers, 
+   uncomment this as soon those are implemented */
+//	__asm__ __volatile__ (	"\t LSETUP (1,1) LC0= %0\n\t"
+//				"l:\t NOP;\n\t"
+//				: :"a" (loops) 
+//				: "LT0","LB0","LC0");
+
+	__asm__ __volatile__ ( "[--SP] = LC0;\n\t"
+				"[--SP] = LT0;\n\t"
+				"[--SP] = LB0;\n\t"
+				"LSETUP (1f,1f) LC0 = %0;\n\t"
+				"1:\t NOP;\n\t"
+				"LB0 = [SP++];\n\t"
+				"LT0 = [SP++];\n\t"
+				"LC0 = [SP++];\n"
+				: 
+				:"a" (loops));
 }
 
 #include <linux/param.h> /* needed for HZ */
