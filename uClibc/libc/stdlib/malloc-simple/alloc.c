850139176ef3923de30f0c66fc95af3b83fde5ef
magicyang <magicyang@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-08-16 07:53:33 +0000

    *** empty log message ***
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1140 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- uClibc/libc/stdlib/malloc-simple/alloc.c
+++ uClibc/libc/stdlib/malloc-simple/alloc.c
@@ -1,12 +1,11 @@
 /* alloc.c
  *
- * Written by Erik Andersen <andersee@codepoet.org>
+ * Written by Erik Andersen <andersee@debian.org>
  * LGPLv2
  *
  * Parts of the memalign code were stolen from malloc-930716.
  */
 
-#define _GNU_SOURCE
 #include <features.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -20,29 +19,31 @@
 #ifdef L_malloc
 void *malloc(size_t size)
 {
-	void *result;
+    void *result;
 
-	if (unlikely(size == 0)) {
+    if (unlikely(size == 0)) {
 #if defined(__MALLOC_GLIBC_COMPAT__)
-		size++;
+	size++;
 #else
-		/* Some programs will call malloc (0).  Lets be strict and return NULL */
-		return 0;
+	/* Some programs will call malloc (0).  Lets be strict and return NULL */
+	return 0;
 #endif
-	}
+    }
 
-#ifdef __ARCH_HAS_MMU__
-# define MMAP_FLAGS MAP_PRIVATE | MAP_ANONYMOUS
+#ifdef __UCLIBC_HAS_MMU__
+    result = mmap((void *) 0, size + sizeof(size_t), PROT_READ | PROT_WRITE,
+	    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (result == MAP_FAILED)
+	return 0;
+    * (size_t *) result = size;
+    return(result + sizeof(size_t));
 #else
-# define MMAP_FLAGS MAP_SHARED | MAP_ANONYMOUS
+    result = mmap((void *) 0, size, PROT_READ | PROT_WRITE,
+	    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (result == MAP_FAILED)
+	return 0;
+    return(result);
 #endif
-
-	result = mmap((void *) 0, size + sizeof(size_t), PROT_READ | PROT_WRITE,
-	              MMAP_FLAGS, 0, 0);
-	if (result == MAP_FAILED)
-		return 0;
-	* (size_t *) result = size;
-	return(result + sizeof(size_t));
 }
 #endif
 
@@ -74,21 +75,27 @@ void * calloc(size_t nmemb, size_t lsize)
 #ifdef L_realloc
 void *realloc(void *ptr, size_t size)
 {
-	void *newptr = NULL;
+    void *newptr = NULL;
 
-	if (!ptr)
-		return malloc(size);
-	if (!size) {
-		free(ptr);
-		return malloc(0);
-	}
+    if (!ptr)
+	return malloc(size);
+    if (!size) {
+	free(ptr);
+	return malloc(0);
+    }
 
-	newptr = malloc(size);
-	if (newptr) {
-		memcpy(newptr, ptr, *((size_t *) (ptr - sizeof(size_t))));
-		free(ptr);
-	}
-	return newptr;
+    newptr = malloc(size);
+    if (newptr) {
+	memcpy(newptr, ptr,
+#ifdef __UCLIBC_HAS_MMU__
+		*((size_t *) (ptr - sizeof(size_t)))
+#else
+		size
+#endif
+	      );
+	free(ptr);
+    }
+    return newptr;
 }
 #endif
 
@@ -96,23 +103,28 @@ void *realloc(void *ptr, size_t size)
 extern int weak_function __libc_free_aligned(void *ptr);
 void free(void *ptr)
 {
-	if (unlikely(ptr == NULL))
-		return;
-	if (unlikely(__libc_free_aligned != NULL)) {
-		if (__libc_free_aligned(ptr))
-			return;
+    if (ptr == NULL)
+	return;
+    if (unlikely(__libc_free_aligned!=NULL)) {
+	if (__libc_free_aligned(ptr)) {
+	    return;
 	}
-	ptr -= sizeof(size_t);
-	munmap(ptr, * (size_t *) ptr + sizeof(size_t));
+    }
+#ifdef __UCLIBC_HAS_MMU__
+    ptr -= sizeof(size_t);
+    munmap(ptr, * (size_t *) ptr + sizeof(size_t));
+#else
+    munmap(ptr, 0);
+#endif
 }
 #endif
 
 #ifdef L_memalign
 #ifdef __UCLIBC_HAS_THREADS__
 #include <pthread.h>
-pthread_mutex_t __malloc_lock = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
-# define LOCK	__pthread_mutex_lock(&__malloc_lock)
-# define UNLOCK	__pthread_mutex_unlock(&__malloc_lock);
+static pthread_mutex_t __malloclock;
+# define LOCK	__pthread_mutex_lock(&__malloclock)
+# define UNLOCK	__pthread_mutex_unlock(&__malloclock);
 #else
 # define LOCK
 # define UNLOCK
@@ -121,67 +133,73 @@ pthread_mutex_t __malloc_lock = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
 /* List of blocks allocated with memalign or valloc */
 struct alignlist
 {
-	struct alignlist *next;
-	__ptr_t aligned;	/* The address that memaligned returned.  */
-	__ptr_t exact;	/* The address that malloc returned.  */
+    struct alignlist *next;
+    __ptr_t aligned;	/* The address that memaligned returned.  */
+    __ptr_t exact;	/* The address that malloc returned.  */
 };
 struct alignlist *_aligned_blocks;
 
 /* Return memory to the heap. */
 int __libc_free_aligned(void *ptr)
 {
-	struct alignlist *l;
+    struct alignlist *l;
 
-	if (ptr == NULL)
-		return 0;
-
-	LOCK;
-	for (l = _aligned_blocks; l != NULL; l = l->next) {
-		if (l->aligned == ptr) {
-			/* Mark the block as free */
-			l->aligned = NULL;
-			ptr = l->exact;
-			ptr -= sizeof(size_t);
-			munmap(ptr, * (size_t *) ptr + sizeof(size_t));
-			return 1;
-		}
-	}
-	UNLOCK;
+    if (ptr == NULL)
 	return 0;
+
+    LOCK;
+    for (l = _aligned_blocks; l != NULL; l = l->next) {
+	if (l->aligned == ptr) {
+	    /* Mark the block as free */
+	    l->aligned = NULL;
+	    ptr = l->exact;
+#ifdef __UCLIBC_HAS_MMU__
+	    ptr -= sizeof(size_t);
+	    munmap(ptr, * (size_t *) ptr + sizeof(size_t));
+#else
+	    munmap(ptr, 0);
+#endif
+	    return 1;
+	}
+    }
+    UNLOCK;
+    return 0;
 }
 void * memalign (size_t alignment, size_t size)
 {
-	void * result;
-	unsigned long int adj;
+    void * result;
+    unsigned long int adj;
 
-	result = malloc (size + alignment - 1);
-	if (result == NULL)
-		return NULL;
-
-	adj = (unsigned long int) ((unsigned long int) ((char *) result -
-	      (char *) NULL)) % alignment;
-	if (adj != 0) {
-		struct alignlist *l;
-		LOCK;
-		for (l = _aligned_blocks; l != NULL; l = l->next)
-			if (l->aligned == NULL)
-				/* This slot is free.  Use it.  */
-				break;
-		if (l == NULL) {
-			l = (struct alignlist *) malloc (sizeof (struct alignlist));
-			if (l == NULL) {
-				free(result);
-				UNLOCK;
-				return NULL;
-			}
-			l->next = _aligned_blocks;
-			_aligned_blocks = l;
-		}
-		l->exact = result;
-		result = l->aligned = (char *) result + alignment - adj;
+    result = malloc (size + alignment - 1);
+    if (result == NULL)
+	return NULL;
+    adj = (unsigned long int) ((unsigned long int) ((char *) result -
+		(char *) NULL)) % alignment;
+    if (adj != 0)
+    {
+	struct alignlist *l;
+	LOCK;
+	for (l = _aligned_blocks; l != NULL; l = l->next)
+	    if (l->aligned == NULL)
+		/* This slot is free.  Use it.  */
+		break;
+	if (l == NULL)
+	{
+	    l = (struct alignlist *) malloc (sizeof (struct alignlist));
+	    if (l == NULL) {
+		free(result);
 		UNLOCK;
+		return NULL;
+	    }
+	    l->next = _aligned_blocks;
+	    _aligned_blocks = l;
 	}
+	l->exact = result;
+	result = l->aligned = (char *) result + alignment - adj;
+	UNLOCK;
+    }
 
-	return result;
+    return result;
 }
 #endif
+
