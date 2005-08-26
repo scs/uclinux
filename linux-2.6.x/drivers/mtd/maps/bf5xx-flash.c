ad285405d726f87120776933378c34d7223b5a86
hennerich <hennerich@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-08-26 09:13:11 +0000

    Initial checkin of a more generic bf5xx flash driver map. This driver is currently untested, and will hopefully obsolete bf533-flash.c, with all its weird stuff
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1177 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ linux-2.6.x/drivers/mtd/maps/bf5xx-flash.c
@@ -0,0 +1,244 @@
+/*
+ * Flash memory access on BlackFin BF5xx based devices
+ * 
+ * (C) 2000 Nicolas Pitre <nico@cam.org>
+ * (C) 2004 LG Soft India
+ * 
+ */
+
+#include <linux/config.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#include <linux/mtd/mtd.h>
+#include <linux/mtd/map.h>
+#include <linux/mtd/partitions.h>
+
+#include <asm/blackfin.h>
+#include <asm/io.h>
+
+#ifndef CONFIG_BFIN
+#error This is for BlackFin BF5xx boards only
+#endif
+
+#define CONFIG_BFIN_SHARED_FLASH_ENET /* TODO: Remove this as soon a upper level kconfig option is available */
+
+#define BFIN_FLASH_AMBCTL0VAL	((CONFIG_BFIN_FLASH_BANK_1 << 16) | CONFIG_BFIN_FLASH_BANK_0)
+#define BFIN_FLASH_AMBCTL1VAL	((CONFIG_BFIN_FLASH_BANK_3 << 16) | CONFIG_BFIN_FLASH_BANK_2) 
+
+struct flash_save {
+#if defined(CONFIG_BFIN_SHARED_FLASH_ENET)
+    u32 ambctl0;
+    u32 ambctl1;
+#endif
+    unsigned long flags;
+} ;
+
+#if defined(CONFIG_BFIN_SHARED_FLASH_ENET)
+static inline void switch_to_flash(struct flash_save *save)
+{
+	local_irq_save(save->flags);
+
+	*pFIO_FLAG_C	= (PF1 | PF0);
+
+	__builtin_bfin_ssync();
+
+
+	save->ambctl0	= *pEBIU_AMBCTL0;
+	save->ambctl1	= *pEBIU_AMBCTL1;
+	*pEBIU_AMBCTL0 = BFIN_FLASH_AMBCTL0VAL;
+	*pEBIU_AMBCTL1 = BFIN_FLASH_AMBCTL1VAL;
+	__builtin_bfin_ssync();
+}
+#else
+static static inline void switch_to_flash(struct flash_save *save) {}
+#endif
+
+#if defined(CONFIG_BFIN_SHARED_FLASH_ENET)
+static inline void switch_back(struct flash_save *save)
+{
+
+	*pEBIU_AMBCTL0	= save->ambctl0;
+	*pEBIU_AMBCTL1	= save->ambctl1;
+	__builtin_bfin_ssync();
+
+	*pFIO_FLAG_S	= (PF1 | PF0);	
+
+	local_irq_restore(save->flags);
+}
+#else
+static inline void switch_back(struct flash_save *save) {}
+#endif
+
+#if defined(CONFIG_BFIN_SHARED_FLASH_ENET)
+static inline void setup_pfpins(void)
+{
+	*pFIO_INEN		&= ~(PF1 | PF0);	
+	*pFIO_DIR 		|=  (PF1 | PF0);
+}
+#else
+static inline void setup_pfpins(void) {}
+#endif
+
+
+static map_word bf5xx_read(struct map_info *map, unsigned long ofs)
+{
+	int nValue = 0x0;
+	map_word test;
+
+	struct flash_save save;
+
+	switch_to_flash(&save);
+	__builtin_bfin_ssync();
+        nValue = readw(CONFIG_EBIU_FLASH_BASE + ofs);
+	__builtin_bfin_ssync();
+	switch_back(&save);
+
+	
+	test.x[0]=(__u16)nValue;
+	return test;	
+}
+
+static void bf5xx_copy_from(struct map_info *map, void *to, unsigned long from, ssize_t len)
+{
+	unsigned long i;
+	map_word test;
+
+	for (i = 0; i < len/2*2; i += 2)	{
+
+		test = bf5xx_read(map,from+i);
+		*((u16*)(to + i)) = test.x[0];
+	}
+	if (len & 0x01) {
+
+		test = bf5xx_read(map, from + i);
+		*((u8*)(to + i)) = (u8)test.x[0];
+	}
+}
+
+static void bf5xx_write(struct map_info *map, map_word d1, unsigned long ofs)
+{
+
+	__u16 d;
+	struct flash_save save;
+
+	d = (__u16)d1.x[0];	
+
+	switch_to_flash(&save);
+
+		__builtin_bfin_ssync();
+		  writew(d, CONFIG_EBIU_FLASH_BASE + ofs);		
+		__builtin_bfin_ssync();
+
+	switch_back(&save);
+
+}
+
+static void bf5xx_copy_to(struct map_info *map, unsigned long to, const void *from, ssize_t len)
+{
+
+	struct flash_save save;
+
+	switch_to_flash(&save);
+      
+      memcpy((void *)(CONFIG_EBIU_FLASH_BASE + to), from, len);
+	
+	switch_back(&save);
+}
+
+static struct map_info bf5xx_map = {
+	name:    	"BF5xx flash",
+	0x400000,
+	0x20000000,
+	0x20000000,
+	NULL,		
+	read:		bf5xx_read,
+	copy_from:	bf5xx_copy_from,
+	write:		bf5xx_write,
+	copy_to:	bf5xx_copy_to
+};
+
+
+/*
+ * Here are partition information for all known BlackFin-based devices.
+ * See include/linux/mtd/partitions.h for definition of the mtd_partition
+ * structure.
+ * 
+ * The *_max_flash_size is the maximum possible mapped flash size which
+ * is not necessarily the actual flash size.  It must correspond to the 
+ * value specified in the mapping definition defined by the
+ * "struct map_desc *_io_desc" for the corresponding machine.
+ */
+
+
+static unsigned long bf5xx_max_flash_size = CONFIG_BFIN_FLASH_SIZE;
+
+static struct mtd_partition bf5xx_partitions[] = {
+	{
+		name: "bootloader",
+		size: 0x00080000,
+		offset: 0,
+		mask_flags: MTD_CAP_ROM
+	},{
+		name: "File system image",
+		size: 0x300000,
+		offset: 0x80000
+	},{
+		name: "64K area ;)", 
+		size: 0x10000,
+		offset: 0x00380000,
+	}	
+};
+
+#define NB_OF(x)  (sizeof(x)/sizeof(x[0]))
+
+
+static struct mtd_info *mymtd;
+
+int __init bf5xx_mtd_init(void)
+{
+	struct mtd_partition *parts;
+	int nb_parts = 0;
+	char *part_type;
+
+	bf5xx_map.bankwidth = 2;
+	bf5xx_map.size = bf5xx_max_flash_size;
+
+	setup_pfpins();
+
+	printk(KERN_NOTICE "BF5xx flash: probing %d-bit flash bus\n", bf5xx_map.bankwidth*8);
+	mymtd = do_map_probe("stm_flash", &bf5xx_map);
+	if (!mymtd)
+		return -ENXIO;
+
+	/*
+	 * Static partition definition selection
+	 */
+	part_type = "static";
+#ifdef CONFIG_BFIN
+	parts = bf5xx_partitions;
+	nb_parts = NB_OF(bf5xx_partitions);
+#endif
+
+	if (nb_parts == 0) {
+		printk(KERN_NOTICE "BF5xx flash: no partition info available, registering whole flash at once\n");
+		add_mtd_device(mymtd);
+	} else {
+		printk(KERN_NOTICE "Using %s partition definition\n", part_type);
+		add_mtd_partitions(mymtd, parts, nb_parts);
+	}
+	return 0;
+}
+
+static void __exit bf5xx_mtd_cleanup(void)
+{
+	if (mymtd) {
+		del_mtd_partitions(mymtd);
+		map_destroy(mymtd);
+	}
+}
+
+module_init(bf5xx_mtd_init);
+module_exit(bf5xx_mtd_cleanup);
