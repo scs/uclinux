95c70271b44400cfa730dee8efc3423619da16ee
sonicz <sonicz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-09-01 06:50:42 +0000

    Rollback the change to bug 859. The setting to the asynchronous memory CPLB is correct.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1204 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/arch/blackfin/kernel/setup.c
+++ linux-2.6.x/arch/blackfin/kernel/setup.c
@@ -276,12 +276,7 @@ static void __init generate_cpl_tables(void)
 	pos = fill_cpl_tables(dpdt_table, pos, SIZE_4M, RAM_END - SIZE_4M, SIZE_4M, SDRAM_DGENERIC);
 	pos = fill_cpl_tables(dpdt_table, pos, RAM_END - SIZE_4M, RAM_END - SIZE_1M, SIZE_1M, SDRAM_DGENERIC);
 	pos = fill_cpl_tables(dpdt_table, pos, RAM_END - SIZE_1M, RAM_END, SIZE_1M, SDRAM_DNON_CHBL);
-	pos = fill_cpl_tables(dpdt_table, pos, ASYNC_BANK0_BASE, ASYNC_BANK2_BASE + ASYNC_BANK2_SIZE, ASYNC_BANK0_SIZE, SDRAM_EBIU);
-#if defined(UNCACHED_ASYNCMEM_BLANK3)
-	pos = fill_cpl_tables(dpdt_table, pos, ASYNC_BANK3_BASE, ASYNC_BANK3_BASE + ASYNC_BANK3_SIZE, ASYNC_BANK3_SIZE, SDRAM_DNON_CHBL);
-#else
-	pos = fill_cpl_tables(dpdt_table, pos, ASYNC_BANK3_BASE, ASYNC_BANK3_BASE + ASYNC_BANK3_SIZE, ASYNC_BANK3_SIZE, SDRAM_EBIU);
-#endif
+	pos = fill_cpl_tables(dpdt_table, pos, ASYNC_BANK0_BASE, ASYNC_BANK3_BASE + ASYNC_BANK3_SIZE, ASYNC_BANK0_SIZE, SDRAM_EBIU);
 	pos = fill_cpl_tables(dpdt_table, pos, L1_DATA_A_START, L1_DATA_A_START + L1_DATA_A_LENGTH, SIZE_4K, L1_DMEMORY);
 #if !defined(CONFIG_BF531) 
 	pos = fill_cpl_tables(dpdt_table, pos, L1_DATA_B_START, L1_DATA_B_START + L1_DATA_B_LENGTH, SIZE_4K, L1_DMEMORY);
