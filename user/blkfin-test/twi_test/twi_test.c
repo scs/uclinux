a1e439db1d06bcd104975078a252eeae00009b5a
sonicz <sonicz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-09-06 08:25:27 +0000

    Add twi test suite into user/blackfin-test
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1242 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/blkfin-test/twi_test/twi_test.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1536
+
+int main(int argc, char *argv[])
+{
+  int i2c_dev;
+  int result;
+  int buf[BUF_SIZE];
+  printf("----------- Test TWI Driver ------------\n");
+  i2c_dev = open("/dev/i2c-0", O_RDWR, 0);
+
+  if(i2c_dev == -1) 
+    printf("Unable to open I2C TWI device.\nTest... [FAIL]\n"); 
+  else{
+    printf("Open device complete\n");
+    result = write(i2c_dev, buf, 10);
+    if(result>=0)
+      printf("write... [Pass]\n");
+    else
+      printf("write... [Fail]\n");
+
+    result = read(i2c_dev, buf, BUF_SIZE);
+    if(result>=0)
+      printf("read... [Pass]\n");
+    else
+      printf("read... [Fail]\n");
+
+    close(i2c_dev);
+  }
+  return 0;
+}
+
