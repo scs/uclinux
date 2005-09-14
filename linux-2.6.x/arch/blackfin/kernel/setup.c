dd8d117f772e7a91ce57cf819b20b8252bf558fa
hennerich <hennerich@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-09-14 19:23:40 +0000

    On systems with less than 32 MB SDRAM  not all of the 16 I/D CPLBs are used. We need to invalidate the unused CPLBs, in case a boot loader has already initialized them. This will avoid Data access multiple CPLB hits EXCEPTIONS.
    This patch fixes a bug on BF533-EZKIT when started form u-boot (u-boot enables cache).
    
    
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1269 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/arch/blackfin/kernel/setup.c
+++ linux-2.6.x/arch/blackfin/kernel/setup.c
@@ -295,6 +295,8 @@ static void __init generate_cpl_tables(void)
 	pos = fill_cpl_tables(dcplb_table, pos, RAM_END - SIZE_1M, RAM_END, SIZE_1M, SDRAM_DNON_CHBL);
 	pos = fill_cpl_tables(dcplb_table, pos, ASYNC_BANK3_BASE, ASYNC_BANK3_BASE + ASYNC_BANK3_SIZE, ASYNC_BANK3_SIZE, SDRAM_EBIU);
 	pos = fill_cpl_tables(dcplb_table, pos, SIZE_4M, SIZE_4M+(dcplb_avail * SIZE_4M) , SIZE_4M, SDRAM_DGENERIC);
+	while (pos < 32)                                                   
+			pos = fill_cpl_tables(dcplb_table, pos, 0, 1 , SIZE_4M, 0);
   *(dcplb_table + pos)= -1;
 
 /* Generarte DCPLB switch table */
@@ -308,6 +310,8 @@ static void __init generate_cpl_tables(void)
 #if !defined(CONFIG_BF531) 
 	pos = fill_cpl_tables(dpdt_table, pos, L1_DATA_B_START, L1_DATA_B_START + L1_DATA_B_LENGTH, SIZE_4K, L1_DMEMORY);
 #endif
+	while (pos < 32)
+			pos = fill_cpl_tables(icplb_table, pos, 0, 1 , SIZE_4M, 0);
   *(dpdt_table + pos)= -1;
 #endif
 
