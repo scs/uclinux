9101b90b61eee9d7160f378982dba3049573836d
bernds <bernds@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-09-20 09:52:33 +0000

    Make sure all scheduling happens at interrupt level 15.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1327 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/arch/blackfin/mach-common/ints-priority-sc.c
+++ linux-2.6.x/arch/blackfin/mach-common/ints-priority-sc.c
@@ -338,7 +338,7 @@ static void bf533_demux_gpio_irq(unsigned int intb_irq, struct irqdesc *intb_des
  * the BFin IRQ handling routines.
  */
 
-extern void _deferred_ret_from_exception(void);
+extern void _evt14_softirq(void);
 
 int __init  init_arch_irq(void)
 {
@@ -364,7 +364,7 @@ int __init  init_arch_irq(void)
 	*pEVT11	= evt_evt11;
 	*pEVT12	= evt_evt12;
 	*pEVT13	= evt_evt13;
-	*pEVT14 = _deferred_ret_from_exception;
+	*pEVT14 = _evt14_softirq;
 	*pEVT15 = evt_system_call;
 	__builtin_bfin_csync();
 
