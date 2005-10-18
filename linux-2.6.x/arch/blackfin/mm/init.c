ba55eea5e1943d6207cb654d5dcf5b2a8fef8712
aubrey <aubrey@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-10-18 09:20:35 +0000

    Coding style change
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1420 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/arch/blackfin/mm/init.c
+++ linux-2.6.x/arch/blackfin/mm/init.c
@@ -1,18 +1,31 @@
 /*
- *  arch/blackfin/mm/init.c
+ * File:         arch/blackfin/mm/init.c
+ * Based on:    
+ * Author:     
+ *            
+ * Created:  
+ * Description:  
+ * Rev:          $Id$
  *
- *  Copyright (C) 1998  D. Jeff Dionne <jeff@lineo.ca>,
- *                      Kenneth Albanowski <kjahds@kjahds.com>,
- *  Copyright (C) 2000  Lineo, Inc.  (www.lineo.com) 
+ * Modified:    
+ *               Copyright 2004-2005 Analog Devices Inc.
  *
- *  Based on:
+ * Bugs:         Enter bugs at http://blackfin.uclinux.org/
  *
- *  linux/arch/m68knommu/mm/init.c
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
  *
- *  Copyright (C) 1995  Hamish Macdonald
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  DEC/2000 -- linux 2.4 support <davidm@lineo.com>
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.
+ * If not, write to the Free Software Foundation,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #include <linux/swap.h>
--- linux-2.6.x/arch/blackfin/mm/kmap.c
+++ linux-2.6.x/arch/blackfin/mm/kmap.c
@@ -1,9 +1,32 @@
 /*
- *  linux/arch/blackfin/mm/kmap.c
+ * File:         arch/blackfin/mm/kmap.c
+ * Based on:     arch/m68knommu/mm/kmap.c
+ * Author:       
+ *              
+ * Created:    
+ * Description:  
+ *              
+ * Rev:          $Id$
  *
- *  Based: linux/arch/m68knommu/mm/kmap.c
+ * Modified:    
+ *               Copyright 2004-2005 Analog Devices Inc.
  *
- *  Copyright (C) 2000 Lineo, <davidm@lineo.com>
+ * Bugs:         Enter bugs at http://blackfin.uclinux.org/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.
+ * If not, write to the Free Software Foundation,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
 #undef DEBUG
