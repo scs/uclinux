6a477068f46c500820060ccbab058f513be092dc
bernds <bernds@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-10-19 13:04:28 +0000

    Restore code that was lost in the last merge from mainline uClinux.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1431 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- lib/libc/main.c
+++ lib/libc/main.c
@@ -6,16 +6,14 @@ int main() {
 }
 
 
-/* We've got to provide an entry point that doesn't stuff around with a5 like
- * C routines tend to do.  We must also setup a5 from d5 which won't point to
- * this libraries data segment but from which it can be obtained.
+/* We've got to provide an entry point that doesn't stuff around with p5 like
+ * C routines tend to do.
  */
-asm(	".globl lib_main\n\t"
-	".type lib_main,@function\n"
-	"lib_main:\n\t"
-	"move.l %d5, %a5\n\t"
-	"bra.w main\n"
-	".L__end_lib_main__:\n\t"
-	".size lib_main,.L__end_lib_main__-libmain"
+asm(    ".global lib_main\n\t"
+        ".type lib_main, STT_FUNC;\n"
+        "lib_main:\n\t"
+        "rets = [sp++];\n\t"
+        "jump.l _main;\n"
+        ".size   libmain, .-libmain"
 );
 
