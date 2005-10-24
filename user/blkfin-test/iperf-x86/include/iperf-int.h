85d4cf7d74f78807b56a2b03439a6990a7e00f7f
magicyang <magicyang@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-10-24 03:06:10 +0000

    Add some network test apps for blackfin (and  x86, for testing.)
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1441 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/blkfin-test/iperf-x86/include/iperf-int.h
@@ -0,0 +1,9 @@
+#ifndef _IPERF_INCLUDE_IPERF_INT_H
+#define _IPERF_INCLUDE_IPERF_INT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "iperf 2.0.2"
+/* generated using gnu compiler gcc (GCC) 3.4.4 (Gentoo 3.4.4, ssp-3.4.4-1.0, pie-8.7.8) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
