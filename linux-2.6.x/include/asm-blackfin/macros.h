7e38a97ce0a703fced3b166c23ac458b205760be
magicyang <magicyang@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-10-31 02:23:16 +0000

    fix the error caused by indent.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1460 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/macros.h
+++ linux-2.6.x/include/asm-blackfin/macros.h
@@ -23,109 +23,74 @@
  * sys_reg is the system register and mask are the bits to be set.
  */
 #define bitset_SR(sys_reg, mask)\
-[--SP] = (R7:6);
-\
-r7 = sys_reg;
-\
-r6.l = (mask) & 0xffff;
-\
-r6.h = (mask) >> 16;
-\
-r7 = r7 | r6;
-\
-sys_reg = r7;
-\
-csync;
-\
-(R7:6) =[SP++]
-/*
- * Clear the corresponding bits in a System Register (SR);
- * All bits set in "mask" will be cleared in the SR
- * specified by "sys_reg" bitclr_SR(sys_reg, mask), where
- * sys_reg is the SR and mask are the bits to be cleared.
+		[--SP] = (R7:6);\
+		r7 = sys_reg;\
+		r6.l = (mask) & 0xffff;\
+		r6.h = (mask) >> 16;\
+		r7 = r7 | r6;\
+		sys_reg = r7;\
+		csync;\
+		(R7:6) = [SP++]
+
+/* 
+ * Clear the corresponding bits in a System Register (SR); 
+ * All bits set in "mask" will be cleared in the SR 
+ * specified by "sys_reg" bitclr_SR(sys_reg, mask), where 
+ * sys_reg is the SR and mask are the bits to be cleared. 
  */
 #define bitclr_SR(sys_reg, mask)\
-[--SP] = (R7:6);
-\
-r7 = sys_reg;
-\
-r7 = ~r7;
-\
-r6.l = (mask) & 0xffff;
-\
-r6.h = (mask) >> 16;
-\
-r7 = r7 | r6;
-\
-r7 = ~r7;
-\
-sys_reg = r7;
-\
-csync;
-\
-(R7:6) =[SP++]
-/*
- * Set the corresponding bits in a Memory Mapped Register (MMR);
+		[--SP] = (R7:6);\
+		r7 = sys_reg;\
+		r7 =~ r7;\
+		r6.l = (mask) & 0xffff;\
+		r6.h = (mask) >> 16;\
+		r7 = r7 | r6;\
+		r7 =~ r7;\
+		sys_reg = r7;\
+		csync;\
+		(R7:6) = [SP++]
+
+/* 
+ * Set the corresponding bits in a Memory Mapped Register (MMR); 
  * All bits set in "mask" will be set in the MMR specified by "mmr_reg"
  * bitset_MMR(mmr_reg, mask), where mmr_reg is the MMR and mask are
  * the bits to be set.
  */
 #define bitset_MMR(mmr_reg, mask)\
-[--SP] = (R7:6);
-\
-[--SP] = P5;
-\
-p5.l = mmr_reg & 0xffff;
-\
-p5.h = mmr_reg >> 16;
-\
-r7 =[p5];
-\
-r6.l = (mask) & 0xffff;
-\
-r6.h = (mask) >> 16;
-\
-r7 = r7 | r6;
-\
-[p5] = r7;
-\
-csync;
-\
-p5 =[SP++];
-\
-(R7:6) =[SP++]
-/*
- * Clear the corresponding bits in a Memory Mapped Register (MMR);
+		[--SP] = (R7:6);\
+		[--SP] = P5;\
+		p5.l = mmr_reg & 0xffff;\
+		p5.h = mmr_reg >> 16;\
+		r7 = [p5];\
+		r6.l = (mask) & 0xffff;\
+		r6.h = (mask) >> 16;\
+		r7 = r7 | r6;\
+		[p5] = r7;\
+		csync;\
+		p5 = [SP++];\
+		(R7:6) = [SP++]
+
+/* 
+ * Clear the corresponding bits in a Memory Mapped Register (MMR); 
  * All bits set in "mask" will be cleared in the MMR specified by "mmr_reg"
  * bitclr_MMRreg(mmr_reg, mask), where sys_reg is the MMR and mask are
  * the bits to be cleared.
  */
 #define bitclr_MMR(mmr_reg, mask)\
-[--SP] = (R7:6);
-\
-[--SP] = P5;
-\
-p5.l = mmr_reg & 0xffff;
-\
-p5.h = mmr_reg >> 16;
-\
-r7 =[p5];
-\
-r7 = ~r7;
-\
-r6.l = (mask) & 0xffff;
-\
-r6.h = (mask) >> 16;
-\
-r7 = r7 | r6;
-\
-r7 = ~r7;
-\
-[p5] = r7;
-\
-csync;
-\
-p5 =[SP++];
-\
-(R7:6) =[SP++]
-#endif	/* _MACROS_H */
+		[--SP] = (R7:6);\
+		[--SP] = P5;\
+		p5.l = mmr_reg & 0xffff;\
+		p5.h = mmr_reg >> 16;\
+		r7 = [p5];\
+		r7 =~ r7;\
+		r6.l = (mask) & 0xffff;\
+		r6.h = (mask) >> 16;\
+		r7 = r7 | r6;\
+		r7 =~ r7;\
+		[p5] = r7;\
+		csync;\
+		p5 = [SP++];\
+		(R7:6) = [SP++]
+
+
+#endif /* _MACROS_H */
