825b85b1176ff671185fe6f360b58b33e472d8d2
magicyang <magicyang@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-11-08 03:05:59 +0000

    Add some missing file for sqllite testing.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1493 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/blkfin-test/sqlite_test/wishlist.sql
@@ -0,0 +1,13 @@
+CREATE TABLE item (
+  id integer primary key not null,
+  user_id integer not null,
+  externalkey varchar(40),
+  description text
+);
+CREATE TABLE user (
+  id integer primary key not null,
+  name varchar(40) not null,
+  password char(32) not null,
+  email varchar(60) not null
+);
+CREATE INDEX item_user on item (user_id);
