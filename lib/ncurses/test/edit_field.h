4bd9dc0f1d12c7c2b28be904080f64c4c66a1139
ksh <ksh@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2005-11-24 09:47:35 +0000

    Initial revision
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@1588 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ lib/ncurses/test/edit_field.h
@@ -0,0 +1,22 @@
+/*
+ * $Id$
+ *
+ * Interface of edit_field.c
+ */
+
+#ifndef EDIT_FORM_H_incl
+#define EDIT_FORM_H_incl 1
+
+#include <form.h>
+
+#define EDIT_FIELD(c) (MAX_FORM_COMMAND + c)
+
+#define MY_HELP		EDIT_FIELD('h')
+#define MY_QUIT		EDIT_FIELD('q')
+#define MY_EDT_MODE	EDIT_FIELD('e')
+#define MY_INS_MODE	EDIT_FIELD('t')
+
+extern void help_edit_field(void);
+extern int edit_field(FORM * form, int *result);
+
+#endif /* EDIT_FORM_H_incl */
