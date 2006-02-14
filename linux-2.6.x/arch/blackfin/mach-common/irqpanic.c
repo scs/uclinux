fd34e300e759f9513636be00e547e4586ffe9876
rgetz <rgetz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-02-14 20:15:40 +0000

    Add icache checking code, to investigate bug [#1067]
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@2063 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/arch/blackfin/mach-common/irqpanic.c
+++ linux-2.6.x/arch/blackfin/mach-common/irqpanic.c
@@ -8,7 +8,7 @@
   *
   * Rev:          $Id$
   *
-  * Modified:
+  * Modified:     rgetz - added cache checking code 14Feb06
   *
   *
   * Bugs:         Enter bugs at http://blackfin.uclinux.org/
@@ -40,10 +40,96 @@ extern void dump(struct pt_regs *regs, void *);
  * irq_panic
  * - calls panic with string setup
  *********/
+#ifdef CONFIG_DEBUG_ICACHE_CHECK
+#define L1_ICACHE_START 0xffa10000
+#define L1_ICACHE_END   0xffa13fff
+void irq_panic(int reason, struct pt_regs *regs) __attribute__ ((section (".text.l1")));
+#endif
+
 asmlinkage void irq_panic(int reason, struct pt_regs *regs)
 {
 	int sig = 0;
 	siginfo_t info;
+
+#ifdef CONFIG_DEBUG_ICACHE_CHECK
+        unsigned int cmd, tag, ca, cache_hi, cache_lo, *pa;
+        unsigned short i, j, die;
+        unsigned int bad[10][6];
+
+	/* check entire cache for coherency                */
+        /* Since printk is in cacheable memory,            */
+        /* don't call it until you have checked everything */
+
+	die = 0;
+	i = 0;
+
+	/* check icache */
+
+	for (ca=L1_ICACHE_START; ca <=L1_ICACHE_END && i < 10; ca+= 32) {
+
+		/* Grab various address bits for the itest_cmd fields                      */
+		cmd = ( (( ca & 0x3000) <<  4 ) |   /* ca[13:12] for SBNK[1:0]             */
+			(( ca & 0x0c00) << 16 ) |   /* ca[11:10] for WAYSEL[1:0]           */
+			(( ca & 0x3f8)       ) |    /* ca[09:03] for SET[4:0] and DW[1:0]  */
+			0 );                        /* Access Tag, Read access             */
+
+		__builtin_bfin_ssync();
+		*pITEST_COMMAND=cmd;
+		__builtin_bfin_ssync();
+		tag = *pITEST_DATA0;
+		__builtin_bfin_ssync();
+
+		/* if tag is marked as valid, check it */
+		if ( tag & 1 ) {
+			/* The icache is arranged in 4 groups of 64-bits */
+			for (j = 0 ; j < 32 ; j+=8 ) {
+				cmd = ( (( (ca+j) & 0x3000) <<  4 ) |   /* ca[13:12] for SBNK[1:0]             */
+					(( (ca+j) & 0x0c00) << 16 ) |   /* ca[11:10] for WAYSEL[1:0]           */
+					(( (ca+j) & 0x3f8)       ) |    /* ca[09:03] for SET[4:0] and DW[1:0]  */
+					4 );                            /* Access Data, Read access             */
+
+                                __builtin_bfin_ssync();
+                                 *pITEST_COMMAND=cmd;
+                                __builtin_bfin_ssync();
+
+				cache_hi = *pITEST_DATA1;
+				cache_lo = *pITEST_DATA0;
+
+				pa = ((unsigned int *)((tag & 0xffffcc00)  | ((ca+j) & ~(0xffffcc00))));
+
+				/* 
+				 * Debugging this, enable 
+				 *
+				 * printk("addr: %08x %08x%08x | %08x%08x\n",
+				 *  ((unsigned int *)((tag & 0xffffcc00)  | ((ca+j) & ~(0xffffcc00)))),
+				 *   cache_hi, cache_lo, *(pa+1), *pa);
+				 */
+
+				if ( cache_hi != *(pa+1) || cache_lo != *pa ) {
+					/* Since icache is not working, stay out of it, by not printing */
+					die = 1;
+					bad [i][0] = (ca+j);
+					bad [i][1] = cache_hi;
+					bad [i][2] = cache_lo;
+					bad [i][3] = ((tag & 0xffffcc00)  | ((ca+j) & ~(0xffffcc00)));
+					bad [i][4] = *(pa+1);
+					bad [i][5] = *(pa);
+					i++;
+				}
+			}
+		}
+	}
+	if (die) {
+		printk ("icache coherency error\n");
+		for (j=0 ; j <= i; j++) {
+			printk("cache address   : %08x  cache value : %08x%08x\n",  bad [j][0],  bad [j][1],  bad [j][2]);
+			printk("physical address: %08x  SDRAM value : %08x%08x\n",  bad [j][3],  bad [j][4],  bad [j][5]);
+		}
+		panic("icache coherency error");
+	} else {
+		printk("\n\nicache checked, and OK\n");
+	}
+#endif
 
 	printk("\n\nException: IRQ 0x%x entered\n", reason);
 	printk(" code=[0x%08x],  ", (unsigned int)regs->seqstat);
