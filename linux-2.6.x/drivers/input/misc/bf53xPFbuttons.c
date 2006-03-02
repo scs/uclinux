0aeabac3ee38186fee4f7d49add3bc13d7717826
hennerich <hennerich@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-03-02 07:56:19 +0000

    I've modified the driver: 1) Now can be used with CONFIG_IRQCHIP_DEMUX_GPIO macro activated. 2) The PF's configuration now is not changed at every interrupt for reading the state, but the state is initialized by the init function and then toggle at every interrupt. Maybe now it is more simple adapt it to use pflag generic driver. 3) Italian comments are removed :) 4) Can be used on EZKIT.
    --Michele
    
    
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@2101 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/input/misc/bf53xPFbuttons.c
+++ linux-2.6.x/drivers/input/misc/bf53xPFbuttons.c
@@ -13,40 +13,6 @@ MODULE_AUTHOR("Michele d'Amico <michele.damico@fitre.it>");
 MODULE_DESCRIPTION("PFButton input driver");
 MODULE_LICENSE("GPL");
 
-
-/*********************************************************
- * Definizioni di "pflags.h" : non voglio importarlo perche'
- * non mi sembra un driver generico scritto per esse usato
- * da fuori.
- *********************************************************/
-
-#define SET_FIO_DIR                		1  // Peripheral Flag Direction Register
-#define SET_FIO_POLAR              		2  // Flag Source Polarity Register
-#define SET_FIO_EDGE               		3  // Flag Source Sensitivity Register
-#define SET_FIO_BOTH               		4  // Flag Set on BOTH Edges Register
-#define SET_FIO_INEN					5  // Flag Input Enable Register 
-
-
-#define INPUT							0 // SET_FIO_DIR
-#define OUTPUT							1
-
-#define ACTIVEHIGH_RISINGEDGE			0 // SET_FIO_POLAR
-#define ACTIVELOW_FALLINGEDGE			1
-
-#define LEVEL							0 // SET_FIO_EDGE
-#define EDGE							1
-
-#define SINGLEEDGE						0 // SET_FIO_BOTH
-#define BOTHEDGES						1
-
-#define INPUT_DISABLE					0 // SET_FIO_INEN
-#define INPUT_ENABLE					1
-
-/*********************************************************
- * FINE Definizioni di "pflags.h"
- *********************************************************/
-
-
 #undef	DEBUG
 //#define DEBUG
 
@@ -151,9 +117,36 @@ static unsigned short bf53xPFbuttons_ledcode[LEDS] = {
 };
 
 #define BELLS 0
+#elif defined(CONFIG_BFIN533_EZKIT)
+#define BUTTONS 4
+#define PF_BUTTON1 7
+#define PF_BUTTON2 8
+#define PF_BUTTON3 9
+#define PF_BUTTON4 10
+
+#define PF_BUTTONS_MASK (PF_MASK(PF_BUTTON1) | PF_MASK(PF_BUTTON2) | PF_MASK(PF_BUTTON3) | PF_MASK(PF_BUTTON3))
+
+static unsigned short bf53xPFbuttons_btn_pfmask[BUTTONS] = {
+	[0] 	= PF_MASK(PF_BUTTON1),
+	[1] 	= PF_MASK(PF_BUTTON2),
+	[2] 	= PF_MASK(PF_BUTTON3)
+	[3] 	= PF_MASK(PF_BUTTON4)
+};
+
+static unsigned short bf53xPFbuttons_btncode[BUTTONS] = {
+	[0] 	= (unsigned short) BTN_0,
+	[1] 	= (unsigned short) BTN_1,
+	[2] 	= (unsigned short) BTN_2,
+	[3] 	= (unsigned short) BTN_3
+};
+
+/* I don't know where the leds are routed on EZKIT (Michele) */
+#define LEDS 0
+
+#define BELLS 0
 
 #else
-#error "ONLY Tasvoip and STAMP are supported"
+#error "ONLY Tasvoip, STAMP and EZKIT 533 are supported"
 #endif
 
 struct bf53xPFbuttons {
@@ -185,7 +178,6 @@ static int bf53xPFbuttons_read_proc (char *page, char **start, off_t off, int co
 			     int *eof, void *data);
 
 static short read_state (struct bf53xPFbuttons *bf53xPFbuttons);
-static void check_and_notify(struct bf53xPFbuttons *bf53xPFbuttons);
 
 
 static struct bf53xPFbuttons chip = {
@@ -208,7 +200,31 @@ static struct bf53xPFbuttons chip = {
 	.events_processed = 0,
 };
 
-static short read_state (struct bf53xPFbuttons *bf53xPFbuttons){
+static inline void bf53xPFbuttons_init_pf_edge (struct bf53xPFbuttons *bf53xPFbuttons){
+	*pFIO_EDGE |= PF_BUTTONS_MASK;
+	*pFIO_BOTH |= PF_BUTTONS_MASK;
+	__builtin_bfin_ssync();
+}
+
+static inline void bf53xPFbuttons_init_pf_level (struct bf53xPFbuttons *bf53xPFbuttons){
+	*pFIO_POLAR &= ~PF_BUTTONS_MASK;
+	*pFIO_EDGE &= ~PF_BUTTONS_MASK;
+	__builtin_bfin_ssync();
+}
+
+#ifndef CONFIG_IRQCHIP_DEMUX_GPIO
+static inline void bf53xPFbuttons_mask_IRQ (struct bf53xPFbuttons *bf53xPFbuttons){
+	*pFIO_MASKA_C = PF_BUTTONS_MASK;
+	__builtin_bfin_ssync();
+}
+
+static inline void bf53xPFbuttons_unmask_IRQ (struct bf53xPFbuttons *bf53xPFbuttons){
+	*pFIO_MASKA_S = PF_BUTTONS_MASK;
+  	__builtin_bfin_ssync();
+}
+#endif
+
+static inline short read_state (struct bf53xPFbuttons *bf53xPFbuttons){
 	short val;
 #if defined(CONFIG_BF534)|defined(CONFIG_BF536)|defined(CONFIG_BF537)
 	unsigned short portx_fer;
@@ -216,51 +232,14 @@ static short read_state (struct bf53xPFbuttons *bf53xPFbuttons){
 	*pPORT_FER = 0;
 	__builtin_bfin_ssync();
 #endif
-
-
-	/* TODO:1) disabilitare gli interrupt;2) passare in modalita' normale leggere;3) ritornare in vecchia e riablitare. */
-	/* Disabilito l'interrupt del PF: Non funziona con il multiplexed GPIO, la maschera e' la B (mi pare)*/
-	*pFIO_MASKA_C = PF_BUTTONS_MASK;
-	__builtin_bfin_ssync();
-	/* Configurazione di lettura TODO : studiare se e' possibile partire dallo stato iniziale e tolgare tra i due stati*/
-	*pFIO_POLAR &= ~PF_BUTTONS_MASK;
-	*pFIO_EDGE &= ~PF_BUTTONS_MASK;
-	__builtin_bfin_ssync();
-	/*lettura*/
+	DPRINTK ("bf53xPFbuttons: read_state\n");
 	val = (*pFIO_FLAG_D & PF_BUTTONS_MASK);
-	/*ripristino*/
-	*pFIO_EDGE |= PF_BUTTONS_MASK;
-	*pFIO_BOTH |= PF_BUTTONS_MASK;
-	*pFIO_MASKA_S = PF_BUTTONS_MASK;
-  	__builtin_bfin_ssync();
 #if defined(CONFIG_BF534)|defined(CONFIG_BF536)|defined(CONFIG_BF537)
 	*pPORT_FER = portx_fer;
 	__builtin_bfin_ssync();
 #endif
 	return val;
 }
-
-
-static void check_and_notify(struct bf53xPFbuttons *bf53xPFbuttons){
-	int i;
-	unsigned short nextstate = read_state(bf53xPFbuttons);
-	bf53xPFbuttons->statechanged   = bf53xPFbuttons->laststate ^ nextstate;
-	bf53xPFbuttons->laststate      = nextstate;
-	if (bf53xPFbuttons->statechanged){
-#if BUTTONS
-		for(i=0;i<BUTTONS;i++){
-			if (bf53xPFbuttons->statechanged & bf53xPFbuttons->btn_pfmask[i]){
-				input_report_key(&bf53xPFbuttons->dev, bf53xPFbuttons->btncode[i], (nextstate&bf53xPFbuttons->btn_pfmask[i])?0:1);
-				bf53xPFbuttons->events_sended++;
-			}
-		}
-		input_sync(&bf53xPFbuttons->dev);
-#endif
-	}
-	
-}
-
-
 
 static irqreturn_t bf53xPFbuttons_irq_handler ( int irq, void *dev_id, struct pt_regs *regs ){
 	struct bf53xPFbuttons *bf53xPFbuttons = (struct bf53xPFbuttons *) dev_id;
@@ -270,24 +249,34 @@ static irqreturn_t bf53xPFbuttons_irq_handler ( int irq, void *dev_id, struct pt
 	*pPORT_FER = 0;
 	__builtin_bfin_ssync();
 #endif
-	/*Dovrebbe pulire l'interrupt, se non basta scommentare la riga succesiva TODO: verificare se toliere o meno il clear*/
-	short my_irq = *pFIO_FLAG_D & PF_BUTTONS_MASK;
-	//*pFIO_FLAG_C = PF_BUTTONS_MASK;
-	if (my_irq){
-		check_and_notify(bf53xPFbuttons);
+	DPRINTK ("bf53xPFbuttons: bf53xPFbuttons_irq_handler PF%d\n", (irq - IRQ_PF0));
+#ifdef CONFIG_IRQCHIP_DEMUX_GPIO
+	bf53xPFbuttons->statechanged   = 0x1 << (irq - IRQ_PF0);
+	
+#else
+	bf53xPFbuttons->statechanged   = read_state(bf53xPFbuttons);
+	*pFIO_FLAG_C = bf53xPFbuttons->statechanged;
+	__builtin_bfin_ssync();
+#endif
+	bf53xPFbuttons->laststate      ^= bf53xPFbuttons->statechanged;
+
+	if (bf53xPFbuttons->statechanged){
+		int i;
+		for(i=0;i<BUTTONS;i++){
+			if (bf53xPFbuttons->statechanged & bf53xPFbuttons->btn_pfmask[i]){
+				input_report_key(&bf53xPFbuttons->dev, bf53xPFbuttons->btncode[i], (bf53xPFbuttons->laststate&bf53xPFbuttons->btn_pfmask[i])?0:1);
+				bf53xPFbuttons->events_sended++;
+			}
+		}
+		input_sync(&bf53xPFbuttons->dev);
 	}
 #if defined(CONFIG_BF534)|defined(CONFIG_BF536)|defined(CONFIG_BF537)
 	*pPORT_FER = portx_fer;
 	__builtin_bfin_ssync();
 #endif
 	bf53xPFbuttons->irq_handled++;
-	DPRINTK("bf53xPFbuttons_irq_handler \n");
 	return IRQ_HANDLED;
 }
-
-/*
- * sunkbd_event() handles events from the input module.
- */
 
 static int bf53xPFbuttons_dev_event(struct input_dev *dev, unsigned int type, unsigned int code, int value)
 {
@@ -342,22 +331,69 @@ static int bf53xPFbuttons_dev_event(struct input_dev *dev, unsigned int type, un
 	return -1;
 }
 
+#ifdef CONFIG_IRQCHIP_DEMUX_GPIO
+static void inline bf53xPFbuttons_remove_IRQ(struct bf53xPFbuttons *bf53xPFbuttons,unsigned short mask){
+	int i=0;
+	while (mask){
+		if (mask & 0x1){
+    		free_irq(IRQ_PROG_INTA, bf53xPFbuttons_irq_handler);
+			printk (KERN_WARNING "bf53xPFbuttons: IRQ %d released\n", IRQ_PF0 + i);
+		}
+		++i;
+		mask >>= 1;
+	}
+}
 
+static int inline bf53xPFbuttons_init_IRQ(struct bf53xPFbuttons *bf53xPFbuttons,unsigned short mask){
+	unsigned short i_mask = mask;
+	int i=0;
+	while (mask){
+		if (mask & 0x1){
+			set_irq_type(IRQ_PF0 + i, IRQT_BOTHEDGE);
+			DPRINTK ("bf53xPFbuttons: bf53xPFbuttons_init_IRQ PF%d configured\n", i);
+			if( request_irq (IRQ_PF0 + i, bf53xPFbuttons_irq_handler, SA_INTERRUPT, "bf53xPFbuttons", bf53xPFbuttons) ){
+			    /* Rollback */
+			    printk (KERN_WARNING "bf53xPFbuttons: IRQ %d is not free. Roolback to the previos configuration\n", IRQ_PF0 + i);
+			    bf53xPFbuttons_remove_IRQ(bf53xPFbuttons,i_mask & ~mask);
+				return -EIO;
+			}
+			DPRINTK ("bf53xPFbuttons: bf53xPFbuttons_init_IRQ PF%d IRQ enabled\n", i);
+		}
+		++i;
+		mask >>= 1;
+	}
+	return 0;
+}
+#endif
+
+static void inline bf53xPFbuttons_init_state(struct bf53xPFbuttons *bf53xPFbuttons){
+#if defined(CONFIG_BF534)|defined(CONFIG_BF536)|defined(CONFIG_BF537)
+	unsigned short portx_fer;
+	portx_fer = *pPORT_FER;
+	*pPORT_FER = 0;
+	__builtin_bfin_ssync();
+#endif
+	*pFIO_DIR &= ~PF_BUTTONS_MASK;
+	*pFIO_INEN |= PF_BUTTONS_MASK;
+	bf53xPFbuttons_init_pf_level(bf53xPFbuttons);
+#ifdef CONFIG_IRQCHIP_DEMUX_GPIO
+	//TODO: By CONFIG_IRQCHIP_DEMUX_GPIO seems that for each demuxed gpio an spurious IRQ fired when is activate.
+	bf53xPFbuttons->laststate = PF_BUTTONS_MASK& (~read_state(bf53xPFbuttons));
+#else
+	bf53xPFbuttons->laststate = read_state(bf53xPFbuttons);
+#endif	
+	bf53xPFbuttons->statechanged = 0x0;
+#if defined(CONFIG_BF534)|defined(CONFIG_BF536)|defined(CONFIG_BF537)
+	*pPORT_FER = portx_fer;
+	__builtin_bfin_ssync();
+#endif
+}
 
 static int __init bf53xPFbuttons_init (void){
 	struct bf53xPFbuttons *bf53xPFbuttons = &chip;
 	int i;
-	short maska_state = *pFIO_MASKA_D; /*Per l'eventuale rollback */
-	
-	*pFIO_MASKA_D = 0x00; /*NON VOGLIO NESSUNO SULLA MIA LINEA DI INTERRUPT */
-	__builtin_bfin_ssync();
-	if( request_irq (IRQ_PROG_INTA, bf53xPFbuttons_irq_handler, SA_INTERRUPT, "bf53xPFbuttons", bf53xPFbuttons) ){
-	    /* ROLLBACK */
-	    *pFIO_MASKA_D = maska_state;
-	    __builtin_bfin_ssync();
-	    printk (KERN_WARNING "bf53xPFbuttons: IRQ %d is not free.\n", IRQ_PROG_INTA);
-	    return -EIO;
-	}
+	unsigned long flags;
+	int ret = 0;
 	init_input_dev(&bf53xPFbuttons->dev);
 	bf53xPFbuttons->dev.evbit[0] = 0;
 #if BUTTONS
@@ -408,11 +444,6 @@ static int __init bf53xPFbuttons_init (void){
 	printk(KERN_INFO "input: %s at %s\n", bf53xPFbuttons->name, bf53xPFbuttons->dev.phys);
 	
 	create_proc_read_entry ("driver/bf53xPFbuttons", 0, 0, bf53xPFbuttons_read_proc, bf53xPFbuttons);
-	bf53xPFbuttons->statechanged = 0x0;
-#if BUTTONS
-	*pFIO_DIR &= ~PF_BUTTONS_MASK;
-	*pFIO_INEN |= PF_BUTTONS_MASK; /*Sono gli unici registri che non vengono configurati in read_state()*/
-#endif
 #if LEDS
 	*pFIO_DIR |= PF_LEDS_MASK;
 	*pFIO_INEN &= ~PF_LEDS_MASK;
@@ -421,14 +452,43 @@ static int __init bf53xPFbuttons_init (void){
 	*pFIO_DIR |= PF_BELLS_MASK;
 	*pFIO_INEN &= ~PF_BELLS_MASK;
 #endif
-	bf53xPFbuttons->laststate = read_state(bf53xPFbuttons); /* Imposta OK anche i registri per l'irq */
-	DPRINTK ("pfx: pfbits driver for bf53x IRQ %d\n", IRQ_PROG_INTA);
-
-	return 0;
+#if BUTTONS
+	local_irq_save(flags);
+	bf53xPFbuttons_init_state(bf53xPFbuttons);
+	DPRINTK ("bf53xPFbuttons: bf53xPFbuttons_init start state = 0x%4X\n", bf53xPFbuttons->laststate);
+			
+#ifdef CONFIG_IRQCHIP_DEMUX_GPIO
+	ret = bf53xPFbuttons_init_IRQ(bf53xPFbuttons,PF_BUTTONS_MASK);
+#else
+	{
+		short maska_state = *pFIO_MASKA_D; /*Rollback data*/
+		*pFIO_MASKA_D = 0x00;
+		__builtin_bfin_ssync();
+		bf53xPFbuttons_init_pf_edge(bf53xPFbuttons);
+		bf53xPFbuttons_unmask_IRQ(bf53xPFbuttons);
+		if( request_irq (IRQ_PROG_INTA, bf53xPFbuttons_irq_handler, SA_INTERRUPT, "bf53xPFbuttons", bf53xPFbuttons) ){
+		    /* ROLLBACK */
+		    *pFIO_MASKA_D = maska_state;
+		    __builtin_bfin_ssync();
+		    printk (KERN_WARNING "bf53xPFbuttons: IRQ %d is not free.\n", IRQ_PROG_INTA);
+		    ret = -EIO;
+		}
+	}
+#endif
+#endif /*BUTTONS*/
+	local_irq_restore(flags);
+	DPRINTK ("bf53xPFbuttons: bf53xPFbuttons_init IRQ restored\n");
+	return ret;
 }
 
 void __exit bf53xPFbuttons_exit (void){
+	struct bf53xPFbuttons *bf53xPFbuttons = &chip;
+#ifdef CONFIG_IRQCHIP_DEMUX_GPIO
+	bf53xPFbuttons_remove_IRQ(bf53xPFbuttons,PF_BUTTONS_MASK);
+#else
+	bf53xPFbuttons_mask_IRQ(bf53xPFbuttons);
 	free_irq(IRQ_PROG_INTA, bf53xPFbuttons_irq_handler);
+#endif
 	remove_proc_entry ("driver/bf53xPFbuttons", NULL);
 }
 
