f78ab9e9f0e2431d43f53eb3c2d687541a4b7ef4
hennerich <hennerich@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-03-15 16:36:12 +0000

    Initial checkin twi_lcd test application Task[T266]
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@2174 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/blkfin-test/twi_lcd-test/twi_lcd.h
@@ -0,0 +1,24 @@
+
+/*
+ * Function command codes for io_ctl.
+ */
+#define LCD_On			1
+#define LCD_Off			2
+#define LCD_Clear		3
+#define LCD_Reset		4
+#define LCD_Cursor_Left		5
+#define LCD_Cursor_Right	6
+#define LCD_Disp_Left		7
+#define LCD_Disp_Right		8
+#define LCD_Set_Cursor		10
+#define LCD_Home		11
+#define LCD_Curr_Controller	12
+#define LCD_Cursor_Off		14
+#define LCD_Cursor_On		15
+#define LCD_Set_Cursor_Pos	17
+#define LCD_Blink_Off           18
+#define LCD_Contr           19
+
+#define CONTROLLER_1	0x1
+#define CONTROLLER_2	0x2
+#define CONTROLLER_BOTH	0x3
