dd5ebe526effb4666c4712f5020d39e46f2cb476
aubrey <aubrey@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-03-30 11:07:11 +0000

    Make BF537-TWI support plain i2c-level commands
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@2298 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/i2c/busses/i2c-bfin-twi.c
+++ linux-2.6.x/drivers/i2c/busses/i2c-bfin-twi.c
@@ -455,7 +455,8 @@ static u32 bfin_twi_functionality(struct i2c_adapter *adap)
 {
 	return I2C_FUNC_SMBUS_QUICK | I2C_FUNC_SMBUS_BYTE |
 	       I2C_FUNC_SMBUS_BYTE_DATA | I2C_FUNC_SMBUS_WORD_DATA |
-	       I2C_FUNC_SMBUS_BLOCK_DATA | I2C_FUNC_SMBUS_PROC_CALL;
+	       I2C_FUNC_SMBUS_BLOCK_DATA | I2C_FUNC_SMBUS_PROC_CALL|
+	       I2C_FUNC_I2C;
 }
 
 
