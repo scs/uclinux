d363220b5811449a831ea243c89fb8ac5e1d76e1
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-06-14 18:31:27 +0000

    add a note as to why this driver is disabled
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@2772 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/sound/blackfin/ad1981b.c
+++ linux-2.6.x/sound/blackfin/ad1981b.c
@@ -25,6 +25,10 @@
  * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+/*
+ * NOTE: Only tested on BF535 and never forward ported/validated since, thus disabled.
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
