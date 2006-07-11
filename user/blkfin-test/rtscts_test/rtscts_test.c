06f7fddb7ac0baf5b09e5161b2f7fee7913904e0
sonicz <sonicz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-07-11 08:44:33 +0000

    Add a RTS/CTS test tool for Task[T408].
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@2998 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/blkfin-test/rtscts_test/rtscts_test.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	int rtscts = TIOCM_RTS;
+	int f;
+	char devname[30]="/dev/";
+       
+	if(argc<3 || argv[2][0]!='-' || 
+		!(argv[2][1]=='e' || argv[2][1]=='d' || argv[2][1]=='t' || argv[2][1]=='s')) {
+		printf("Usage: %s <ttySx> <-e|-d|-t|-s>\n\t-e Enable RTS\n\t-d Disable RTS\n\t-t Test RTS\n\t-s Status of RTS/CTS\n", argv[0]);
+		return 0;
+	}
+	
+	strncat(devname, argv[1], 25);
+	f = open(devname, O_RDWR);
+	if(f<0) {
+		printf("Fail to open ttyS0\n");
+		return 0;
+	}
+
+	switch(argv[2][1]) {
+	case 'e':
+		ioctl(f, TIOCMBIS, &rtscts);
+		printf("RTS on %s is enabled.\n", devname);
+		break;
+	case 'd':
+		ioctl(f, TIOCMBIC, &rtscts);
+		printf("RTS on %s is disabled.\n", devname);
+		break;
+	case 't':
+		printf("Disable RTS on %s for 5 seconds. \nIf you hit any keys, you won't see them.\n", devname);
+		ioctl(f, TIOCMBIC, &rtscts);
+	
+		sleep(5);
+	
+		ioctl(f, TIOCMBIS, &rtscts);
+		printf("\nRTS is enabled again. \nYou should see the keys you hit just now.\n");
+		break;
+	case 's':
+		ioctl(f, TIOCMGET, &rtscts);
+		if(rtscts&TIOCM_RTS)
+			printf("RTS on %s is enabled.\n");
+		else
+			printf("RTS on %s is disabled.\n");
+		if(rtscts&TIOCM_CTS)
+			printf("CTS on %s is enabled.\n");
+		else
+			printf("CTS on %s is disabled.\n");
+		break;
+	}
+
+	close(f);
+
+	return 0;
+}
