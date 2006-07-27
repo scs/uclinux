42605025ae1dcbcf3f273328980b08ab49e48837
jane_lv <jane_lv@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-07-27 10:35:29 +0000

    [T455] Updated cvs checkout script.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@3173 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/ltp/build_ltp.sh
+++ testsuites/ltp/build_ltp.sh
@@ -1,10 +1,73 @@
 #!/bin/sh
 
-cvs_server_addr=10.99.22.20
-LTP_SUB_DIR=ltp/ltp-full-20060411
+if [ $# -ne 1 ]; then
+       echo "Usage: $0 cvs_server_addr"
+       echo "Please append CVS repository ip address"
+       exit 1
+fi       
+cvs_server_addr=$1
 
-cvs -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P $LTP_SUB_DIR
+# Checkout current file
+echo "$0:	Checking out from CVS, file [current]"
+cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P ltp/current
+if [ $? -ne 0 ]
+then
+	echo "$0:	Error, CVS checkout failed"
+	exit 1
+fi
 
+# Checkout ltp source directory
+LTP_WORKING_DIR=`cat ltp/current`
+LTP_SUB_DIR=ltp/$LTP_WORKING_DIR
+echo "$0:	Get ltp working directory [$LTP_WORKING_DIR]"
+
+echo "$0:	Checking out from CVS, ltp/$LTP_WORKING_DIR"
+cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P $LTP_SUB_DIR
+if [ $? -ne 0 ]
+then
+	echo "$0:	Error, CVS checkout failed"
+	exit 1
+fi
+
+# Checkout patch file for Makefiles 
+#echo "$0:	Checking out from CVS, file [Makefile.patch]"
+#cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P ltp/Makefile.patch
+#if [ $? -ne 0 ]
+#then
+#	echo "$0:	Error, CVS checkout failed"
+#	exit 1
+#fi
+cp /uclinux/ltp/remote/727/ltp/Makefile.patch $LTP_SUB_DIR/.
+
+# Go to working directory
+echo "$0:	Go to working directory"
 cd $LTP_SUB_DIR
-make uclinux
-make uclinux_install
+
+# Patch for Makefiles
+echo "$0:	Patching Makefiles"
+patch -p0 < Makefile.patch
+if [ $? -ne 0 ]
+then
+	echo "$0:	Error, patching Makefiles failed"
+	exit 1
+fi
+
+# Build ltp testsuites
+echo "$0:	Make ..."
+make -s uclinux
+if [ $? -ne 0 ]
+then
+	echo "$0:	Error, make failed" 
+	exit 1
+fi
+
+echo "$0:	make install ..."
+make -s uclinux_install
+if [ $? -ne 0 ]
+then
+	echo "$0:	Error, make install failed" 
+	exit 1
+fi
+echo "$0:	LTP build done"
+
+exit 0
