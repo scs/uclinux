c50580329abe71b61a8c840e481438c3451dbba0
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-07-30 20:37:08 +0000

    add proper comment header blocks
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@3194 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/net/bfin_mac.c
+++ linux-2.6.x/drivers/net/bfin_mac.c
@@ -1,12 +1,33 @@
-
-
-static const char version[] =
-"bfin_mac.c: v1.0, Aug 27 2005 by Luke Yang <luke.yang@analog.com>\n";
-
-/* Debugging level */
-#ifndef BFINMAC_DEBUG
-#define BFINMAC_DEBUG               0
-#endif
+/*
+ * File:         drivers/net/bfin_mac.c
+ * Based on:
+ * Author:       Luke Yang <luke.yang@analog.com>
+ *
+ * Created:
+ * Description:
+ *
+ * Rev:          $Id$
+ *
+ * Modified:
+ *               Copyright 2004-2006 Analog Devices Inc.
+ *
+ * Bugs:         Enter bugs at http://blackfin.uclinux.org/
+ *
+ * This program is free software ;  you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation ;  either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY ;  without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program ;  see the file COPYING.
+ * If not, write to the Free Software Foundation,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 
 #include <linux/config.h>
 #include <linux/init.h>
