835a97c768b147db3998b2ab2643e83637b0fb2a
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-07-31 22:30:45 +0000

    add proper comment header block
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@3226 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/char/bfin_spi_adc.c
+++ linux-2.6.x/drivers/char/bfin_spi_adc.c
@@ -1,22 +1,33 @@
-/************************************************************
-*
-* Copyright (C) 2004, Analog Devices. All Rights Reserved
-*
-* FILE adsp-spiadc.c
-* PROGRAMMER(S): Michael Hennerich (Analog Devices Inc.)
-*
-*
-* DATE OF CREATION: Sept. 10th 2004
-*
-* SYNOPSIS:
-*
-* DESCRIPTION: SPI-ADC/DAC Driver for ADSP-BF533/2/1. It can
-*              only be used in linux.
-**************************************************************
-* MODIFICATION HISTORY:
-* March 10, 2006  Modify to use new SPI common frame work. (Luke Yang)
-********************************************************/
-
+/*
+ * File:         drivers/char/bfin_spi_adc.c
+ * Based on:
+ * Author:       Michael Hennerich, Luke Yang
+ *
+ * Created:
+ * Description:
+ *
+ * Rev:          $Id$
+ *
+ * Modified:
+ *               Copyright 2004-2006 Analog Devices Inc.
+ *
+ * Bugs:         Enter bugs at http://blackfin.uclinux.org/
+ *
+ * This program is free software ;  you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation ;  either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY ;  without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program ;  see the file COPYING.
+ * If not, write to the Free Software Foundation,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -379,4 +390,5 @@ module_init(bfin_spi_adc_init);
 module_exit(bfin_spi_adc_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("SPI-ADC/DAC Driver");
 MODULE_AUTHOR("Luke Yang");
