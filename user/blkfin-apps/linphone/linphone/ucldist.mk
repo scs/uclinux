b398fac5b117c175ca71b0e2b7b500e3d6e4c70a
hjuergen <hjuergen@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-09-21 00:27:02 +0000

    add new makefiles for linphone
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@3870 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/linphone/linphone/ucldist.mk
+++ user/blkfin-apps/linphone/linphone/ucldist.mk
@@ -1,8 +1,11 @@
 DESTDIR=$(TEMPDIR)
 CFLAGS += -fno-strict-aliasing
-CFLAGS += -I$(DESTDIR)/include/readline
-CFLAGS += -I$(UCLDIR)/lib/ncurses/include/
-LDFLAGS +=-L$(UCLDIR)/lib/ncurses/lib/
+#CFLAGS += -I$(DESTDIR)/include/readline
+SPEEX_CFLAGS=-I$(ROOTDIR)/lib/speex/DESTDIR/usr/include
+SPEEX_LIB=-L$(ROOTDIR)/lib/speex/DESTDIR/usr/lib
+CFLAGS += -I$(ROOTDIR)/lib/readline/DESTDIR/usr/include/readline
+CFLAGS += -I$(ROOTDIR)/lib/ncurses/include/
+LDFLAGS +=-L$(ROOTDIR)/lib/ncurses/lib/
 LINPHONE_FLAGS+=--enable-portaudio=no
 LINPHONE_FLAGS+=--enable-gnome_ui=no
 LINPHONE_FLAGS+=--disable-video
@@ -11,16 +14,18 @@ LINPHONE_FLAGS+=--disable-shared
 LINPHONE_FLAGS+=--enable-static
 LINPHONE_FLAGS+=--disable-glib
 LINPHONE_FLAGS+=--with-osip=$(TEMPDIR)
-LINPHONE_FLAGS+=--with-speex=$(TEMPDIR)
-LINPHONE_FLAGS+=--with-readline=$(TEMPDIR)
-LINPHONE_FLAGS+=PKG_CONFIG_PATH=$(TEMPDIR)/lib/pkgconfig
+LINPHONE_FLAGS+=--with-speex=$(ROOTDIR)/lib/speex/DESTDIR/usr
+LINPHONE_FLAGS+=--with-readline=$(ROOTDIR)/lib/readline/DESTDIR/usr
+LINPHONE_FLAGS+=PKG_CONFIG_PATH=$(TEMPDIR)/lib/pkgconfig:$(ROOTDIR)/lib/speex/DESTDIR/usr/lib/pkgconfig:$(ROOTDIR)/lib/readline/DESTDIR/usr/pkgconfig
+
+PKG_CONFIG=/usr/bin/pkg-config
 
 
 all:	Makefile
-	make install 
+	make install DESTDIR=$(DESTDIR)
 
 Makefile:
-	./configure --host=bfin-uclinux --prefix=$(TEMPDIR) --with-real-prefix=$(DESTDIR) $(LINPHONE_FLAGS) CFLAGS="$(CFLAGS)" LDFLAGS="$(LDFLAGS)" LIBS="$(LDLIBS)" CC="$(CC)"
+	./configure --host=bfin-uclinux --prefix=/usr $(LINPHONE_FLAGS) CFLAGS="$(CFLAGS)" LDFLAGS="$(LDFLAGS)" LIBS="$(LDLIBS)" CC="$(CC) "
  
 
 clean:
