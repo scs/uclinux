7a5b2955017abe07f9bb386feebcc6f5b9c56de5
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-09-30 01:14:49 +0000

    import 0.48.1
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4093 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/dropbear/libtomcrypt/demos/timing.c
@@ -0,0 +1,26 @@
+#include <tomcrypt_test.h>
+
+int main(void)
+{
+init_timer();
+reg_algs();
+time_keysched();
+time_cipher();
+time_cipher2();
+time_cipher3();
+time_hash();
+time_macs();
+time_encmacs();
+time_prng();
+time_mult();
+time_sqr();
+time_rsa();
+time_ecc();
+time_dh();
+return EXIT_SUCCESS;
+
+}
+
+/* $Source$ */
+/* $Revision$ */
+/* $Date$ */
