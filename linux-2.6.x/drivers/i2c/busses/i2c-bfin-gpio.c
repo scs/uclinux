1eab26e4841d7a497e2255654c5cb4bf30f72bbd
hennerich <hennerich@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-10-25 15:03:53 +0000

    Enabel i2c-bfin-gpio on all Blackfin architectures and on all GPIO capabale IOs
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4257 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/i2c/busses/i2c-bfin-gpio.c
+++ linux-2.6.x/drivers/i2c/busses/i2c-bfin-gpio.c
@@ -25,58 +25,42 @@
 
 #include <asm/blackfin.h>
 
-
-#define	HHBF_I2C_SCLK		    (1 << CONFIG_BFIN_SCL)
-#define	HHBF_I2C_SDATA		    (1 << CONFIG_BFIN_SDA)
-
 #define	I2C_HW_B_HHBF		    I2C_HW_B_FRODO  /* 0x13 */
 
 static void hhbf_setsda(void *data, int state)
 {
-#if 0 	/* comment by mhfan */
-    if (state)
-	bfin_write_FIO_FLAG_D(bfin_read_FIO_FLAG_D() |  HHBF_I2C_SDATA);
-    else
-	bfin_write_FIO_FLAG_D(bfin_read_FIO_FLAG_D() & ~HHBF_I2C_SDATA);
-#else
     if (state) {
-	bfin_write_FIO_DIR(bfin_read_FIO_DIR() & ~HHBF_I2C_SDATA);
-	bfin_write_FIO_INEN(bfin_read_FIO_INEN() |  HHBF_I2C_SDATA);
+	set_gpio_dir(CONFIG_BFIN_SDA, GPIO_DIR_INPUT);
+	set_gpio_inen(CONFIG_BFIN_SDA, GPIO_INPUT_ENABLE);
+
     } else {
-	bfin_write_FIO_INEN(bfin_read_FIO_INEN() & ~HHBF_I2C_SDATA);
-	bfin_write_FIO_DIR(bfin_read_FIO_DIR() |  HHBF_I2C_SDATA);
-	bfin_write_FIO_FLAG_C(HHBF_I2C_SDATA);
+
+	set_gpio_inen(CONFIG_BFIN_SDA, GPIO_INPUT_DISABLE);
+	set_gpio_dir(CONFIG_BFIN_SDA, GPIO_DIR_OUTPUT);
+	set_gpio_data(CONFIG_BFIN_SDA, 0);
+
     }
-#endif	/* comment by mhfan */
 }
 
 static void hhbf_setscl(void *data, int state)
 {
-    if (state)
-	bfin_write_FIO_FLAG_S(HHBF_I2C_SCLK);
-    else
-	bfin_write_FIO_FLAG_C(HHBF_I2C_SCLK);
+
+	set_gpio_data(CONFIG_BFIN_SCL, state);
+
 }
 
 static int hhbf_getsda(void *data)
 {
-    return ((bfin_read_FIO_FLAG_D() & HHBF_I2C_SDATA) != 0);
+
+      return (get_gpio_data(CONFIG_BFIN_SDA) != 0);
+
 }
 
-#if 0 	/* comment by mhfan */
-static int hhbf_getscl(void *data)
-{
-    return ((bfin_read_FIO_FLAG_D() & HHBF_I2C_SCLK) != 0);
-}
-#endif	/* comment by mhfan */
 
 static struct i2c_algo_bit_data bit_hhbf_data = {
     .setsda  = hhbf_setsda,
     .setscl  = hhbf_setscl,
     .getsda  = hhbf_getsda,
-#if 0 	/* comment by mhfan */
-    .getscl  = hhbf_getscl,
-#endif	/* comment by mhfan */
     .udelay  = CONFIG_I2C_BFIN_GPIO_CYCLE_DELAY,
     .mdelay  = CONFIG_I2C_BFIN_GPIO_CYCLE_DELAY,
     .timeout = HZ
@@ -91,22 +75,32 @@ static struct i2c_adapter hhbf_ops = {
 
 static int __init i2c_hhbf_init(void)
 {
-    bfin_write_FIO_DIR(bfin_read_FIO_DIR() |  HHBF_I2C_SCLK);		// Set SCLK as output
-    bfin_write_FIO_POLAR(bfin_read_FIO_POLAR() & ~HHBF_I2C_SDATA);		// Enable Active Hight
-    bfin_write_FIO_EDGE(bfin_read_FIO_EDGE() & ~HHBF_I2C_SDATA);		// Enable Level Sensitivity
-    bfin_write_FIO_INEN(bfin_read_FIO_INEN() |  HHBF_I2C_SDATA);		// Enable SDATA Input Buffer
-    bfin_write_FIO_DIR(bfin_read_FIO_DIR() & ~HHBF_I2C_SDATA); 	// Set SDATA as input/high
-#if 0 	/* comment by mhfan */
-    bfin_write_FIO_DIR(bfin_read_FIO_DIR() |  HHBF_I2C_SDATA);
-    bfin_write_FIO_FLAG_D(bfin_read_FIO_FLAG_D() |  HHBF_I2C_SDATA);
-#endif	/* comment by mhfan */
-    bfin_write_FIO_FLAG_S  (  HHBF_I2C_SCLK);		// Set SCLK high
+
+    if(request_gpio(CONFIG_BFIN_SCL, REQUEST_GPIO)) {
+    	printk(KERN_ERR "%s: request_gpio GPIO %d failed \n",__FUNCTION__, CONFIG_BFIN_SCL);
+	return -1;
+	}
+
+    if(request_gpio(CONFIG_BFIN_SDA, REQUEST_GPIO)) {
+    	printk(KERN_ERR "%s: request_gpio GPIO %d failed \n",__FUNCTION__, CONFIG_BFIN_SDA);
+	return -1;
+	}
+
+
+    set_gpio_dir(CONFIG_BFIN_SCL, GPIO_DIR_OUTPUT);
+//    set_gpio_polar(CONFIG_BFIN_SDA, GPIO_POLAR_AH_RE);    /*default*/
+//    set_gpio_edge(CONFIG_BFIN_SDA, GPIO_EDGE_LEVEL);	/*default*/ 
+    set_gpio_inen(CONFIG_BFIN_SDA, GPIO_INPUT_ENABLE);
+//    set_gpio_dir(CONFIG_BFIN_SDA, GPIO_DIR_INPUT);  /*default*/
+    set_gpio_data(CONFIG_BFIN_SCL, 1);    
 
     return i2c_bit_add_bus(&hhbf_ops);
 }
 
 static void __exit i2c_hhbf_exit(void)
 {
+    free_gpio(CONFIG_BFIN_SCL);
+    free_gpio(CONFIG_BFIN_SDA);    
     i2c_bit_del_bus(&hhbf_ops);
 }
 
