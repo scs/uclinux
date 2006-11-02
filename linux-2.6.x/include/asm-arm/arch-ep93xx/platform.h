88abd1c40a0dfa7ef9cd1ed8e52bcf8e452cab1c
magicyang <magicyang@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-11-02 13:08:27 +0000

    update kernel to 2.6.18
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4311 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ linux-2.6.x/include/asm-arm/arch-ep93xx/platform.h
@@ -0,0 +1,15 @@
+/*
+ * linux/include/asm-arm/arch-ep93xx/platform.h
+ */
+
+#ifndef __ASSEMBLY__
+
+void ep93xx_map_io(void);
+void ep93xx_init_irq(void);
+void ep93xx_init_time(unsigned long);
+void ep93xx_init_devices(void);
+void ep93xx_clock_init(void);
+extern struct sys_timer ep93xx_timer;
+
+
+#endif
