4c7dc7f4a637e162131e22e3bbfb73e185a105c1
aubrey <aubrey@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-11-07 08:52:57 +0000

    Bug[#1810]: cvs kernel will no compile
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4348 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/drivers/mtd/nand/bfin_nand.c
+++ linux-2.6.x/drivers/mtd/nand/bfin_nand.c
@@ -32,6 +32,8 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/delay.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/nand.h>
 #include <linux/mtd/partitions.h>
@@ -49,8 +51,7 @@
 static struct mtd_info *bfin_mtd = NULL;
 static void __iomem *p_nand;
 static int nand_width = 1; /* default x8*/
-
-#define NB_OF(x)  (sizeof(x)/sizeof(x[0]))
+static void (*bf5xx_write_byte)(struct mtd_info *, u_char);
 
 /*
  * Define partitions for flash device
@@ -143,21 +144,6 @@ static u16 bfin_read_word(struct mtd_info *mtd)
 }
 
 /**
- * bfin_write_word -  write one word to the chip
- * @mtd:	MTD device structure
- * @word:	data word to write
- *
- *  write function for 16bit buswith without
- * endianess conversion
- */
-static void bfin_write_word(struct mtd_info *mtd, u16 word)
-{
-	struct nand_chip *this = mtd->priv;
-	writew(word, this->IO_ADDR_W);
-	__builtin_bfin_ssync();
-}
-
-/**
  * bfin_write_buf -  write buffer to chip
  * @mtd:	MTD device structure
  * @buf:	data buffer
@@ -170,7 +156,7 @@ static void bfin_write_buf(struct mtd_info *mtd, const u_char *buf, int len)
 	int i;
 	struct nand_chip *this = mtd->priv;
 
-	for (i=0; i<len; i++) {
+	for (i = 0; i < len; i++) {
 		writeb(buf[i], this->IO_ADDR_W);
 		__builtin_bfin_ssync();
 	}
@@ -189,7 +175,7 @@ static void bfin_read_buf(struct mtd_info *mtd, u_char *buf, int len)
 	int i;
 	struct nand_chip *this = mtd->priv;
 
-	for (i=0; i<len; i++) {
+	for (i = 0; i < len; i++) {
 		buf[i] = readb(this->IO_ADDR_R);
 		__builtin_bfin_ssync();
 	}
@@ -208,7 +194,7 @@ static int bfin_verify_buf(struct mtd_info *mtd, const u_char *buf, int len)
 	int i;
 	struct nand_chip *this = mtd->priv;
 
-	for (i=0; i<len; i++) {
+	for (i = 0; i < len; i++) {
 		if (buf[i] != readb(this->IO_ADDR_R))
 			return -EFAULT;
 		__builtin_bfin_ssync();
@@ -232,7 +218,7 @@ static void bfin_write_buf16(struct mtd_info *mtd, const u_char *buf, int len)
 	u16 *p = (u16 *) buf;
 	len >>= 1;
 
-	for (i=0; i<len; i++) {
+	for (i = 0; i < len; i++) {
 		writew(p[i], this->IO_ADDR_W);
 		__builtin_bfin_ssync();
 	}
@@ -254,7 +240,7 @@ static void bfin_read_buf16(struct mtd_info *mtd, u_char *buf, int len)
 	u16 *p = (u16 *) buf;
 	len >>= 1;
 
-	for (i=0; i<len; i++) {
+	for (i = 0; i < len; i++) {
 		p[i] = readw(this->IO_ADDR_R);
 		__builtin_bfin_ssync();
 	}
@@ -275,7 +261,7 @@ static int bfin_verify_buf16(struct mtd_info *mtd, const u_char *buf, int len)
 	u16 *p = (u16 *) buf;
 	len >>= 1;
 
-	for (i=0; i<len; i++) {
+	for (i = 0; i < len; i++) {
 		if (p[i] != readw(this->IO_ADDR_R))
 			return -EFAULT;
 		__builtin_bfin_ssync();
@@ -283,6 +269,18 @@ static int bfin_verify_buf16(struct mtd_info *mtd, const u_char *buf, int len)
 	return 0;
 }
 
+/* Select the chip by setting nCE to low */
+#define NAND_CTL_SETNCE         1
+/* Deselect the chip by setting nCE to high */
+#define NAND_CTL_CLRNCE         2
+/* Select the command latch by setting CLE to high */
+#define NAND_CTL_SETCLE         3
+/* Deselect the command latch by setting CLE to low */
+#define NAND_CTL_CLRCLE         4
+/* Select the address latch by setting ALE to high */
+#define NAND_CTL_SETALE         5
+/* Deselect the address latch by setting ALE to low */
+#define NAND_CTL_CLRALE         6
 
 static void bfin_hwcontrol(struct mtd_info *mtd, int cmd)
 {
@@ -311,13 +309,146 @@ int bfin_device_ready(struct mtd_info *mtd)
 	return ret;
 }
 
+/**
+ * bfin_select_chip - control -CE line
+ *	Forbid driving -CE manually permitting the NAND controller to do this.
+ *	Keeping -CE asserted during the whole sector reads interferes with the
+ *	NOR flash and PCMCIA drivers as it causes contention on the static bus.
+ *	We only have to hold -CE low for the NAND read commands since the flash
+ *	chip needs it to be asserted during chip not ready time but the NAND
+ *	controller keeps it released.
+ *
+ * @mtd:	MTD device structure
+ * @chip:	chipnumber to select, -1 for deselect
+ */
+static void bfin_select_chip(struct mtd_info *mtd, int chip)
+{
+}
+
+/**
+ * bfin_command - Send command to NAND device
+ * @mtd:	MTD device structure
+ * @command:	the command to be sent
+ * @column:	the column address for this command, -1 if none
+ * @page_addr:	the page address for this command, -1 if none
+ */
+static void bfin_command(struct mtd_info *mtd, unsigned command, int column, int page_addr)
+{
+	register struct nand_chip *this = mtd->priv;
+	int ce_override = 0, i;
+	ulong flags = 0;
+
+	/* Begin command latch cycle */
+	bfin_hwcontrol(mtd, NAND_CTL_SETCLE);
+	/*
+	 * Write out the command to the device.
+	 */
+	if (command == NAND_CMD_SEQIN) {
+		int readcmd;
+
+		if (column >= mtd->writesize) {
+			/* OOB area */
+			column -= mtd->writesize;
+			readcmd = NAND_CMD_READOOB;
+		} else if (column < 256) {
+			/* First 256 bytes --> READ0 */
+			readcmd = NAND_CMD_READ0;
+		} else {
+			column -= 256;
+			readcmd = NAND_CMD_READ1;
+		}
+		bf5xx_write_byte(mtd, readcmd);
+	}
+	bf5xx_write_byte(mtd, command);
+
+	/* Set ALE and clear CLE to start address cycle */
+	bfin_hwcontrol(mtd, NAND_CTL_CLRCLE);
+
+	if (column != -1 || page_addr != -1) {
+		bfin_hwcontrol(mtd, NAND_CTL_SETALE);
+
+		/* Serially input address */
+		if (column != -1) {
+			/* Adjust columns for 16 bit buswidth */
+			if (this->options & NAND_BUSWIDTH_16)
+				column >>= 1;
+			bf5xx_write_byte(mtd, column);
+		}
+		if (page_addr != -1) {
+			bf5xx_write_byte(mtd, (u8)(page_addr & 0xff));
+
+			if (command == NAND_CMD_READ0 ||
+			    command == NAND_CMD_READ1 ||
+			    command == NAND_CMD_READOOB) {
+				/*
+				 * NAND controller will release -CE after
+				 * the last address byte is written, so we'll
+				 * have to forcibly assert it. No interrupts
+				 * are allowed while we do this as we don't
+				 * want the NOR flash or PCMCIA drivers to
+				 * steal our precious bytes of data...
+				 */
+				ce_override = 1;
+				local_irq_save(flags);
+				bfin_hwcontrol(mtd, NAND_CTL_SETNCE);
+			}
+
+			bf5xx_write_byte(mtd, (u8)(page_addr >> 8));
+
+			/* One more address cycle for devices > 32MiB */
+			if (this->chipsize > (32 << 20))
+				bf5xx_write_byte(mtd, (u8)((page_addr >> 16) & 0x0f));
+		}
+		/* Latch in address */
+		bfin_hwcontrol(mtd, NAND_CTL_CLRALE);
+	}
+
+	/*
+	 * Program and erase have their own busy handlers.
+	 * Status and sequential in need no delay.
+	 */
+	switch (command) {
+
+	case NAND_CMD_PAGEPROG:
+	case NAND_CMD_ERASE1:
+	case NAND_CMD_ERASE2:
+	case NAND_CMD_SEQIN:
+	case NAND_CMD_STATUS:
+		return;
+
+	case NAND_CMD_RESET:
+		break;
+
+	case NAND_CMD_READ0:
+	case NAND_CMD_READ1:
+	case NAND_CMD_READOOB:
+		/* Check if we're really driving -CE low (just in case) */
+		if (unlikely(!ce_override))
+			break;
+
+		/* Apply a short delay always to ensure that we do wait tWB. */
+		ndelay(100);
+		/* Wait for a chip to become ready... */
+		for (i = this->chip_delay; !this->dev_ready(mtd) && i > 0; --i)
+			udelay(1);
+
+		/* Release -CE and re-enable interrupts. */
+		bfin_hwcontrol(mtd, NAND_CTL_CLRNCE);
+		local_irq_restore(flags);
+		return;
+	}
+	/* Apply this short delay always to ensure that we do wait tWB. */
+	ndelay(100);
+
+	while(!this->dev_ready(mtd));
+}
+
 /*
  * Main initialization routine
  */
 int __init bfin_nand_init (void)
 {
 	struct nand_chip *this;
-	int nb_parts = 0;
 	int retval;
 
 	/* Allocate memory for MTD device structure and private data */
@@ -332,11 +463,12 @@ int __init bfin_nand_init (void)
 	this = (struct nand_chip *) (&bfin_mtd[1]);
 
 	/* Initialize structures */
-	memset((char *) bfin_mtd, 0, sizeof(struct mtd_info));
-	memset((char *) this, 0, sizeof(struct nand_chip));
+	memset(bfin_mtd, 0, sizeof(struct mtd_info));
+	memset(this, 0, sizeof(struct nand_chip));
 
 	/* Link the private data with the MTD structure */
 	bfin_mtd->priv = this;
+	bfin_mtd->owner = THIS_MODULE;
 	
 	/* Configure GPIO-BFIN_NAND_READY */
 #if defined(CONFIG_BF534) || defined(CONFIG_BF536) || defined(CONFIG_BF537)
@@ -349,11 +481,13 @@ int __init bfin_nand_init (void)
 	p_nand = ioremap(BFIN_NAND_BASE, 0x1000);
 
 	/* Set address of hardware control function */
-	this->hwcontrol = bfin_hwcontrol;
 	this->dev_ready = bfin_device_ready;
+	this->select_chip = bfin_select_chip;
+	this->cmdfunc = bfin_command;
+
 	/* 30 us command delay time */
-	this->chip_delay = 30;		
-	this->eccmode = NAND_ECC_SOFT;
+	this->chip_delay = 30;
+	this->ecc.mode = NAND_ECC_SOFT;
 
 	this->options = NAND_NO_AUTOINCR;
 
@@ -361,8 +495,7 @@ int __init bfin_nand_init (void)
 		this->options |= NAND_BUSWIDTH_16;
 
 	this->read_byte = (!nand_width) ? bfin_read_byte16 : bfin_read_byte;
-	this->write_byte = (!nand_width) ? bfin_write_byte16 : bfin_write_byte;
-	this->write_word = bfin_write_word;
+	bf5xx_write_byte = (!nand_width) ? bfin_write_byte16 : bfin_write_byte;
 	this->read_word = bfin_read_word;
 	this->write_buf = (!nand_width) ? bfin_write_buf16 : bfin_write_buf;
 	this->read_buf = (!nand_width) ? bfin_read_buf16 : bfin_read_buf;
@@ -373,10 +506,9 @@ int __init bfin_nand_init (void)
 		retval = -ENXIO;
 		goto outio;
 	}
-	nb_parts = NB_OF(partition_info);
 
 	/* Register the partitions */
-	add_mtd_partitions(bfin_mtd, partition_info, nb_parts);
+	add_mtd_partitions(bfin_mtd, partition_info, ARRAY_SIZE(partition_info));
 
 	return 0;
  outio:
@@ -391,7 +523,6 @@ module_init(bfin_nand_init);
 /*
  * Clean up routine
  */
-#ifdef MODULE
 static void __exit bfin_cleanup (void)
 {
 	/* Release resources, unregister device */
@@ -404,7 +535,6 @@ static void __exit bfin_cleanup (void)
 	iounmap ((void *)p_nand);
 }
 module_exit(bfin_cleanup);
-#endif
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Aubrey.Li <aubrey.li@analog.com>");
