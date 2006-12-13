746e4be7990737a27c928034ddf89dfd1f3e823b
sonicz <sonicz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2006-12-13 08:56:06 +0000

    Task[#2351]. Remove __old_kernel_stat.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4564 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- linux-2.6.x/include/asm-blackfin/stat.h
+++ linux-2.6.x/include/asm-blackfin/stat.h
@@ -1,20 +1,6 @@
 #ifndef _BFIN_STAT_H
 #define _BFIN_STAT_H
 
-struct __old_kernel_stat {
-	unsigned short st_dev;
-	unsigned short st_ino;
-	unsigned short st_mode;
-	unsigned short st_nlink;
-	unsigned short st_uid;
-	unsigned short st_gid;
-	unsigned short st_rdev;
-	unsigned long st_size;
-	unsigned long st_atime;
-	unsigned long st_mtime;
-	unsigned long st_ctime;
-};
-
 struct stat {
 	unsigned short st_dev;
 	unsigned short __pad1;
