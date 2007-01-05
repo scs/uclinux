5aece9fc6f53c8d5ae07bb7dbe73055fa9002133
jane_lv <jane_lv@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-01-05 07:45:28 +0000

    [#2443] Switch to SVN.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4674 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/ltp/build_ltp.sh
+++ testsuites/ltp/build_ltp.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 1 ]; then
        echo "Usage: $0 cvs_server_addr"
-       echo "Please append CVS repository ip address"
+       echo "Please append SVN repository ip address"
        exit 1
 fi       
 cvs_server_addr=$1
@@ -13,36 +13,39 @@ then
 	echo "$0:	Clean directory"
 fi
 
+## Ignore this step by svn ##
 # Checkout current file
-echo "$0:	Checking out from CVS, file [current]"
-cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P ltp/current
-if [ $? -ne 0 ]
-then
-	echo "$0:	Error, CVS checkout failed"
-	exit 1
-fi
+#echo "$0:	Checking out from CVS, file [current]"
+#cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P ltp/current
+#if [ $? -ne 0 ]
+#then
+#	echo "$0:	Error, CVS checkout failed"
+#	exit 1
+#fi
 
+## We don't need patch for Makefile now ##
 # Checkout patch file for Makefiles 
-echo "$0:	Checking out from CVS, file [Makefile.patch]"
-cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P ltp/Makefile.patch
-if [ $? -ne 0 ]
-then
-	echo "$0:	Error, CVS checkout failed"
-	exit 1
-fi
+#echo "$0:	Checking out from CVS, file [Makefile.patch]"
+#cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P ltp/Makefile.patch
+#if [ $? -ne 0 ]
+#then
+#	echo "$0:	Error, CVS checkout failed"
+#	exit 1
+#fi
 
 # Checkout ltp source directory
-LTP_WORKING_DIR=`cat ltp/current`
-LTP_SUB_DIR=ltp/$LTP_WORKING_DIR
+#LTP_WORKING_DIR=`cat ltp/current`
+LTP_SUB_DIR=ltp-full-20061121
 CWD=`pwd`
 
-echo "$0:	Get ltp working directory [$LTP_WORKING_DIR]"
+#echo "$0:	Get ltp working directory [$LTP_WORKING_DIR]"
 
-echo "$0:	Checking out from CVS, ltp/$LTP_WORKING_DIR"
-cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P $LTP_SUB_DIR
+echo "$0:	Checking out from SVN, ltp/$LTP_WORKING_DIR"
+#cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P $LTP_SUB_DIR
+svn -q co svn://$cvs_server_addr/ltp/trunk/$LTP_SUB_DIR
 if [ $? -ne 0 ]
 then
-	echo "$0:	Error, CVS checkout failed"
+	echo "$0:	Error, SVN checkout failed"
 	exit 1
 fi
 
@@ -50,14 +53,15 @@ fi
 echo "$0:	Go to working directory"
 cd $LTP_SUB_DIR
 
+## We don't need patch for Makefile now ##
 # Patch for Makefiles
-echo "$0:	Patching Makefiles"
-patch -p0 < ../Makefile.patch
-if [ $? -ne 0 ]
-then
-	echo "$0:	Error, patching Makefiles failed"
-	exit 1
-fi
+#echo "$0:	Patching Makefiles"
+#patch -p0 < ../Makefile.patch
+#if [ $? -ne 0 ]
+#then
+#	echo "$0:	Error, patching Makefiles failed"
+#	exit 1
+#fi
 
 # Build ltp testsuites
 echo "$0:	Make ..."
