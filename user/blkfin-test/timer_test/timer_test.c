3f5c3591faf3be5173291d7f3277693c91dc9f97
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-01-17 00:59:18 +0000

    [#2424] show two different methods for setting up kernel timers
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4771 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-test/timer_test/timer_test.c
+++ user/blkfin-test/timer_test/timer_test.c
@@ -1,32 +1,15 @@
 /*
  * File:         timer_test.c
- * Based on:
  * Author:       Mike Frysinger
- *
- * Created:      Jan 2007
- * Description:  Example module for playing with kernel timers
+ * Description:  example code for playing with kernel timers
  *
  * Rev:          $Id: timer_test.c 3342 2006-08-09 20:32:43Z vapier $
- *
- * Modified:
  *               Copyright 2007 Analog Devices Inc.
  *
  * Bugs:         Enter bugs at http://blackfin.uclinux.org/
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see the file COPYING, or write
- * to the Free Software Foundation, Inc.,
- * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Licensed under the GPL-2 or later.
+ * http://www.gnu.org/licenses/gpl.txt
  */
 
 #include <linux/kernel.h>
@@ -36,7 +19,7 @@
 
 #define PRINTK(x...) printk(KERN_DEBUG "timer_test: " x)
 
-static struct timer_list timer_test;
+static struct timer_list timer_test_1, timer_test_2;
 
 static ulong delay = 5;
 module_param(delay, ulong, 0);
@@ -51,18 +34,35 @@ static int __init timer_test_init(void)
 {
 	int ret;
 	PRINTK("timer module init\n");
-	setup_timer(&timer_test, timer_test_func, 1234);
-	PRINTK("arming timer to fire %lu seconds from now\n", delay);
-	ret = mod_timer(&timer_test, jiffies + msecs_to_jiffies(delay * 1000));
-	PRINTK("mod_timer() returned %i\n", ret);
+
+	/* These two methods for setting up a timer are equivalent.
+	 * Depending on your code, it may be easier to do this in
+	 * steps or all at once.
+	 */
+
+	PRINTK("arming timer 1 to fire %lu seconds from now\n", delay);
+	setup_timer(&timer_test_1, timer_test_func, 1234);
+	ret = mod_timer(&timer_test_1, jiffies + msecs_to_jiffies(delay * 1000));
+	if (ret)
+		PRINTK("mod_timer() returned %i!  that's not good!\n", ret);
+
+	PRINTK("arming timer 2 to fire %lu seconds from now\n", delay*2);
+	init_timer(&timer_test_2);
+	timer_test_2.function = timer_test_func;
+	timer_test_2.data = 9876;
+	timer_test_2.expires = jiffies + msecs_to_jiffies(delay * 2 * 1000);
+	add_timer(&timer_test_2);
+
 	return 0;
 }
 
 static void __exit timer_test_cleanup(void)
 {
 	PRINTK("timer module cleanup\n");
-	if (del_timer(&timer_test))
-		PRINTK("timer is still in use!\n");
+	if (del_timer(&timer_test_1))
+		PRINTK("timer 1 is still in use!\n");
+	if (del_timer(&timer_test_2))
+		PRINTK("timer 2 is still in use!\n");
 }
 
 module_init(timer_test_init);
