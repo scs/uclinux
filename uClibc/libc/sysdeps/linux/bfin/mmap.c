ef5031c18f9514fad9a4a2e6453179b9361cbe18
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-01-24 06:49:57 +0000

    [#2341] make sure errno is set properly and fixup handling of syscall
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4804 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- uClibc/libc/sysdeps/linux/bfin/mmap.c
+++ uClibc/libc/sysdeps/linux/bfin/mmap.c
@@ -8,18 +8,15 @@
 #ifdef __NR_mmap2
 #include <asm/page.h>
 
-#ifndef __UCLIBC_HAS_LFS__
-#define __NR___syscall_mmap2	    __NR_mmap2
-inline _syscall6(__ptr_t, __syscall_mmap2, __ptr_t, addr, 
-	size_t, len, int, prot, int, flags, int, fd, off_t, offset);
-#endif
+#define __NR___syscall_mmap2 __NR_mmap2
+static inline _syscall6(__ptr_t, __syscall_mmap2, __ptr_t, addr, size_t, len,
+                 int, prot, int, flags, int, fd, __off_t, offset);
 
-inline __ptr_t mmap(__ptr_t addr, size_t len, int prot,
-		int flags, int fd, __off_t offset)
+__ptr_t mmap(__ptr_t addr, size_t len, int prot,
+                    int flags, int fd, __off_t offset)
 {
-	unsigned long buffer[6];
-
 	if (offset & ~PAGE_MASK) {
+		__set_errno(EINVAL);
 		return NULL;
 	}
 	return __syscall_mmap2(addr, len, prot, flags, fd, offset >> PAGE_SHIFT);
@@ -27,6 +24,6 @@ inline __ptr_t mmap(__ptr_t addr, size_t len, int prot,
 
 #else
 
-inline _syscall6 (__ptr_t, mmap, __ptr_t, addr, size_t, len, int, prot,
-	   int, flags, int, fd, __off_t, offset);
+_syscall6(__ptr_t, mmap, __ptr_t, addr, size_t, len, int, prot,
+          int, flags, int, fd, __off_t, offset);
 #endif
