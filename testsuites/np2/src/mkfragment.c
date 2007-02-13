1d87b524f0dd5ef8b0e54a96c538bad7d05b5df7
jane_lv <jane_lv@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-02-13 06:51:42 +0000

    [#2311] Add case for np2 test.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@4932 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ testsuites/np2/src/mkfragment.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUF 4096
+
+int main ()
+{
+        int i;
+        unsigned long *p,*j;
+
+        j = p = malloc(BUF*4);
+
+        for(i=0; i < BUF; i++){
+                *p = 0;
+                p++;
+        }
+
+        p = j;
+
+        for(i=0; i< BUF; i++){
+                *p = (int)malloc(1024*4);
+                if (!(*p))
+                        break;
+                p++;
+        }
+
+        p = j;
+        for(i=0; i< BUF; i++){
+                free ((void *)(*p));
+                p++;
+        }
+
+        free (j);
+}
