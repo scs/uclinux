f556442a4254e237bcc7e74f866d1cb67354b9c4
royhuang <royhuang@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-03-07 07:03:31 +0000

    Fix bug[#2816] also affect linphone. So change linphone accordingly
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5014 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-apps/linphone/linphone-1.6.0/ucldist.mk
+++ user/blkfin-apps/linphone/linphone-1.6.0/ucldist.mk
@@ -1,12 +1,5 @@
 DESTDIR=$(TEMPDIR)
 CFLAGS += -fno-strict-aliasing
-CFLAGS += -I$(ROOTDIR)/lib/readline/DESTDIR/usr/include/readline
-CFLAGS += -I$(ROOTDIR)/lib/alsa-lib/DESTDIR/usr/local/include
-CFLAGS += -L$(ROOTDIR)/lib/alsa-lib/DESTDIR/usr/local/lib
-CFLAGS += -I$(ROOTDIR)/lib/ncurses/include/
-LDFLAGS += -L$(ROOTDIR)/lib/ncurses/lib/
-CFLAGS += -I$(ROOTDIR)/lib/speex/DESTDIR/usr/include
-LDFLAGS += -L$(ROOTDIR)/lib/speex/DESTDIR/usr/lib
 LINPHONE_FLAGS+=--enable-portaudio=no
 LINPHONE_FLAGS+=--enable-gnome_ui=no
 LINPHONE_FLAGS+=--disable-video
@@ -16,9 +9,7 @@ LINPHONE_FLAGS+=--enable-static
 LINPHONE_FLAGS+=--disable-glib
 LINPHONE_FLAGS+=--enable-ipv6=no
 LINPHONE_FLAGS+=--with-osip=$(TEMPDIR)
-LINPHONE_FLAGS+=--with-speex=$(ROOTDIR)/lib/speex/DESTDIR/usr
-LINPHONE_FLAGS+=--with-readline=$(ROOTDIR)/lib/readline/DESTDIR/usr
-LINPHONE_FLAGS+=PKG_CONFIG_PATH=$(TEMPDIR)/lib/pkgconfig:$(ROOTDIR)/lib/speex/DESTDIR/usr/lib/pkgconfig:$(ROOTDIR)/lib/readline/DESTDIR/usr/lib/pkgconfig
+LINPHONE_FLAGS+=PKG_CONFIG_PATH=$(TEMPDIR)/lib/pkgconfig:$(STAGEDIR)/usr/lib/pkgconfig
 
 PKG_CONFIG=/usr/bin/pkg-config
 
@@ -37,7 +28,7 @@ build/Makefile:
 		--host=$(CONFIGURE_HOST) \
 		--prefix=/usr \
 		$(LINPHONE_FLAGS) \
-		CFLAGS="$(CFLAGS)" \
+		CFLAGS="-ffast-math -mfast-fp $(CFLAGS)" \
 		LDFLAGS="$(LDFLAGS)" \
 		LIBS="$(LDLIBS)" \
 		CC="$(CC) "
