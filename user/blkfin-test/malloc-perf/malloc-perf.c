7b172ca85d5b28aadf49bc06519949717dca6df8
grace <grace@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-04-02 10:42:41 +0000

    [#2443] Update malloc-perf test case.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5147 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-test/malloc-perf/malloc-perf.c
+++ user/blkfin-test/malloc-perf/malloc-perf.c
@@ -7,9 +7,10 @@
 int main(int argc, char* argv[]) {
   unsigned int i, * j, k, a, t1, t2, max, ave, min;
   struct timeval tim1, tim2;
+  unsigned int first_ave;
 
   k = 4 ;
-  for (i = 0; i <= 16 * 1024 ; i+=k ) {
+  for (i = 4; i <= 1024 ; i+=k ) {
         max = 0;
         ave = 0;
         min = 0xFFFFFFFF;
@@ -23,11 +24,18 @@ int main(int argc, char* argv[]) {
                 if ( min > t1 ) min = t1;
                 ave = ave + t1;
         }
+	if(i==4)
+		first_ave = ave;
         printf("%05ik : 0x%08x  %06i %06i %06i\n", i, j, min, ave/128, max);
         k = 1024;
         if ( i < 1024 ) k = 128;
         if ( i < 128 ) k = 4;
   }
+
+  if(first_ave * 2 < ave)
+	printf("TEST FAIL\n");
+  else
+	printf("TEST PASS\n");
 
   return 0;
 }
