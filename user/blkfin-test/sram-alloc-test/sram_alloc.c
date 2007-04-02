b5bba3421607bde3498fa8d9853a43e015c9a274
grace <grace@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-04-02 10:47:01 +0000

    [#2443] Update sram_alloc test case.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5148 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-test/sram-alloc-test/sram_alloc.c
+++ user/blkfin-test/sram-alloc-test/sram_alloc.c
@@ -5,9 +5,19 @@ int main ()
 {
   char * a;
   a = (char *) sram_alloc (100, L1_DATA_A_SRAM);
+  if ( a == null )
+        printf("L1_DATA_A_SRAM ALLOC FAIL\n");
+  else
+        printf("L1_DATA_A_SRAM ALLOC PASS\n");
+
   printf ("a = %x\n", a);
   sram_free (a);
   a = (char *) sram_alloc (1000, L1_DATA_B_SRAM);
+  if ( a == null )
+        printf("L1_DATA_B_SRAM ALLOC FAIL\n");
+  else
+        printf("L1_DATA_B_SRAM ALLOC PASS\n");
+
   printf ("a = %x\n", a);
   sram_free (a);
 
