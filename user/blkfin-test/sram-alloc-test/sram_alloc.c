9a7f3156625e80d72d78f80419c9a23bb6386f91
jiez <jiez@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-04-28 08:38:05 +0000

    Bug [#3178]. This test case mainly checks if the sram_alloc is in libc and usable. So try to allocate just only 1 byte such that it don't fail because there is not enough free sram.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5208 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-test/sram-alloc-test/sram_alloc.c
+++ user/blkfin-test/sram-alloc-test/sram_alloc.c
@@ -4,15 +4,15 @@
 int main ()
 {
   char * a;
-  a = (char *) sram_alloc (100, L1_DATA_A_SRAM);
-  if ( a == NULL )
+  a = (char *) sram_alloc (1, L1_DATA_A_SRAM);
+  if ( a == NULL)
         printf("L1_DATA_A_SRAM ALLOC FAIL\n");
   else
         printf("L1_DATA_A_SRAM ALLOC PASS\n");
 
   printf ("a = %x\n", a);
   sram_free (a);
-  a = (char *) sram_alloc (1000, L1_DATA_B_SRAM);
+  a = (char *) sram_alloc (1, L1_DATA_B_SRAM);
   if ( a == NULL )
         printf("L1_DATA_B_SRAM ALLOC FAIL\n");
   else
