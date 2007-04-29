d25ad814abe4d3c66a56a761a277996737ce0ec6
sonicz <sonicz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-04-29 06:52:12 +0000

    Task[#2424] Update twi test suite for regression test.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5210 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-test/twi_test/twi_test.c
+++ user/blkfin-test/twi_test/twi_test.c
@@ -11,14 +11,15 @@ int main(int argc, char* argv[])
     unsigned char outp_buf[100];
     int regval, regaddr, rc = 0;
     int i2c_fd;
-    int slave_addr;
+    int slave_addr, wrong_addr;
 
-    if(argc < 2) {
-	    printf("Usage: twi_test 0x<slave addr>\n");
+    if(argc < 3) {
+	    printf("Usage: twi_test 0x<correct slave addr> 0x<wrong slave addr>\n Example: 0x58 for ad5280 i2c chip on bf537-lq035 board.\n");
 	    return 0;
     }
 
     slave_addr = strtol(argv[1], NULL, 16);
+    wrong_addr = strtol(argv[2], NULL, 16);
 
     printf("slave addr: 0x%x\n", slave_addr);
 
@@ -26,7 +27,7 @@ int main(int argc, char* argv[])
     
     i2c_fd = open("/dev/i2c-0", O_RDWR);
 
-    printf("Program with correct I2C_SLAVE address\r\n");
+    printf("Case 1: Program with correct I2C_SLAVE address\r\n-----------------------------------------\n");
     if(ioctl(i2c_fd, I2C_SLAVE, slave_addr)<0) {/* Correct address */
 	    printf("Fail to set SLAVE address\n");
 	    return 0;
@@ -34,79 +35,124 @@ int main(int argc, char* argv[])
 
     regaddr = 2;
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (rc) {
+	    printf("Fail to get data from register %d\n", regaddr);
+	    return 0;
+    }
+
+    printf("reg %d = 0x%x\r\n", regaddr, regval);
 
     sleep(1);
     regaddr = 1;
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (rc) {
+	    printf("Fail to get data from register %d\n", regaddr);
+	    return 0;
+    }
+
+    printf("reg %d = 0x%x\r\n", regaddr, regval);
 
     sleep(1);
     regaddr = 0;
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (rc) {
+	    printf("Fail to get data from register %d\n", regaddr);
+	    return 0;
+    }
+
+    printf("reg %d = 0x%x\r\n", regaddr, regval);
 
     sleep(1);
     regaddr = 0x06; regval = 0x7f;
     rc = local_cam_set_register(i2c_fd, 1, regaddr, regval);
+    if (rc) {
+	    printf("Fail to set data to register %d\n", regaddr);
+	    return 0;
+    }
+
 
     sleep(1);
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (rc) {
+	    printf("Data set to register %d is wrong\n", regaddr);
+	    return 0;
+    }
 
-    printf("Program with incorrect I2C_SLAVE address\r\n");
-    ioctl(i2c_fd, I2C_SLAVE, 0x10); /* Incorrect address */
+    printf("reg %d = 0x%x\r\n", regaddr, regval);
+
+    printf("Case 2: Program with incorrect I2C_SLAVE address\r\n----------------------------------------\n");
+    ioctl(i2c_fd, I2C_SLAVE, wrong_addr); /* Incorrect address */
 
     sleep(1);
     regaddr = 2;
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
-
-    sleep(1);
-    regaddr = 1;
-    rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
-
-    sleep(1);
-    regaddr = 0;
-    rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (!rc) {
+	    printf("Wrong addr is incorrect, read %d from reg %d\n", regval, regaddr);
+	    return 0;
+    }
 
     sleep(1);
     regaddr = 0x06; regval = 0x20;
     rc = local_cam_set_register(i2c_fd, 1, regaddr, regval);
-
-    sleep(1);
-    rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (!rc) {
+	    printf("Wrong addr is incorrect, write %d to reg %d\n", regval, regaddr);
+	    return 0;
+    }
     
-    printf("Program with correct I2C_SLAVE address\r\n");
+    printf("Case 3: Program with correct I2C_SLAVE address\r\n------------------------------------------\n");
     ioctl(i2c_fd, I2C_SLAVE, slave_addr); /* Correct address */
 
     sleep(1);
     regaddr = 2;
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (rc) {
+	    printf("Fail to get data from register %d\n", regaddr);
+	    return 0;
+    }
+
+    printf("reg %d = 0x%x\r\n", regaddr, regval);
 
     sleep(1);
     regaddr = 1;
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (rc) {
+	    printf("Fail to get data from register %d\n", regaddr);
+	    return 0;
+    }
+
+    printf("reg %d = 0x%x\r\n", regaddr, regval);
 
     sleep(1);
     regaddr = 0;
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (rc) {
+	    printf("Fail to get data from register %d\n", regaddr);
+	    return 0;
+    }
+
+    printf("reg %d = 0x%x\r\n", regaddr, regval);
 
     sleep(1);
     regaddr = 0x09; regval = 0x30;
     rc = local_cam_set_register(i2c_fd, 1, regaddr, regval);
+    if (rc) {
+	    printf("Fail to set data to register %d\n", regaddr);
+	    return 0;
+    }
+
 
     sleep(1);
     rc = local_cam_get_register(i2c_fd, 1, regaddr, &regval);
-    if (!rc)    printf("reg %d = 0x%x\r\n", regaddr, regval);
+    if (rc) {
+	    printf("Data set to register %d is wrong\n", regaddr);
+	    return 0;
+    }
+
+    printf("reg %d = 0x%x\r\n", regaddr, regval);
     
     close(i2c_fd);
+
+    printf("TWI I2C test .... [PASS]\n");
 
     return 0;
 }
