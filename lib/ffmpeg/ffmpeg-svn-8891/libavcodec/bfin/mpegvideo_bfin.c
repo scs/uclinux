c31bbee5116d62e1fa1cd23ce93742d54770f4ed
mmh <mmh@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-06-12 18:39:25 +0000

    [#3265] mainline and our version are way out of sync
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5397 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- lib/ffmpeg/ffmpeg-svn-8891/libavcodec/bfin/mpegvideo_bfin.c
+++ lib/ffmpeg/ffmpeg-svn-8891/libavcodec/bfin/mpegvideo_bfin.c
@@ -20,9 +20,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include "dsputil.h"
-#include "mpegvideo.h"
-#include "avcodec.h"
+#include "../dsputil.h"
+#include "../mpegvideo.h"
+#include "../avcodec.h"
 
 
 #ifdef PROFILE
