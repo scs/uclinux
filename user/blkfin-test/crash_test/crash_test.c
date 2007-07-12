4608b3a1ab22d2cfd5f565d5076877a79fe6d97e
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-07-12 20:14:18 +0000

    [#3229] our improved exception handling prevents this asm() from actually crashing the kernel anymore ... so now we crash the kernel by filling the first 16megs of RAM with NOPs
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5450 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/blkfin-test/crash_test/crash_test.c
+++ user/blkfin-test/crash_test/crash_test.c
@@ -1,20 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #define COUNTDOWN 3
 
-int main(int argc,char *argv[]){
-
+int main(int argc, char *argv[])
+{
 	int cd = 3;
 	fprintf(stdout,"Go in PANIC ....");
 	fflush(stdout);
-	while(cd--){
+	while (cd--) {
 		sleep(1);
-		fprintf(stdout,"%d ...",cd+1);
+		fprintf(stdout, "%d ...", cd+1);
 		fflush(stdout);
 	}
 
-	asm("R3.L=0xFFF0\n\r;R3.H=0x9FFF\n\r;P3=R3;jump (P3)\n\r;");
-}
+	/* fill the first 16 megs (offset by 4k page) */
+	memset((void*)0x4000, 0x0, 0x1000000);
 
+	return 1;
+}
