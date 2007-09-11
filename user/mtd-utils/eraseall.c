39ebc44dd4fa0cf428011b1e2c2a8ac964576eed
sonicz <sonicz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-09-11 02:10:33 +0000

    Task[#3507] Enable eraseall for bf548 nor flash.
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5586 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- user/mtd-utils/eraseall.c
+++ user/mtd-utils/eraseall.c
@@ -63,6 +63,9 @@ int main( int argc,char *argv[] )
    }
 
    erase.length = meminfo.erasesize;
+   erase.start = 0;
+   ioctl( fd, MEMLOCK, &erase);
+
    for (erase.start = 0; erase.start < meminfo.size;
 	erase.start += meminfo.erasesize) {
        
@@ -73,12 +76,19 @@ int main( int argc,char *argv[] )
        }
        fflush( stdout );
 	
+       if(ioctl( fd, MEMUNLOCK, &erase) != 0) {
+	       fprintf( stderr, "\n%s: %s: MTD Unlock failure: %s\n", exe_name, 
+			mtd_device, strerror( errno) );
+               continue;
+       }
+
        if(ioctl( fd, MEMERASE, &erase) != 0)
        {
 	       fprintf( stderr, "\n%s: %s: MTD Erase failure: %s\n", exe_name, 
 			mtd_device, strerror( errno) );
-	       //exit( 1 );
        }
+
+       ioctl( fd, MEMLOCK, &erase);
    }
    if( !quiet ) {
        printf( "\rErased %ld Kibyte @ %lx -- 100%% complete.       \n",
