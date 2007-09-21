6070b97769f8d010e245949877e907bc966f2b06
rgetz <rgetz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2007-09-21 00:47:27 +0000

    [#3551] - remove optional postfix/prefix operators that dash doesn't support
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@5603 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/adi_ice_helpers/bfin-uclinux-vdsp_summary.sh
+++ testsuites/adi_ice_helpers/bfin-uclinux-vdsp_summary.sh
@@ -15,7 +15,7 @@ do
   sum_percent=`echo "scale=2; $sum_fig * 100 / $total" | bc`
   j=`echo "scale=2; length($sum_fig * 100 / $total)" | bc`
   i=`echo "x=l($sum_fig)/l(10); scale =0 ; $tab - x/1 " | bc -l`
-  for (( ; i >= 1 ; i-- ))
+  for (( ; i >= 1 ; i=i-1 ))
   do
     echo -n " "
   done
@@ -23,7 +23,7 @@ do
   if [ "$sum_percent" == "0" ] ; then
     echo -n "  .00%) "
   else
-    for (( ; j <= 3 ; j++ ))
+    for (( ; j <= 3 ; j=j+1 ))
     do
       echo -n " "
     done
