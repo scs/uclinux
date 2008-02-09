a8553ad27ddd7b9496dd50fe6cdee49bbb7b6024
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2008-02-09 20:06:44 +0000

    add a template mk for autotool based packages
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@6194 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ tools/autotools.mk
@@ -0,0 +1,25 @@
+# Requirements:
+# - set VER to the package directory
+# - define romfs target
+# Optional:
+# - set CONF_OPTS
+#
+# Then just add to your package Makefile:
+# include $(ROOTDIR)/tools/autotools.mk
+
+all: build-$(VER)/Makefile
+	$(MAKE) -C build-$(VER) install DESTDIR=$(STAGEDIR)
+
+build-$(VER)/Makefile:
+	chmod a+rx $(VER)/configure # for CVS users with screwed perms
+	find $(VER) -type f -print0 | xargs -0 touch -r $(VER)/configure
+	set -e ; \
+	rm -rf build-$(VER) ; \
+	mkdir build-$(VER) ; \
+	cd build-$(VER) ; \
+	../$(VER)/configure $(CONFIGURE_OPTS) $(CONF_OPTS)
+
+clean:
+	rm -rf build*
+
+.PHONY: all clean romfs
