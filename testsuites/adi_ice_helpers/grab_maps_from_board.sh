5c1a6a20b8fac7e80bd528c184cd56bdcf32246e
rgetz <rgetz@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2008-08-05 18:21:46 +0000

    [#4311] Fixes things when application is run in background or started from system() command
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@6974 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/adi_ice_helpers/grab_maps_from_board.sh
+++ testsuites/adi_ice_helpers/grab_maps_from_board.sh
@@ -13,4 +13,4 @@ $rsh cat /proc/kallsyms | grep " [Tt] " > System.list
 $rsh cat /proc/maps | grep "x[ps] " | awk 'NF > 5' > user.list
 $rsh cat /proc/sram | grep -v NULL | sed -n "/--- L1 Instruction/,// p" | grep -v " L1 Instruction" | sed "s/-/ /" > l1_sram.list
 ${rsh} lsmod | awk '{print $1}' | grep -v Module > modules.list
-${rsh} ps | grep -v "\[.*\]" | grep -v PID | awk '{print $1 " " $5 }' | awk -F "[/ ]" '{print $NF" " $1}'> pid.list
+${rsh} ps | grep -v "\[.*\]" | grep -v PID | sed 's/ < / /g' | awk '{print $1 " " $5 }' | awk -F "[/ ]" '{print $NF" " $1}' | sed 's/^-//' > pid.list
