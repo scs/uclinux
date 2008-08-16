a2c991e076edafe31c26622785b4d24771899925
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2008-08-16 15:02:27 +0000

    version bump DirectFB to 1.2.2
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7002 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ lib/DirectFB/DirectFB-1.2.2/src/core/core_parts.h
@@ -0,0 +1,131 @@
+/*
+   (c) Copyright 2001-2008  The world wide DirectFB Open Source Community (directfb.org)
+   (c) Copyright 2000-2004  Convergence (integrated media) GmbH
+
+   All rights reserved.
+
+   Written by Denis Oliver Kropp <dok@directfb.org>,
+              Andreas Hundt <andi@fischlustig.de>,
+              Sven Neumann <neo@directfb.org>,
+              Ville Syrjälä <syrjala@sci.fi> and
+              Claudio Ciccani <klan@users.sf.net>.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the
+   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __CORE_PARTS_H__
+#define __CORE_PARTS_H__
+
+#include <fusion/types.h>
+#include <fusion/lock.h>
+
+#include <directfb.h>
+
+#include <core/coretypes.h>
+#include <core/coredefs.h>
+
+
+typedef DFBResult (*CoreInitialize)( CoreDFB *core,
+                                     void    *data_local,
+                                     void    *data_shared );
+
+typedef DFBResult (*CoreJoin)      ( CoreDFB *core,
+                                     void    *data_local,
+                                     void    *data_shared );
+
+typedef DFBResult (*CoreShutdown)  ( CoreDFB *core,
+                                     bool     emergency );
+
+typedef DFBResult (*CoreLeave)     ( CoreDFB *core,
+                                     bool     emergency );
+
+typedef DFBResult (*CoreSuspend)   ( CoreDFB *core );
+
+typedef DFBResult (*CoreResume)    ( CoreDFB *core );
+
+
+typedef struct {
+     const char     *name;
+
+     int             size_local;
+     int             size_shared;
+
+     CoreInitialize  Initialize;
+     CoreJoin        Join;
+     CoreShutdown    Shutdown;
+     CoreLeave       Leave;
+     CoreSuspend     Suspend;
+     CoreResume      Resume;
+
+     void           *data_local;
+     void           *data_shared;
+
+     bool            initialized;
+} CorePart;
+
+
+DFBResult dfb_core_part_initialize( CoreDFB  *core,
+                                    CorePart *core_part );
+
+DFBResult dfb_core_part_join      ( CoreDFB  *core,
+                                    CorePart *core_part );
+
+DFBResult dfb_core_part_shutdown  ( CoreDFB  *core,
+                                    CorePart *core_part,
+                                    bool      emergency );
+
+DFBResult dfb_core_part_leave     ( CoreDFB  *core,
+                                    CorePart *core_part,
+                                    bool      emergency );
+
+
+#define DFB_CORE_PART(part,Type)                                               \
+                                                                               \
+static DFBResult dfb_##part##_initialize( CoreDFB           *core,             \
+                                          DFB##Type         *data,             \
+                                          DFB##Type##Shared *shared );         \
+                                                                               \
+static DFBResult dfb_##part##_join      ( CoreDFB           *core,             \
+                                          DFB##Type         *data,             \
+                                          DFB##Type##Shared *shared );         \
+                                                                               \
+static DFBResult dfb_##part##_shutdown  ( DFB##Type         *data,             \
+                                          bool               emergency );      \
+                                                                               \
+static DFBResult dfb_##part##_leave     ( DFB##Type         *local,            \
+                                          bool               emergency );      \
+                                                                               \
+static DFBResult dfb_##part##_suspend   ( DFB##Type         *local );          \
+                                                                               \
+static DFBResult dfb_##part##_resume    ( DFB##Type         *local );          \
+                                                                               \
+CorePart dfb_##part = {                                                        \
+     #part,                                                                    \
+                                                                               \
+     sizeof(DFB##Type),                                                        \
+     sizeof(DFB##Type##Shared),                                                \
+                                                                               \
+     (void*)dfb_##part##_initialize,                                           \
+     (void*)dfb_##part##_join,                                                 \
+     (void*)dfb_##part##_shutdown,                                             \
+     (void*)dfb_##part##_leave,                                                \
+     (void*)dfb_##part##_suspend,                                              \
+     (void*)dfb_##part##_resume                                                \
+}
+
+
+#endif
+
---
+++ lib/DirectFB/DirectFB-1.2.2/systems/x11/x11.h
@@ -0,0 +1,105 @@
+/*
+   (c) Copyright 2001-2008  The world wide DirectFB Open Source Community (directfb.org)
+   (c) Copyright 2000-2004  Convergence (integrated media) GmbH
+
+   All rights reserved.
+
+   Written by Denis Oliver Kropp <dok@directfb.org>,
+              Andreas Hundt <andi@fischlustig.de>,
+              Sven Neumann <neo@directfb.org>,
+              Ville Syrjälä <syrjala@sci.fi> and
+              Claudio Ciccani <klan@users.sf.net>.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the
+   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __X11SYSTEM__X11_H__
+#define __X11SYSTEM__X11_H__
+
+#include <fusion/call.h>
+#include <fusion/lock.h>
+#include <core/layers.h>
+#include <core/surface.h>
+#include <core/surface_buffer.h>
+#include <core/surface_pool.h>
+
+#include "x11image.h"
+#include "xwindow.h"
+
+typedef struct {
+     CoreLayerRegionConfig  config;
+} SetModeData;
+
+typedef struct {
+     DFBRegion              region;
+
+     CoreSurfaceBufferLock *lock;
+} UpdateScreenData;
+
+
+typedef struct {
+     UpdateScreenData     update;
+     SetModeData          setmode;
+                         
+     Bool                 use_shm;
+     int                  xshm_major;
+     int                  xshm_minor;
+                         
+     FusionSkirmish       lock;
+     FusionCall           call;
+
+     FusionSHMPoolShared *data_shmpool;
+ 
+     CoreSurfacePool     *x11image_pool;
+     CoreSurfacePool     *vpsmem_pool;
+     unsigned int         vpsmem_length;
+ 
+     CoreSurface         *primary;
+     XWindow             *xw;
+     Display             *display;
+     Screen*              screenptr;
+     int                  screennum;
+                     
+     Visual              *visuals[DFB_NUM_PIXELFORMATS];
+} DFBX11;
+
+typedef enum {
+     X11_CREATE_WINDOW,
+     X11_UPDATE_SCREEN,
+     X11_SET_PALETTE,
+     X11_IMAGE_INIT,
+     X11_IMAGE_DESTROY,
+     X11_DESTROY_WINDOW,
+} DFBX11Call;
+
+
+
+DFBResult dfb_x11_create_window_handler ( CoreLayerRegionConfig *config );
+DFBResult dfb_x11_destroy_window_handler( void );
+
+DFBResult dfb_x11_update_screen_handler ( UpdateScreenData *data );
+DFBResult dfb_x11_set_palette_handler   ( CorePalette *palette );
+
+DFBResult dfb_x11_image_init_handler    ( x11Image *image );
+DFBResult dfb_x11_image_destroy_handler ( x11Image *image );
+
+
+extern DFBX11  *dfb_x11;
+extern CoreDFB *dfb_x11_core;
+
+
+#endif //__X11SYSTEM__X11_H__
+
