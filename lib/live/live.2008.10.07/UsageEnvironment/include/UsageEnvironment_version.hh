283cd25d020392c33b60a5498f8161e342b4ef74
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2008-10-09 22:57:24 +0000

    version bump to live.2008.10.07
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7200 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ lib/live/live.2008.10.07/UsageEnvironment/include/UsageEnvironment_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "UsageEnvironment" library
+// Copyright (c) 1996-2008 Live Networks, Inc.  All rights reserved.
+
+#ifndef _USAGEENVIRONMENT_VERSION_HH
+#define _USAGEENVIRONMENT_VERSION_HH
+
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2008.10.07"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1223337600
+
+#endif
---
+++ lib/live/live.2008.10.07/groupsock/include/groupsock_version.hh
@@ -0,0 +1,10 @@
+// Version information for the "groupsock" library
+// Copyright (c) 1996-2008 Live Networks, Inc.  All rights reserved.
+
+#ifndef _GROUPSOCK_VERSION_HH
+#define _GROUPSOCK_VERSION_HH
+
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2008.10.07"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1223337600
+
+#endif
