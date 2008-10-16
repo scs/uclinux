25e0d0d1506dbaaffbf43aada75d8d03ac36b93f
vivili <vivili@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2008-10-16 10:57:56 +0000

    [#4182]make sure this test runs on core A
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7229 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/l1_app/test_helloworld.c
+++ testsuites/l1_app/test_helloworld.c
@@ -15,6 +15,10 @@ int main(int argc, char *argv[])
 {
 	int r;
 	r=helloworld("hello\n");
+
+        unsigned long mask=1;  //only run on core A
+        sched_setaffinity(getpid(), sizeof(mask), &mask);
+
 	/* Check for instruction address */
 	printf("[INS] get %d from helloworld(0x%x):", r, ((unsigned long *)helloworld)[0]);
 	if (((((unsigned long *)helloworld)[0]) > TEST_INS_CACHE_HIGH) || \
