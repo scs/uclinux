61f14aef0c74f31f726e12bf7c04f7140faf58a5
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2008-11-06 23:08:13 +0000

    script to auto fetch & apply fixes from upstream
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7327 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

---
+++ user/busybox/get-hotfixes.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+cd ${0%/*}
+
+base_url="http://busybox.net/downloads/fixes-"
+
+bdir=$(awk '$1 == "VER" { print $NF }' Makefile)
+bver=${bdir#busybox-}
+
+echo "Getting fixes for ${bver}"
+
+url="${base_url}${bver}/"
+
+cd ${bdir}
+
+#rm -rf hotfixes
+#svn_st=$(svn st)
+if [[ -n ${svn_st} ]] ; then
+	echo "ERROR: uncommitted changes"
+	echo "${svn_st}"
+	exit 1
+fi
+#mkdir hotfixes
+cd hotfixes
+#wget -nv -m -nd -np ${url}
+hotfixes=$(echo *.patch)
+cd ..
+
+popts="--no-backup-if-mismatch -p1 -f"
+for h in ${hotfixes} ; do
+	printf "%40s: " "${h}"
+	if grep -qs ${h} HOTFIXES ; then
+		echo "DONE"
+		continue
+	fi
+	if patch ${popts} --dry-run >/dev/null < hotfixes/${h} ; then
+		echo "APPLYING"
+		patch ${popts} >/dev/null < hotfixes/${h}
+		echo ${h} >> HOTFIXES
+		svn commit -m "apply upstream ${url}${h}"
+	else
+		echo "!! FAIL !!"
+	fi
+done
