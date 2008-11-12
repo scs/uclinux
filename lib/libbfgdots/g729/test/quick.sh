14f35ffc0f8482453ad5370af3c119e5f3a349aa
vivili <vivili@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2008-11-12 03:57:29 +0000

    [#2312]libbfgdots: modify shell script to make it work with command make check; atapi: modify match pattern
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7341 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- lib/libbfgdots/g729/test/quick.sh
+++ lib/libbfgdots/g729/test/quick.sh
@@ -6,9 +6,7 @@
 
 TEST_DIR=/var
 #LD_DEBUG=all ./$1 $TEST_DIR/test_data/g729a/std_in_en/SINE.BIN $TEST_DIR/test_data/g729a/std_out_en/SINEA.BIT --enc --g729a --mips
-./$1 $TEST_DIR/test_data/g729a/std_in_en/SINE.BIN $TEST_DIR/test_data/g729a/std_out_en/SINEA.BIT --enc --g729a --mips --multi
-./$1 $TEST_DIR/test_data/g729a/std_in_de/SINEA.BIT $TEST_DIR/test_data/g729a/std_out_de/SINEA.OUT --dec --g729a --mips --multi
-./$1 $TEST_DIR/test_data/g729ab/tstseq1.bin $TEST_DIR/test_data/g729ab/tstseq1a.bit --enc --g729ab --mips --multi
-./$1 $TEST_DIR/test_data/g729ab/tstseq1a.bit $TEST_DIR/test_data/g729ab/tstseq1a.out --dec --g729ab --mips --multi
-
-
+$1 $TEST_DIR/test_data/g729a/std_in_en/SINE.BIN $TEST_DIR/test_data/g729a/std_out_en/SINEA.BIT --enc --g729a --mips --multi
+$1 $TEST_DIR/test_data/g729a/std_in_de/SINEA.BIT $TEST_DIR/test_data/g729a/std_out_de/SINEA.OUT --dec --g729a --mips --multi
+$1 $TEST_DIR/test_data/g729ab/tstseq1.bin $TEST_DIR/test_data/g729ab/tstseq1a.bit --enc --g729ab --mips --multi
+$1 $TEST_DIR/test_data/g729ab/tstseq1a.bit $TEST_DIR/test_data/g729ab/tstseq1a.out --dec --g729ab --mips --multi
