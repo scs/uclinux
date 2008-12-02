ab25a33d3186a2bf8d40e39bbda6c1a411c1870b
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2008-12-02 12:14:51 +0000

    extend dma_memcpy tests  - test multiple sizes: tiny, medium, the 65k limit, multiples of 65k  - test multiple regions: L1 inst, L1 data, L2, external memory  - test alignment: 8bit, 16bit, and 32bit  - test for buffer over/underflows
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7411 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- testsuites/dmacopy/src/dmacopy.c
+++ testsuites/dmacopy/src/dmacopy.c
@@ -3,15 +3,12 @@
 #include <string.h>
 #include <bfin_sram.h>
 
-#define MEM_SIZE 0x1000
-
 static inline void show_diff(int first, int last)
 {
 	printf(" %i", first);
 	if (first != last)
 		printf("...%i", last);
 }
-
 static void dump_diff(char *ptr_a, char *ptr_b, int len)
 {
 	int i, first = -1, last;
@@ -31,79 +28,193 @@ static void dump_diff(char *ptr_a, char *ptr_b, int len)
 	printf("\n");
 }
 
-int main(void)
+/* Make sure dma_memcpy() does not over/under flow the buffers given to it.
+ * We do this by padding each buffer with a canary at the start and end.  So
+ * the actual allocation looks like:
+ * [size][canary][ ... buffer ... ][canary]
+ * The canary used is the pointer returned by malloc so we know we have a
+ * unique value at all times.  If dma_memcpy() does anything bad, the canaries
+ * will get killed in the process.
+ */
+#define canary_size sizeof(char *)
+static void *xmalloc(size_t size)
 {
-	int i, ret;
-	char *src = malloc(MEM_SIZE);
-	char *dst = malloc(MEM_SIZE);
-	char *sram = sram_alloc(MEM_SIZE, L1_INST_SRAM);
-	char *ptr;
-
-	if (!src) {
-		printf("FAIL: src = malloc(%i) failed\n", MEM_SIZE);
-		return 1;
+	char *ret = malloc(size + (canary_size * 2) + sizeof(size));
+	char *canary, *canary1, *canary2;
+	if (!ret) {
+		printf("FAIL: malloc(%zu) failed\n", size);
+		exit(10);
 	}
-	if (!dst) {
-		printf("FAIL: dst = malloc(%i) failed\n", MEM_SIZE);
-		return 1;
+	canary = (void *)&ret;
+	canary1 = ret + canary_size;
+	canary2 = canary1 + canary_size + size;
+	memcpy(ret, &size, sizeof(size));
+	memcpy(canary1, canary, canary_size);
+	memcpy(canary2, canary, canary_size);
+	return ret + sizeof(size) + canary_size;
+}
+static void xfree(void *ptr)
+{
+	char *ret;
+	char *canary, *canary1, *canary2;
+	size_t size;
+	ret = (char *)ptr - sizeof(size) - canary_size;
+	canary = (void *)&ret;
+	memcpy(&size, ret, sizeof(size));
+	canary1 = ret + canary_size;
+	canary2 = canary1 + canary_size + size;
+	if (memcmp(canary, canary1, canary_size))
+		printf("FAIL: leading canary was killed {%i,%i,%i,%i} vs {%i,%i,%i,%i}!\n",
+			canary1[0], canary1[1], canary1[2], canary1[3], canary[0], canary[1], canary[2], canary[3]);
+	if (memcmp(canary, canary2, canary_size))
+		printf("FAIL: trailing canary was killed {%i,%i,%i,%i} vs {%i,%i,%i,%i}!\n",
+			canary2[0], canary2[1], canary2[2], canary2[3], canary[0], canary[1], canary[2], canary[3]);
+	free(ret);
+}
+
+/* Do the actual test:
+ *  - set buffers to values known to be different
+ *  - copy src to chk
+ *  - copy chk to dst
+ *  - compare src and dst
+ */
+int _do_test(char *src_desc, char *dst_desc, char *src, char *dst, char *chk, int size)
+{
+	static int test_num = 1;
+	int ret = 0;
+	void *ptr;
+
+	memset(src, 's', size);
+	memset(dst, 'd', size);
+	if (((ulong)chk & 0xffa00000) != 0xffa00000)
+		memset(chk, 'c', size);
+
+	ptr = dma_memcpy(chk, src, size);
+	if (ptr)
+		printf("PASS: dma_memcpy %s[s] to %s[c]\n", src_desc, dst_desc);
+	else
+		printf("FAIL: dma_memcpy %s[s] to %s[c]\n", src_desc, dst_desc), ++ret;
+
+	ptr = dma_memcpy(dst, chk, size);
+	if (ptr)
+		printf("PASS: dma_memcpy %s[c] to %s[d]\n", dst_desc, src_desc);
+	else
+		printf("FAIL: dma_memcpy %s[c] to %s[d]\n", dst_desc, src_desc), ++ret;
+
+	int i = memcmp(dst, src, size);
+	if (!i)
+		printf("PASS: dma_memcpy test case %i, memcmp result is %d\n", test_num, i);
+	else {
+		printf("FAIL: dma_memcpy test case %i, memcmp result is %d\n", test_num, i), ++ret;
+		dump_diff(dst, src, size);
 	}
 
-	ret = 0;
-	memset(src, '*', MEM_SIZE);
-	memset(dst, 'a', MEM_SIZE);
+	++test_num;
+
+	return ret;
+}
 
 /*
  * test case 1 - dma_memcpy from src(SDRAM) to sram(SRAM) and sram(SRAM) to dst(SDRAM), then compare
  *               src(SDRAM) and dst(SDRAM) to make sure that copy into or from SRAM is ok.
+ *               also check that 8/16/32 bit transfers work by mucking with alignment.
  */
+int sram_test(int size, char *sram_desc, int flags)
+{
+	int ret = 0;
+	char *src = xmalloc(size);
+	char *dst = xmalloc(size);
+	char *sram = sram_alloc(size, flags);
 
-	if (!sram)
-		printf("SKIP: SDRAM <-> SRAM: sram_alloc(%i) failed\n", MEM_SIZE), ++ret;
-	else {
-		ptr = dma_memcpy(sram, src, MEM_SIZE);
-		if (ptr)
-			printf("PASS: dma_memcpy SDRAM to SRAM\n");
-		else
-			printf("FAIL: dma_memcpy SDRAM to SRAM\n"), ++ret;
+	printf("TEST:  --- SRAM (%s) <-> SDRAM w/%i bytes ---\n", sram_desc, size);
 
-		ptr = dma_memcpy(dst, sram, MEM_SIZE);
-		if (ptr)
-			printf("PASS: dma_memcpy SRAM to SDRAM\n");
-		else
-			printf("FAIL: dma_memcpy SRAM to SDRAM\n"), ++ret;
-
-		i = memcmp(dst, src, MEM_SIZE);
-		if (!i)
-			printf("PASS: dma_memcpy test case 1, memcmp result is %d\n", i);
-		else {
-			printf("FAIL: dma_memcpy test case 1, memcmp result is %d\n", i), ++ret;
-			dump_diff(dst, src, MEM_SIZE);
-		}
+	if (!sram) {
+		printf("FAIL: sram_alloc(%i) failed\n", size);
+		return 1;
 	}
+
+	if ((ulong)src % 4 != 0 || (ulong)dst % 4 != 0 ||
+	    (ulong)sram % 4 != 0 || size % 4 != 0)
+	{
+		printf("FAIL: SRAM src/dst/size are not 32bit aligned to start:\n"
+			"\t%p / %p / %p / %i\n",
+			src, dst, sram, size);
+		return 1;
+	}
+
+	ret += _do_test("SDRAMx32", "SRAMx32", src, dst, sram, size);
+	ret += _do_test("SDRAMx16", "SRAMx16", src+2, dst+2, sram+2, size-2);
+	ret += _do_test("SDRAMx8", "SRAMx8", src+1, dst+1, sram+1, size-1);
+
+	xfree(src);
+	xfree(dst);
+	sram_free(sram);
+
+	return ret;
+}
 
 /*
  * test case 2 - dma_memcpy from src(SDRAM) to dst(SDRAM), memcmp of src and dst,
  *               make sure that dma_memcpy in SDRAM is ok
+ *               also check that 8/16/32 bit transfers work by mucking with alignment.
  */
+int sdram_test(int size)
+{
+	int ret = 0;
+	char *src = xmalloc(size);
+	char *dst = xmalloc(size);
+	char *chk = xmalloc(size);
 
-	memset(dst, 'b', MEM_SIZE);
-	ptr = dma_memcpy(dst, src, MEM_SIZE);
-	if (ptr)
-		printf("PASS: dma_memcpy SDRAM to SDRAM\n");
-	else
-		printf("FAIL: dma_memcpy SDRAM to SDRAM\n"), ++ret;
+	printf("TEST:  --- SDRAM <-> SDRAM w/%i bytes ---\n", size);
 
-	i = memcmp(dst, src, MEM_SIZE);
-	if (!i)
-		printf("PASS: dma_memcpy test case 2, memcmp result is %d\n", i);
-	else {
-		printf("FAIL: dma_memcpy test case 2, memcmp result is %d\n", i), ++ret;
-		dump_diff(dst, src, MEM_SIZE);
+	if ((ulong)src % 4 != 0 || (ulong)dst % 4 != 0 ||
+	    (ulong)chk % 4 != 0 || size % 4 != 0)
+	{
+		printf("FAIL: SDRAM src/dst/size are not 32bit aligned to start\n"
+			"\t%p / %p / %p / %i\n",
+			src, dst, chk, size);
+		return 1;
 	}
 
-	free(src);
-	free(dst);
-	sram_free(sram);
+	ret += _do_test("SDRAMx32", "SDRAMx32", src, dst, chk, size);
+	ret += _do_test("SDRAMx16", "SDRAMx16", src+2, dst+2, chk+2, size-2);
+	ret += _do_test("SDRAMx8", "SDRAMx8", src+1, dst+1, chk+1, size-1);
+
+	xfree(src);
+	xfree(dst);
+	xfree(chk);
+
+	return ret;
+}
+
+int has_l2(void)
+{
+	/* if the part does not have L2, then don't try to use it */
+	return system("grep -qs '^L2 SRAM[[:space:]]*:[[:space:]]*0' /proc/cpuinfo");
+}
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(*arr))
+#define TEST_RANGE(range, func, args...) \
+	for (i = 0; i < ARRAY_SIZE(range##_range); ++i) \
+		ret += func(range##_range[i], ## args)
+
+int main(void)
+{
+	int ret = 0, i;
+	int sml_range[] = { 0x10, 0x1000 };
+	int mid_range[] = { 0x10, 0x1000, 0x10000, 0x12340 };
+	int lrg_range[] = { 0x10, 0x1000, 0x10000, 0x12340, 0x54320, 0x123450 };
+
+	TEST_RANGE(sml, sram_test, "L1 INST", L1_INST_SRAM);
+	TEST_RANGE(sml, sram_test, "L1 DATA", L1_DATA_SRAM);
+	if (has_l2())
+		TEST_RANGE(mid, sram_test, "L2", L2_SRAM);
+	TEST_RANGE(lrg, sdram_test);
+
+	if (ret)
+		printf("SUMMARY: %i tests failed\n", ret);
+	else
+		printf("SUMMARY: all tests passed\n");
 
 	return ret;
 }
