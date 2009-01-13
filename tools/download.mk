81a0adee92a466f51c8f502b6d445ed5a115f1e0
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2009-01-13 11:30:04 +0000

    force the z flag since older versions of tar do not dynamically detect the compression format
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7607 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- tools/download.mk
+++ tools/download.mk
@@ -1,6 +1,9 @@
 # Requirements:
 # - set VER to the package directory
 # - set URL to the download URL
+#
+# TODO:
+# - should make file format more dynamic ... not require .tar.gz ...
 
 $(DOWNLOADDIR)/$(VER).tar.gz:
 	wget -c $(URL) -P $(DOWNLOADDIR)
@@ -8,6 +11,6 @@ $(DOWNLOADDIR)/$(VER).tar.gz:
 download: $(DOWNLOADDIR)/$(VER).tar.gz
 
 $(VER)/.unpacked: $(DOWNLOADDIR)/$(VER).tar.gz
-	tar xf $<
+	tar zxf $<
 	for p in $$PWD/patches/*.patch ; do ( cd $(VER) && patch -p1 < $$p ) || exit $$? ; done
 	touch $(VER)/.unpacked
