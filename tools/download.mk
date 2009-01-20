4352a2b49fa7c688710686e4c13ac95fa67e839d
vapier <vapier@3e2a5524-39e8-4cb9-84a9-c49da69cba3a>
2009-01-20 03:31:53 +0000

    handle target filename and compression method dynamically
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7648 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- tools/download.mk
+++ tools/download.mk
@@ -1,16 +1,26 @@
 # Requirements:
 # - set VER to the package directory
 # - set URL to the download URL
-#
-# TODO:
-# - should make file format more dynamic ... not require .tar.gz ...
 
-$(DOWNLOADDIR)/$(VER).tar.gz:
-	wget -c $(URL) -P $(DOWNLOADDIR)
+A = $(DOWNLOADDIR)/$(notdir $(URL))
+$(A):
+	wget -c $(URL) -P $(dir $(A))
 .PHONY: download
-download: $(DOWNLOADDIR)/$(VER).tar.gz
+download: $(A)
 
-$(VER)/.unpacked: $(DOWNLOADDIR)/$(VER).tar.gz
-	tar zxf $<
-	for p in $$PWD/patches/*.patch ; do ( cd $(VER) && patch -p1 < $$p ) || exit $$? ; done
+P = )
+DECOMP = $(shell \
+	case $(A) in \
+	*.gz   $(P) echo gzip -dc  ;; \
+	*.bz2  $(P) echo bzip2 -dc ;; \
+	*.lzma $(P) echo lzma -dc  ;; \
+	*      $(P) echo cat       ;; \
+	esac \
+)
+
+$(VER)/.unpacked: $(A)
+	$(DECOMP) $< | tar xf -
+ifneq (,$(wildcard $(CURDIR)/patches/*.patch))
+	for p in $(CURDIR)/patches/*.patch ; do ( cd $(VER) && patch -p1 < $$p ) || exit $$? ; done
+endif
 	touch $(VER)/.unpacked
