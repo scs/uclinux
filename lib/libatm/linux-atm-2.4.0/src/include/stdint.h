adbb4b76c93bd33dabc2bc8e21ba5df1ff55e07c
Mike Frysinger <vapier@gentoo.org>
2009-04-24 04:28:41 +0000

    drop old linux-atm-2.4.0 version
    git-svn-id: svn://localhost/svn/uclinux-dist/trunk@7987 3e2a5524-39e8-4cb9-84a9-c49da69cba3a

--- lib/libatm/linux-atm-2.4.0/src/include/stdint.h
+++
@@ -1,2 +0,0 @@
-/* stdint.h - provides uintXX_t until glibc does */
-#include_next <stdint.h>
--- lib/libatm/linux-atm-2.4.0/src/test/mkerrnos.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-while (<>) {
-    next unless /^#define\s+(E\S+)\s*/;
-    printf("  { \"%s\", %s },\n",$1,$1) || die "print: $!";
-}
